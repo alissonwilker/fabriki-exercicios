0ac3226bfdd567b0d9eba15f833e7e7dc0558711
Alisson Wilker <alissonwilker@gmail.com>
2018-03-20 00:14:11 -0300

    Novo exercicio 7142.

---
+++ LDP7142/LDP7142_validador.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ev
+
+EXERCICIO=LDP7142
+
+executarEComparar () {
+	echo Entrada $2
+	java -cp ../target/classes/ Main < $1_entrada_$2.txt > saida.txt
+	diff saida.txt $1_saida_$2.txt
+	echo OK!
+
+}
+
+mvn compile;
+cd $EXERCICIO
+for ENTRADA in $(ls | grep entrada | cut -d '_' -f 3 | cut -d '.' -f 1)
+do
+	executarEComparar $EXERCICIO $ENTRADA;
+done
