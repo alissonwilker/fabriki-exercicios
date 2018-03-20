20e6fd11a451f40dc8418128f70353d7d6da819b
Alisson Wilker <alissonwilker@gmail.com>
2018-03-19 23:40:45 -0300

    Ajustando multiplicador matrizes.

--- multiplicacao_matrizes/multiplicacao_matrizes_validador.sh
+++ multiplicacao_matrizes/multiplicacao_matrizes_validador.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -ev
 
-nomeClasse=$1
+EXERCICIO=multiplicacao_matrizes
 
 executarEComparar () {
-
 	echo Entrada $2
-	java -cp target/classes/ $nomeClasse < $1_entrada_$2.txt > saida.txt
+	java -cp ../target/classes/ Main < $1_entrada_$2.txt > saida.txt
 	diff saida.txt $1_saida_$2.txt
 	echo OK!
 
 }
 
 mvn compile;
+cd $EXERCICIO
 executarEComparar $nomeClasse 01;
 executarEComparar $nomeClasse 02;
 
