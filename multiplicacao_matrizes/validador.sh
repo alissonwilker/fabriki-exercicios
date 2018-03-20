14e315e41bb30449cf758477d8f3ecec6d6a8893
Alisson Wilker <alissonwilker@gmail.com>
2018-03-19 22:58:06 -0300

    Ajustando validador.sh

--- multiplicacao_matrizes/validador.sh
+++ multiplicacao_matrizes/validador.sh
@@ -6,7 +6,7 @@ nomeClasse=$1
 executarEComparar () {
 
 	echo Entrada $2
-	java -cp ../target/classes/ $nomeClasse < $1_entrada_$2.txt > saida.txt
+	java -cp target/classes/ $nomeClasse < $1_entrada_$2.txt > saida.txt
 	diff saida.txt $1_saida_$2.txt
 	echo OK!
 
