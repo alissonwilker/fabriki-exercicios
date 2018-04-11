80d16c4a7f62410170303eccd4856f723adc5d41
12Hemily <38285738+12Hemily@users.noreply.github.com>
2018-04-11 11:20:33 -0300

    resolucao exercicio

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,18 @@
+public abstract class Cliente {
+	private String nome;
+	
+	
+	public String getNome(){
+		
+		return this.nome;
+}	
+    protected Cliente(String nome){
+	    this.nome = nome;
+	
+}
+    
+
+}
+
+
+
