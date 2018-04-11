4d380ba13181f6e10f8203f0f823b8c95aed0f95
12Hemily <38285738+12Hemily@users.noreply.github.com>
2018-04-11 11:20:32 -0300

    resolucao exercicio

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,33 @@
+public class ContaBancaria {
+	private Cliente cliente;
+	private int saldo;
+
+	public ContaBancaria(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	public void depositar(int valor) {
+		if (valor > 0) {
+			saldo += valor;
+			System.out.println("Valor Depositado: " + valor + "\n");
+			System.out.println("Saldo Atualizado: " + saldo);
+		}
+	}
+
+	public int sacar(int valor) {
+		if (valor > 0 && valor <= saldo) {
+			saldo -= valor;
+			System.out.println("Valor Sacado: " + valor + "\n");
+			System.out.println("Saldo Atualizado: " + saldo);
+		}
+		return valor;
+	}
+}
