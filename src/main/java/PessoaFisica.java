9f97fa6a7fb80d2f6e279ab0aaa688030b056521
Felipe Lima <felipellima83@gmail.com>
2018-04-11 10:03:36 -0300

    resolucao exercicio

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,16 @@
+
+public class PessoaFisica extends Cliente{
+
+	private String cpf;	
+	
+	public PessoaFisica(String nome, String cpf) {
+		// TODO Auto-generated constructor stub
+		super(nome);
+		this.cpf = cpf;		
+	}
+	
+	public String getCpf(){
+		return this.cpf;
+	}
+	
+}
