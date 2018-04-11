2af65f2bb31a143883d88bbef3ecffc08eeafbd9
Aluferox <38284778+Aluferox@users.noreply.github.com>
2018-04-11 11:50:21 -0300

    resolucao exercicio

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+
+public abstract class Cliente {
+			
+	private String nome;
+	
+	protected Cliente(String nomeCliente){
+		nome = nomeCliente;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getNome() {
+		
+		return nome;
+	}
+}
