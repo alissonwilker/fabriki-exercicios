854e1ac0553a009e71dc372230b651c3526dc8d6
Alisson Wilker <alissonwilker@gmail.com>
2018-05-20 21:29:27 -0300

    Corrigindo script.

--- resources/scripts/infra_testes.sh
+++ resources/scripts/infra_testes.sh
@@ -7,7 +7,11 @@ mkdir -p src/main/resources/
 mkdir -p src/main/webapp/WEB-INF/lib/
 
 executarTestesIntegracao() {
-	mvn clean install verify $PROFILE_WILDFLY_MANAGED;
+	wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz;
+	mkdir /tmp/geckodriver;
+	tar -xzf geckodriver-v0.20.1-linux64.tar.gz -C /tmp/geckodriver;
+	export PATH=$PATH:/tmp/gecko-driver;
+	xvfb-run mvn clean install verify $PROFILE_WILDFLY_MANAGED;
 }
 
 executarTestesUnitarios() {
