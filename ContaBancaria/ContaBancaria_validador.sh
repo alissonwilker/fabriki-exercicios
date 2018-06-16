d79a0edede2562330033fe12c78b1bf2c82bbf66
Alisson Wilker <alissonwilker@gmail.com>
2018-06-16 01:14:17 -0300

    Melhorando código de exercícios web.

--- ContaBancaria/ContaBancaria_validador.sh
+++ ContaBancaria/ContaBancaria_validador.sh
@@ -4,6 +4,6 @@ source ./resources/scripts/infra_testes.sh
 
 EXERCICIO=ContaBancaria
 
-cp ./$EXERCICIO/ContaBancariaTest.java src/test/java/
+cp -R ./$EXERCICIO/src/* src/*
 
 executarTestesUnitarios;
--- ContaBancariaWeb/ContaBancariaWeb_validador.sh
+++ ContaBancariaWeb/ContaBancariaWeb_validador.sh
@@ -4,8 +4,6 @@ source ./resources/scripts/infra_testes.sh
 
 EXERCICIO=ContaBancariaWeb
 
-cp ./$EXERCICIO/ContaBancariaTest.java src/test/java/
-cp ./$EXERCICIO/ContaBancariaWebITCase.java src/integration-test/java/
-cp ./$EXERCICIO/index.html src/main/webapp/
+cp -R ./$EXERCICIO/src/* src/*
 
 executarTestesIntegracao;
