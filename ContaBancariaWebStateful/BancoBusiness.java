cbe8ba0e362c59c8220e525b13aa61cbb873de69
Alisson Wilker <alissonwilker@gmail.com>
2018-06-17 14:27:44 -0300

    Adicionando implementação parcial das classes.

--- ContaBancariaWebStateful/BancoBusiness.java
+++ ContaBancariaWebStateful/BancoBusiness.java
@@ -3,13 +3,15 @@ public class BancoBusiness {
         pessoaFisica, pessoaJuridica
     }
 
+    /**
+     * Deposita um valor e em seguida saca outro valor, retornando o novo saldo.
+     * @param contaBancaria A conta bancaria na qual deve ser realizada a operacao.
+     * @param valorDeposito O valor a ser depositado na conta.
+     * @param valorSaque O valor a ser sacado da conta.
+     * @return O novo saldo apos as operacoes de deposito e saque.
+     */
     public Integer depositarSacarValor(ContaBancaria contaBancaria, int valorDeposito, int valorSaque) {
-        if (contaBancaria != null) {
-            contaBancaria.depositar(valorDeposito);
-            int saldo = contaBancaria.sacar(valorSaque);
-            return saldo;
-        }
-        return null;
+        // TODO IMPLEMENTE O CODIGO DESTE METODO.
     }
 
     public Integer sacarValor(ContaBancaria contaBancaria, int valorSaque) {
@@ -46,15 +48,14 @@ public class BancoBusiness {
         return null;
     }
 
+    /**
+     * Recupera o CPF do cliente (dono) da conta bancaria.
+     * @param contaBancaria A conta bancaria cujo CPF do cliente (dono) deve ser recuperado.
+     * @return O CPF do cliente (dono) da conta bancaria.
+     */
     public String recuperarCpfCliente(ContaBancaria contaBancaria) {
-        if (contaBancaria != null) {
-            Cliente cliente = contaBancaria.getCliente();
-            if (cliente instanceof PessoaFisica) {
-                return ((PessoaFisica) cliente).getCpf();
-            }
-        }
-        return null;
-    }
+        // TODO IMPLEMENTE O CODIGO DESTE METODO.
+   }
 
     public String recuperarNomeCliente(ContaBancaria contaBancaria) {
         if (contaBancaria != null) {
@@ -63,13 +64,14 @@ public class BancoBusiness {
         return null;
     }
 
+    /**
+     * Cria uma conta bancaria de Pessoa Fisica ou de Pessoa Juridica, de acordo com o tipo de cliente.
+     * @param nomeCliente O nome do cliente (dono) da conta bancaria.
+     * @param cpfCnpj O CPF ou CNPJ do cliente (dono) da conta bancaria.
+     * @param tipoCliente O tipo de cliente (Pessoa Fisica ou Pessoa Juridica).
+     * @return A nova conta bancaria do cliente. 
+     */
     public ContaBancaria criarContaBancaria(String nomeCliente, String cpfCnpj, TipoCliente tipoCliente) {
-        ContaBancaria contaBancaria;
-        if (TipoCliente.pessoaFisica.equals(tipoCliente)) {
-            contaBancaria = new ContaBancaria(new PessoaFisica(nomeCliente, cpfCnpj));
-        } else {
-            contaBancaria = new ContaBancaria(new PessoaJuridica(nomeCliente, cpfCnpj));
-        }
-        return contaBancaria;
+        // TODO IMPLEMENTE O CODIGO DESTE METODO.
     }
 }
