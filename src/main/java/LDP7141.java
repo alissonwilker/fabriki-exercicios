fd9866eb94b4d3f50efa458d921ceded8248223b
12Hemily <38285738+12Hemily@users.noreply.github.com>
2018-06-29 14:58:03 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,24 @@
+public class LDP7141 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int kilo = 0;
+		int multa = 0;
+		int excedente= 0;
+		
+		System.out.println ("Digite o kilo: ");
+		kilo = entrada.nextInt();
+		 
+			if (kilo > 50){
+				excedente = kilo - 50;
+				multa = excedente*4;
+				
+			}
+		
+            saida.println ("excedente =  "+excedente);
+            System.out.println ("multa = "+multa);
+      }
+
+}
