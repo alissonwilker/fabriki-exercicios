3b995ae7b4414ec372e7e44a32b3304af631632b
pqpsamuk <38284164+pqpsamuk@users.noreply.github.com>
2018-06-30 10:00:13 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,19 @@
+public class LDP7141 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Qual é o valor do Peso?");
+
+        
+        
+        int peso = Integer.parseInt(input.nextLine());
+
+        
+        int excesso = peso - 50;
+        if(excesso > 0) {                
+            int multa = excesso * 4 ;
+            
+             System.out.printf("Valor do Excedente é " + excesso + "KG" + " Valor da multa é " + multa + "R$");
+        }
+    }    
+}
