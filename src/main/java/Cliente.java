9b10eaf599c3a69ac425e59c74a632af895aea2d
raimundojorgemoreira <38284541+raimundojorgemoreira@users.noreply.github.com>
2018-07-01 14:57:55 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,19 @@
+public abstract class Cliente {
+	private String nome;
+	
+	
+	protected Cliente(String nome){
+		
+		this.nome=nome;
+		
+	}
+	
+	public String getNome(){
+		return nome;
+	}
+	
+	
+	
+	
+}
+   
