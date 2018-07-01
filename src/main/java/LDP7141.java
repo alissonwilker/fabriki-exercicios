b2e27cc87b0320474e3d0f9aed959b5c02aa7e39
guinsf <38284477+guinsf@users.noreply.github.com>
2018-07-01 13:09:45 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,25 @@
+
+public class LDP7141 {
+	
+	private static Scanner entrada = new scanner(System.in);
+	private static PrintStream = new scanner = System.out;
+	
+	public static void main(String[] args) {
+		
+		
+		
+		int ex;
+		
+		
+		System.out.println("Peso dos peixes em quilos:");
+		int peso;
+		if (peso>50) {
+			(ex<peso-50);
+		
+		System.out.println("Excesso de peso");
+	}
+		else
+			System.out.println("Peso dentro do limite");
+	}
+	
+}
