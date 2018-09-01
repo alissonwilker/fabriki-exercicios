9fa5d6eb647f367d1b7226321eebd8a15e64adbf
alessandrobsb061 <42880569+alessandrobsb061@users.noreply.github.com>
2018-08-31 21:53:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,20 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class LDP7141 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+	public static void main(String[] args){
+		int peso = entrada.nextInt();
+		int multa = 0;
+		int pesoexcedente=0;		
+		if (peso > 50 ) {
+			pesoexcedente = peso - 50;
+			multa = pesoexcedente * 4;
+			saida.println("excedente = "+ pesoexcedente);
+			saida.println("multa = " + multa);
+		} else {
+			saida.println("excedente = "+ pesoexcedente);
+			saida.println("multa = " + multa);
+		}	
+	}
+}
