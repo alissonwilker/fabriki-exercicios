4f50a6f70f724d64d70d7ec00c1c2f7d0636b50d
mhcp5673 <42880611+mhcp5673@users.noreply.github.com>
2018-08-31 21:55:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,22 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7141 {
+	private static Scanner entrada = new Scanner(System.in);
+    private static PrintStream saída = System.out;
+
+	public static void main(String[] args) {
+		int pesoEntrada = entrada.nextInt();
+        int pesoExcedente = entrada.nextByte()-50;
+        if (pesoEntrada == 50 ){	
+         System.out.println("pesoEntrada");
+        } else if(pesoExcedente >= 50){
+          System.out.println("pesoExcedente =");
+          else (pesoExcedente*= 4){
+          System.out.println("pesoExcedente*= 4 =" + multa);  
+          }
+          
+        }
+	}
+
+}
