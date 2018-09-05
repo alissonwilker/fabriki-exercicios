38e1db05abec466039dce30e19ccdd6e1be0336e
mhcp5673 <42880611+mhcp5673@users.noreply.github.com>
2018-09-05 20:48:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,41 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7142 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int C;
+		int N, E = 0, PrecoHora, PrecoExcedente;
+		boolean continuar = true;
+		char texto;
+
+		do {
+			E = 0;
+			PrecoHora = 10;
+			PrecoExcedente = 20;
+
+			System.out.print("Digite o código do operário: ");
+			C = entrada.nextInt();
+
+			System.out.print("Digite o número de horas trabalhadas: ");
+			N = entrada.nextInt();
+
+			if (N > 50) {
+				E = N - 50;
+			}
+
+			System.out.println("\ncodigo: " + C + "\nSalario: " + ((N - E) * 10) + "\nSalario excedente: "
+					+ (E * PrecoExcedente) + "\nSalario total: " + (E * PrecoExcedente + (N - E) * PrecoHora)
+					);
+			texto = entrada.next().charAt(0);
+			if (texto == 's' || texto == 'S') {
+				continuar = false;
+
+			}
+
+		} while (continuar);
+
+	}
+
+}
