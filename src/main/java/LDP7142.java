e63c1ef1b2c7435623a3a3bf79e6b84ad205c153
CarlosAndreBarbosadaSilva <42880530+CarlosAndreBarbosadaSilva@users.noreply.github.com>
2018-09-05 20:21:17 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,30 @@
+mport java.util.Scanner;
+
+public class LDP7142 {
+
+	public static void main(String[] args) {
+		Scanner entrada=new scanner(Sistem.in);
+		int c;
+		double n,e =0, precohora,precoexcedente;
+		boolean continuar =true;
+		char texto;
+		do {
+		 E = 0;
+		 precohora=10;
+		 precoexcedente=20
+			
+			if(n > 50){
+				
+			E = n-50;
+			}
+				
+			System.out.print("\nID:"+ C +"\nsalario:"+((N-E)*10) +"\nsalario excedente:"
+			+ (E * precoexcedente) + "\nsalario total:" +(E * precoexcedente + (N -E) * precohora)
+			+ "\n\n Deseja encerra o programa?(S/N)");
+			texto = entrada.nex().charAt(0);
+			if (texto =="S"|| texto =="S"){
+				continuar = false;
+				
+			}
+		}while (continuar);
+	}
