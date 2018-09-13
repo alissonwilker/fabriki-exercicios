c5247114dcc99443513046f54317ba9ac4ade4a4
CarlosEduLima <42013761+CarlosEduLima@users.noreply.github.com>
2018-09-12 21:25:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+
+public class LDP7143  {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		
+		 int a = entrada.nextInt();
+		 int b = entrada.nextInt();
+		 int c = entrada.nextInt();
+		 int d = entrada.nextInt();
+		 
+		 int a2 =  (a*a);
+		 int b2 =  (b*b);
+		 int c2 =  (c*c);
+		 int d2 =  (d*d);
+		 
+		 if (c2 >= 1000) {
+			 saida.println(c2);
+			
+		}
+
+		else {
+			saida.println(a2);
+			saida.println(b2);
+			saida.println(c2);
+			saida.println(d2);
+			
+		}
+		
+		
+		
+	}
+
+}
