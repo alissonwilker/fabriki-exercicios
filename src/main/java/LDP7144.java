44457f2ad53094ddc53ee135acba6b64fec7ede6
michelclemer <42013634+michelclemer@users.noreply.github.com>
2018-09-14 21:02:55 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Mayara
+ */
+public class LDP7144 {
+    public static void main(String[] args) {
+        Scanner t= new Scanner(System.in);
+        
+        int num = t.nextInt();
+        
+        if ((num % 2)== 0) {
+            if (num >=0) {
+                System.out.println("PAR E POSITIVO");
+            }else{
+                System.out.println("PAR E NEGATIVO");
+            }
+        }
+        else{
+            if (num>0) {
+                 System.out.println("ÍMPAR E POSITIVO");
+            }else{
+                System.out.println("ÍMPAR E NEGATIVO");
+            }
+           
+        }
+    }
+}
