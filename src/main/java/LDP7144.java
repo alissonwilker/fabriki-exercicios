389adbcb7b33c8349bdcbd69bc7f96b33e7d703b
CarlosAndreBarbosadaSilva <42880530+CarlosAndreBarbosadaSilva@users.noreply.github.com>
2018-09-14 21:42:00 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LDP7144 {
+	private static Scanner entrada = new Scanner (System.in);
+	
+	public static void main(String[] args) {
+		        
+        int num ;
+		boolean parar = false;
+		char letra;
+
+		do{
+			System.out.println("Digite um número inteiro");
+			num = entrada.nextInt();
+
+
+			if(num%2==0){
+				System.out.println("O número é par.\n");
+			}else{
+				System.out.println("O número  é impar\n");
+			}
+			if(num<0){
+				System.out.println("O número é negativo\n");
+			}else{
+				System.out.println("O número é positivo\n");
+			}
+
+			System.out.println("Deseja sair? (s/N)? ");
+			letra = entrada.next().charAt(0);
+			if(letra == 's' || letra == 'S'){
+				parar = true;
+			}
+
+		}while(parar==false);
+
+	}
+}
+			
+			
+		
+	
