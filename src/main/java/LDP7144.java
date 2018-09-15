df968d923e7556eee98bf9a3454da9cd0c495b19
marcosvandrade <41090574+marcosvandrade@users.noreply.github.com>
2018-09-14 21:09:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author Marcos
+ * 
+ * Faça um programa que leia um número inteiro e mostre uma mensagem indicando se este número é par ou ímpar, e se é positivo ou negativo
+ * 
+ */
+public class LDP7144 {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        int numero = entrada.nextInt();
+        
+        if (numero >=0 && numero % 2 == 0) {
+        
+            System.out.println("PAR E POSITIVO");
+        
+        }
+        
+        if (numero >= 0 && numero % 2 != 0){
+        
+            System.out.println("ÍMPAR E POSITIVO");
+                
+        }
+        
+        if (numero < 0 && numero % 2 == 0){
+        
+            System.out.println("PAR E NEGATIVO");
+        
+        }
+        
+        if (numero < 0 && numero % 2 != 0 ){
+        
+            System.out.println("ÍMPAR E NEGATIVO");
+        
+        }
+                
+    }
+    
+}
