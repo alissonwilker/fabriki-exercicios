89707e601266f5caf750f19cdf0ff4cdaf099a96
LucBarros <42049529+LucBarros@users.noreply.github.com>
2018-09-14 21:00:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,28 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7144 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+	
+	public static void main(String[]args) {
+		
+		String parouimpar = "";
+		String negouposi = "";
+		
+		int num = entrada.nextInt();
+		
+		if (num%2==0){
+			 parouimpar = "PARA";}
+			else{ 
+		     parouimpar = "ÍMPAR";
+		}
+		
+		if (num >= 0) {
+		    negouposi = "POSITIVO";}
+			else{
+		    negouposi = "NEGATIVO";
+			}
+		saida.println(parouimpar +" E "+ negouposi);
+		}
+}
