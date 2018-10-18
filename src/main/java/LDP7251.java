3559782fdbe2a81edfc90087d53fb4cef49835c8
CarlosEduLima <42013761+CarlosEduLima@users.noreply.github.com>
2018-10-17 21:49:32 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LDP7251 {
+	private static Scanner entrada = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int num;
+		int maior = 0;
+		do {
+			System.out.println("Digite um número:");
+			num = entrada.nextInt();
+			if (num > maior) {
+				maior = entrada.nextInt();
+           }
+			
+			else if (num < maior) {
+				maior = entrada.nextInt();
+			}
+			
+			System.out.println("maior = " + maior);
+		} while (num != 0);
+
+	}
+
+}
