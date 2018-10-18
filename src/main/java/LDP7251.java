4f73b99e26ad29703d188b210359b3f809e12f36
michelclemer <42013634+michelclemer@users.noreply.github.com>
2018-10-17 21:35:30 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,24 @@
+ */
+public class LDP7251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner t = new Scanner(System.in);
+        int maior = 0;
+        int i = t.nextInt();
+        while(i!=0){
+            if(i>maior){
+                maior = i;
+            }
+            else if(i <0 && i<maior){
+                maior = i;
+            }
+            i = t.nextInt();
+        }
+        System.out.println("maior = "+maior);
+    }
+    
+}
