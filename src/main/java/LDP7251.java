e25666845fdd442db0ad795175bc4fd4da65fe39
nery1000 <30377461+nery1000@users.noreply.github.com>
2018-10-19 21:39:43 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,43 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7251 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+	static int maior = 0;
+
+	public static void main(String[] args) {
+
+		int num = entrada.nextInt();
+
+		maior = num;
+		
+		if ( num == 0) {
+			System.out.println("maior = " + maior);
+		}
+		else {
+		while (num != 0) {
+			System.out.println("Digite o proximo númeoro positivo: ");
+			num = entrada.nextInt();
+			if (num > maior && num != 0) {
+				maior = num;
+			} else {
+				maior = maior;
+			}
+		}
+
+//		while (num < 0) {
+//			System.out.println("Digite o proximo númeoro negativo: ");
+//			num = entrada.nextInt();
+//			if (num < maior) {
+//				maior = num;
+//			} else {
+//				maior = maior;
+//			}
+//		}
+
+		System.out.println("maior = " + maior);
+		}
+	}
+
+}
