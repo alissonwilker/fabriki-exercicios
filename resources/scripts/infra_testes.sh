a48c7da7e2a6506d3edd9203287a8e9e75a48f85
Alisson Wilker <alissonwilker@gmail.com>
2018-10-20 21:09:26 -0300

    Parametrizando validador e adicionando files_zip.

--- resources/scripts/infra_testes.sh
+++ resources/scripts/infra_testes.sh
@@ -1,10 +1,25 @@
-set -ev
+#set -ev
 
 PROFILE_WILDFLY_MANAGED="-P wildfly-managed"
 
 mkdir -p src/test/java/
 mkdir -p src/main/resources/
 mkdir -p src/main/webapp/WEB-INF/lib/
+
+executarEComparar() {
+	echo Entrada $2
+	java -cp ./target/classes/ $1 < $1_entrada_$2.txt > saida.txt
+	diff saida.txt $1_saida_$2.txt
+	echo OK!
+}
+
+executarTestesEntradaESaida() {
+	mvn -e -X -V compile -Dmaven.test.skip=true -Dmaven.javadoc.skip=true;
+	for ENTRADA in $(ls | grep entrada | cut -d '_' -f 3 | cut -d '.' -f 1)
+	do
+		executarEComparar $1 $ENTRADA;
+	done
+}
 
 executarTestesIntegracao() {
 	wget https://github.com/mozilla/geckodriver/releases/download/v0.20.1/geckodriver-v0.20.1-linux64.tar.gz;
@@ -13,22 +28,6 @@ executarTestesIntegracao() {
 	xvfb-run mvn clean install verify $PROFILE_WILDFLY_MANAGED;
 }
 
-executarTestesUnitarios() { 
+executarTestesUnitarios() {
 	mvn clean test;
-}
-
-executarEComparar() {
-	echo Entrada $2
-	java -cp ../target/classes/ $1 < $1_entrada_$2.txt > saida.txt
-	diff saida.txt $1_saida_$2.txt
-	echo OK!
-}
-
-executarTestesEntradaESaida() {
-	mvn -e -X -V compile -Dmaven.test.skip=true -Dmaven.javadoc.skip=true; 
-	cd $1
-	for ENTRADA in $(ls | grep entrada | cut -d '_' -f 3 | cut -d '.' -f 1)
-	do
-		executarEComparar $1 $ENTRADA;
-	done
 }
---
+++ resources/scripts/validador.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./resources/scripts/infra_testes.sh
+
+FILES_ZIP_URL=$1
+TIPO_VALIDACAO=$2
+EXERCICIO=$3
+
+if [ $FILES_ZIP_URL ]; then
+  curl -o files.zip $1;
+  unzip files.zip;
+fi
+
+$2 $3;
