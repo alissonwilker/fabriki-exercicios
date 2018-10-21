fa19b4696e5a55c1b70912bdce503cd49b5ebf85
Alisson Wilker <alissonwilker@gmail.com>
2018-10-20 22:30:04 -0300

    Corrigindo curl.

--- resources/scripts/validador.sh
+++ resources/scripts/validador.sh
@@ -7,10 +7,10 @@ TIPO_VALIDACAO=$2
 EXERCICIO=$3
 
 if [ $FILES_ZIP_URL ]; then
-#  curl -o files.zip $1;
-  wget -o files.zip $1;
-  ls -lha;
-  zip -sf files.zip;
+  curl -o files.zip -L $1;
+#  wget -o files.zip $1;
+#  ls -lha;
+#  zip -sf files.zip;
   unzip files.zip;
 fi
 
