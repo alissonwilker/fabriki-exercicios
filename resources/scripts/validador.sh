c62ccdc5073c653fddaff532b5e97ba7b3c82979
Alisson Wilker <alissonwilker@gmail.com>
2018-10-30 00:18:49 -0200

    Removendo unzip.

--- resources/scripts/validador.sh
+++ resources/scripts/validador.sh
@@ -6,9 +6,9 @@ FILES_ZIP_URL=$1
 TIPO_VALIDACAO=$2
 EXERCICIO=$3
 
-if [ $FILES_ZIP_URL ]; then
-  curl -o files.zip -L $1;
-  unzip files.zip;
-fi
+#if [ $FILES_ZIP_URL ]; then
+#  curl -o files.zip -L $1;
+#  unzip files.zip;
+#fi
 
 $2 $3;
