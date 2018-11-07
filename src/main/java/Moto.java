12f4a554edaf182acbd1f2ad6a796a30609e3ccb
alissonwilker <alissonwilker@gmail.com>
2018-11-07 03:07:05 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Moto.java
@@ -0,0 +1,12 @@
+/**
+ * Tipo que representa uma Moto.
+ *
+ */
+public class Moto extends Automovel {
+    /**
+     * Construtor que apenas configura a quantidade de pneus do veiculo.
+     */
+    public Moto() {
+        this.qtdePneus = 2;
+    }
+}
