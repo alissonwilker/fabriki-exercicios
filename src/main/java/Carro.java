4afe55b4b21833b2ffeab474fec33b8b6514424f
alissonwilker <alissonwilker@gmail.com>
2018-11-07 20:17:41 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,17 @@
+
+public class Carro extends Automovel {
+
+	public Carro() {
+		//TODO
+	}
+	
+	public void moverParaTras() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void engatarMarchaARe() {
+		//TODO
+	}
+
+}
