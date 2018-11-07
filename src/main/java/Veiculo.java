57b2ea8db0dd4d8e4730e19e0f3bc5cf08cea0d4
alissonwilker <alissonwilker@gmail.com>
2018-11-07 20:17:41 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,45 @@
+
+public abstract class Veiculo {
+	protected int velocidade;
+	protected int qtdePneus;
+	
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getVelocidade() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	protected boolean estaEmMovimento() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void frear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void parar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getQtdePneus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	protected void mover() {
+		
+	}
+
+}
