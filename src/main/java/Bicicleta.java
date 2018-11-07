90300d03867d0813030094ef0dc7e72fe3947d85
alissonwilker <alissonwilker@gmail.com>
2018-11-07 20:41:21 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,20 @@
+
+public class Bicicleta extends Veiculo {
+	public Bicicleta() {
+		//TODO
+	}
+}
+//
+///**
+// * Tipo que representa uma Bicicleta.
+// *
+// */
+//public class Bicicleta extends Veiculo {
+//    /**
+//     * Construtor que apenas configura a quantidade de pneus do veiculo.
+//     */
+//    public Bicicleta() {
+//        this.qtdePneus = 2;
+//    }
+//
+//}
