5f43bc0415f2b7bfef3849257023b2a999cbf076
CarlosAndreBarbosadaSilva <42880530+CarlosAndreBarbosadaSilva@users.noreply.github.com>
2018-11-11 19:53:21 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,18 @@
+public class Carro extends Automovel{
+	
+	public Carro(){
+	}
+	
+	
+	
+
+	public void moverParaTras() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void engatarMarchaARe(){
+	
+	}
+
+}
