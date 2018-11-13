70d4638edc7055fef828dcca39a355adc5e9eb53
marcosvandrade <41090574+marcosvandrade@users.noreply.github.com>
2018-11-13 14:34:24 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,41 @@
+public class Carro extends Automovel {
+
+	private boolean marchaARe;
+		
+	public boolean getMarchaARe() {
+		return marchaARe;
+	}
+
+	public void setMarchaARe(boolean marchaARe) {
+		this.marchaARe = marchaARe;
+	}
+
+	public Carro() {
+		setQtdePneus(4);
+	}
+	
+	public void moverParaTras() {
+		if(estaLigado() == false) {
+			ligar();
+			engatarMarchaARe();
+			mover();
+			
+		}else {
+			ligar();
+			engatarMarchaARe();
+			mover();
+		}
+	}
+	
+	protected void engatarMarchaARe() {
+		if(marchaAFrente == true) {
+			if(estaEmMovimento() == true) {
+				parar();
+				marchaARe = true;
+			} else {
+				marchaARe = true;
+			}
+		}
+	}
+	
+}
