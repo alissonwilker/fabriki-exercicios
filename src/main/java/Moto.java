4bd7bcd5f20c8536a4221ae6695c818f964b0a8e
augustochaves23 <42880517+augustochaves23@users.noreply.github.com>
2018-11-13 14:18:42 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Moto.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Moto extends Automovel {
+	private Moto moto;
+
+    public void setUp() throws Exception {
+        moto = new Moto();
+    }
+
+    public void testMoto() {
+        assertEquals(2, moto.getQtdePneus());
+        moto.aumentarVelocidade();
+        assertEquals(0, moto.getVelocidade());
+        assertFalse(moto.estaEmMovimento());
+        moto.frear();
+        assertEquals(0, moto.getVelocidade());
+        moto.moverParaFrente();
+        assertEquals(1, moto.getVelocidade());
+        assertTrue(moto.estaEmMovimento());
+        assertTrue(moto.estaLigado());
+        assertTrue(moto.estaEmMarchaAFrente());
+        moto.aumentarVelocidade();
+        assertEquals(2, moto.getVelocidade());
+        moto.desligar();
+        assertFalse(moto.estaEmMovimento());
+        assertFalse(moto.estaLigado());
+        moto.moverParaFrente();
+        assertTrue(moto.estaLigado());
+        assertTrue(moto.estaEmMarchaAFrente());
+        assertEquals(1, moto.getVelocidade());
+    }
+
+}
