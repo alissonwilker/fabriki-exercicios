0d273fa8771fb5c7be209c83e86d38b001e91bab
FilipeTSI-BSB <42013700+FilipeTSI-BSB@users.noreply.github.com>
2018-11-13 20:46:20 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,17 @@
+public class Carro extends Automovel {
+
+	public Carro() {
+	qtdePneus = 4;
+}
+
+public void moverParaTras() {
+
+frear();
+parar();
+engatarMarchaARe();
+}
+protected void engatarMarchaARe() {
+		velocidade++;
+		marchaAFrente=false;
+	}
+}
