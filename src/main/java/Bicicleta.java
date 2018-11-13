9f258c9c90aabd73131ed8c5ae7e446e93ac6f90
augustochaves23 <42880517+augustochaves23@users.noreply.github.com>
2018-11-13 14:18:43 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Bicicleta extends Veiculo {
+	private Bicicleta bicicleta;
+
+    public void setUp() throws Exception {
+        bicicleta = new Bicicleta();
+    }
+
+    public void testBicicleta() {
+        assertEquals(2, bicicleta.getQtdePneus());
+        bicicleta.aumentarVelocidade();
+        assertEquals(0, bicicleta.getVelocidade());
+        assertFalse(bicicleta.estaEmMovimento());
+        bicicleta.frear();
+        assertEquals(0, bicicleta.getVelocidade());
+        bicicleta.moverParaFrente();
+        assertEquals(1, bicicleta.getVelocidade());
+        assertTrue(bicicleta.estaEmMovimento());
+        bicicleta.aumentarVelocidade();
+        assertEquals(2, bicicleta.getVelocidade());
+        bicicleta.moverParaFrente();
+        assertEquals(2, bicicleta.getVelocidade());
+        bicicleta.frear();
+        assertEquals(1, bicicleta.getVelocidade());
+        bicicleta.parar();
+        assertEquals(0, bicicleta.getVelocidade());
+    }
+
+}
