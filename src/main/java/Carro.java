1654f6da3b0e68dab796998fb830bbbf4b717464
Lucas P. Sodré <llucasbrasill@gmail.com>
2018-11-13 12:17:44 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,15 @@
+
+public class Carro  extends Automovel {
+    
+    Carro carro = new Carro(){
+        
+        @Override
+        public void moverParaFrente() {
+            super.moverParaFrente(); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+        
+    };
+        
+    
+}
