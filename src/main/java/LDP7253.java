8a0393293718688d84b34ff052c5d7684dfff184
michelclemer <42013634+michelclemer@users.noreply.github.com>
2018-11-14 20:43:26 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author micehl
+ */
+public class LDP7253 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 1; i<=100; i++){
+            if((i % 10)==0){
+                System.out.println("multiplo de 10");
+            }
+        }
+    }
+    
+}
