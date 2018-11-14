edd8f058febdcc367c5364d75573cc2c32a12c6f
FilipeTSI-BSB <42013700+FilipeTSI-BSB@users.noreply.github.com>
2018-11-14 20:59:43 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,11 @@
+public class LDP7253 {
+private static PrintStream saida = System.out; 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 100; ++i) {
+			if(i%10==0){
+			saida.println("Múltiplo 10");
+		}
+	}
+}
+}
