d9c5a9572c80f3209a89f53c61295bf5e89bd6c8
lucasmss <42013706+lucasmss@users.noreply.github.com>
2018-11-14 20:55:23 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,44 @@
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		int n;
+		
+		n =1;
+		
+		while(n>0 && n<101){
+			n++;
+			if(n==10){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==20){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==30){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==40){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==50){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==60){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==70){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==80){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==90){
+				System.out.println("Múltiplo de 10");
+			}
+			if(n==100){
+				System.out.println("Múltiplo de 10");
+			}
+		}
+
+	}
+
+}
