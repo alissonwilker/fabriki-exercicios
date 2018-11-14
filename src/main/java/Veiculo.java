b92d5d84c5d8925ca13e8acaaaafdedf7609964c
LucBarros <42049529+LucBarros@users.noreply.github.com>
2018-11-14 20:51:05 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,52 @@
+public abstract class Veiculo {
+	
+	protected int velocidade;
+	protected int qtdePneus;
+	
+	
+
+	public int getVelocidade() {
+		// TODO Auto-generated method stub
+		return velocidade;
+	}
+	
+	public int getQtdePneus() {
+		// TODO Auto-generated method stub
+		return qtdePneus;
+	}
+	
+	
+
+	public void frear() {
+		// TODO Auto-generated method stub
+	if(velocidade > 0) {	
+		velocidade--;
+	}else {
+		velocidade = 0;
+	}
+		
+	}
+
+	public void parar() {
+		// TODO Auto-generated method stub
+		velocidade = 0;
+	}
+
+
+
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+	if(estaEmMovimento() == true) {	
+		velocidade++;
+	}
+	}
+
+
+
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+	if(velocidade <= 0) {	
+		velocidade++;
+	}
+	}
+	
