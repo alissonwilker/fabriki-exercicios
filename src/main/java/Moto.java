12620b925cf20a220b1394630c0f6674cfd01995
augustochaves23 <42880517+augustochaves23@users.noreply.github.com>
2018-11-13 23:54:00 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Moto.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Moto extends Veiculo{
+	public Moto() {
+		qtdePneus=2;
+	}
+}
