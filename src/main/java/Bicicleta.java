430082a4630cff7863e79cd647576e725ef86c6f
augustochaves23 <42880517+augustochaves23@users.noreply.github.com>
2018-11-13 23:54:01 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Bicicleta extends Veiculo{
+	public Bicicleta() {
+		qtdePneus=2;
+	}
+}
