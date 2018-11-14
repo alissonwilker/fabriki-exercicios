d89f24fa96d7fb572646ef6a285f7768d8de759e
Lucas P. Sodré <llucasbrasill@gmail.com>
2018-11-14 21:17:05 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,22 @@
+public class LDP7142 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+    int cont = 0;
+    
+    while (cont <= 100) {
+        cont++;
+        
+        if (cont % 10 == 0) {
+            System.out.println("Mútiplo de 10");	
+        }
+    }
+    
+        
+    }
+    
+}
