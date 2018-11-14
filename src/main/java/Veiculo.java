041f55ca899be259b5edc37e9386197ce308414b
augustochaves23 <42880517+augustochaves23@users.noreply.github.com>
2018-11-13 23:56:29 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class Veiculo{
+	protected int velocidades;
+	protected int qtdePneus;
+	
+	protected boolean estaEmMovimento() {
+		if(velocidades>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public void aumentarVelocidade() {
+		if(estaEmMovimento()==true) {
+			velocidades++;
+		}
+	}
+	public void moverParaFrent() {
+		if(estaEmMovimento()==false) {
+			velocidades++;
+		}
+	}
+	public void frear() {
+		if(velocidades>0) {
+			--velocidades;
+		}
+	}
+	public void parar() {
+		velocidades=0;
+	}
+	public int getVelocidade() {
+		return velocidades;
+	}
+	public int getQtdePneus() {
+		return qtdePneus;
+	}
+	protected void mover() {
+		
+	}
+}
