ed37b07dadfb083672c3b09fa5552c93db9aa913
PauloRBS <42013673+PauloRBS@users.noreply.github.com>
2018-11-14 20:58:09 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,9 @@
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 100; ++i)
+		if (i%10 == 0){
+			System.out.println(i % 10 == 0 ? i + " - Multiplo de 10" : i);
+		}
+	}
+}
