889b4f75d33ca44e378e83cd439a81ee063336bb
fehgabriel <42880614+fehgabriel@users.noreply.github.com>
2018-11-14 20:58:09 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,16 @@
+
+
+public class LDP7253 {
+public static void main(String[] args){
+	
+	for(int i=0;i < 100; i++){
+	 if( i%10 == 0){
+		 System.out.println("Múltiplo de 10");
+		 
+	 }
+		
+		
+	}
+	
+}
+}
