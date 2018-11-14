f36c8f65ab36a07299bb2404d5337aea0c0a0170
fehgabriel <42880614+fehgabriel@users.noreply.github.com>
2018-11-13 23:56:12 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,21 @@
+public class Carro extends Automovel{
+	
+	public Carro(){
+		qtdePneus = 4;
+	}
+
+	public void moverParaTras() {
+		// TODO Auto-generated method stub
+		frear();
+		parar();
+		engatarMarchaARe();
+		 
+		
+	}
+	
+	protected void engatarMarchaARe(){
+		velocidade++;
+		
+	}
+
+}
