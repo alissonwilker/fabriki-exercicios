ee573e1f2ddb069b740d0ab90c2bf69859bd86be
Piterns <42013680+Piterns@users.noreply.github.com>
2018-11-21 19:50:13 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,12 @@
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		int mult;
+		for(mult=0;mult<=100;mult++){
+			if(mult % 10==0){
+				System.out.println("'Multiplo de 10'");
+			}
+			
+		}
+	}
+}
