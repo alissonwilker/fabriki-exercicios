177ae8052aa26adf52c0ff3eaa747209513579f3
michelclemer <42013634+michelclemer@users.noreply.github.com>
2018-11-21 20:57:04 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,13 @@
+public class PessoaJuridica extends Cliente {
+    private String cnpj;
+
+    
+    public PessoaJuridica(String nome, String cnpj){
+      super(nome);
+  }
+    
+    public String getCnpj(){
+        return cnpj;
+    }
+    
+}
