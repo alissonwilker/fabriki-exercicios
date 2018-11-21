12d0daf575465433ac9395702eb36e7a72126fc0
nery1000 <30377461+nery1000@users.noreply.github.com>
2018-11-21 21:40:04 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+
+public abstract class Cliente {
+
+	private String nome;
+
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome(String nome) {
+		return nome;
+
+	}
+}
