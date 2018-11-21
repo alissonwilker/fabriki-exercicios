36343b003d4ef515281cdf21304545c5a7643399
Neander <42013621+neanderlink2@users.noreply.github.com>
2018-11-21 20:33:37 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,44 @@
+public class ContaBancaria {
+    private int saldo;
+    private Cliente cliente;
+    private int id;
+    private static int contador = 0;
+    
+    public ContaBancaria (Cliente cliente) {
+    	contador++;
+        id = contador;        
+        this.cliente = cliente;
+    }
+    
+    public Cliente getCliente(){
+        return cliente;
+    }
+    
+    public int getSaldo () {
+        return saldo;
+    }
+    
+    public int depositar(int valor) {
+        
+        if (valor>0) {
+            saldo += valor;
+        }
+        
+        return saldo;
+    }
+    
+    public int sacar(int valor) {
+        
+        if (valor <= saldo && valor > 0) {
+            saldo -= valor;
+        }
+        
+        return saldo;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    
+}
