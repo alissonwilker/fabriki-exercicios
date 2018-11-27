7657c1b7903d26211ee638aa2ab177311094fdec
CarlosAndreBarbosadaSilva <42880530+CarlosAndreBarbosadaSilva@users.noreply.github.com>
2018-11-27 20:56:14 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,21 @@
+public class PessoaFisica extends Cliente {
+	private String cpf;
+	
+
+	public PessoaFisica(String string, String string2) {
+		// TODO Auto-generated constructor stub
+		cpf = string;
+	}
+
+	public String getCpf() {
+		return getCpf();
+	}
+
+
+	
+
+	}
+
+
+	
+
