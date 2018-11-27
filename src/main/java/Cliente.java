7de5f44d9ef1d1279d9ac23aca9ff4cb491d713e
maycobrito <42880612+maycobrito@users.noreply.github.com>
2018-11-27 10:28:02 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,16 @@
+public abstract class Cliente {
+	private String nome;
+	
+	protected Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	
+	
+	
+}
