42a8c6a9a67378516988abde5ba7934b56a50cef
CarlosAndreBarbosadaSilva <42880530+CarlosAndreBarbosadaSilva@users.noreply.github.com>
2018-11-27 20:56:12 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,15 @@
+public class PessoaJuridica extends Cliente {
+	private String cnpj;
+
+	public PessoaJuridica(String string, String string2) {
+		// TODO Auto-generated constructor stub
+		cnpj= string;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+
+
+}
