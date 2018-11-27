9e6b6a8117f13c0758793cacdf875e024738b303
maycobrito <42880612+maycobrito@users.noreply.github.com>
2018-11-27 10:38:21 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,32 @@
+public class ContaBancaria extends Cliente {
+	
+	private int saldo;
+	private Cliente cliente;
+	private int id;
+	private int contador;
+	
+		
+	public ContaBancaria(Cliente cliente) {
+		super(cliente);
+		return nome;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public int getId() {
+		return id;
+	}
+		
+	public int depositar(int) {
+		return 0;
+	}
+	
+	
+}
+
