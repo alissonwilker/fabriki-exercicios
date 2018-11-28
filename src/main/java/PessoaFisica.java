4b95b0570130357374aa8531a359582256c13bcb
lucasmss <42013706+lucasmss@users.noreply.github.com>
2018-11-28 19:07:42 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,14 @@
+
+public class PessoaFisica extends Cliente {
+	private String cpf;
+	
+	public PessoaFisica(String string, String string2) {
+		// TODO Auto-generated constructor stub
+		Cliente();
+		getCpf();
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+}
