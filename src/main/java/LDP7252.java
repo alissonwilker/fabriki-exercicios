b69b8da35a18d939f466ef241d479f6c5d43bb46
FilipeTSI-BSB <42013700+FilipeTSI-BSB@users.noreply.github.com>
2018-11-28 21:14:41 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,20 @@
+import java.io.PrintStream;
+
+public class LDP7252 {
+
+private static PrintStream saida = System.out;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	int g=1;
+
+	for(int i=0;i<64;i++) {
+	
+		g=g+(g*2);
+}
+	saida.println("totalGraos = "+g);
+	}
+
+}
