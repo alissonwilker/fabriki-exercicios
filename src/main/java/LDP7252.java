a9b856d3850131296dc93f548f15a182ef2c20b9
LucBarros <42049529+LucBarros@users.noreply.github.com>
2018-11-28 20:03:55 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,15 @@
+
+public class LDP7252 {
+	public static void main (String[]args) {
+		int quadrados = 0;
+		int graos = 1;
+		
+		while (quadrados <= 64){
+			graos = graos + (graos*2);
+			
+			quadrados ++;
+		}
+		System.out.println("totalGraos = " + graos);
+	}
+}
+
