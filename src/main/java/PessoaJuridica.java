430556e4d5b888c618f0c9c8c28f2da4f1c7c2a9
lucasmss <42013706+lucasmss@users.noreply.github.com>
2018-11-28 19:07:41 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,14 @@
+
+public class PessoaJuridica extends Cliente {
+	private String cnpj;
+
+	public PessoaJuridica(String string, String string2) {
+		// TODO Auto-generated constructor stub
+		Cliente();
+		getCnpj();
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+}
