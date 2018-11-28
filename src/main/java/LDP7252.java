2271ed64f0033fe905741b3efce2b174fc2c7531
CarlosEduLima <42013761+CarlosEduLima@users.noreply.github.com>
2018-11-28 20:37:25 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,13 @@
+public class LDP7252 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total = 0;
+		for (int i = 0; i <= 63; i++) {
+			total += Math.pow(i, 2);
+		}
+
+		System.out.println("totalGraos = " + total);
+	}
+
+}
