dbe43d6994cdad944a4de491640c74ff0b1a10fe
lucasmss <42013706+lucasmss@users.noreply.github.com>
2018-12-03 18:32:56 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,19 @@
+
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int calculo = 0;
+		
+		for(int i = 0; i < 100; i ++) {
+			
+		calculo = (i % 10);
+		
+		if(calculo == 0) {
+			System.out.println("Multiplo de 10");
+		}
+			
+		}
+	}
+
+}
