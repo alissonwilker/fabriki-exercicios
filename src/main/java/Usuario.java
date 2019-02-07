2522d03e2169eb59bc6ddf289b4baaee610361a8
Alisson Wilker <alissonwilker@gmail.com>
2019-02-06 22:15:07 -0200

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,46 @@
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries(value = { @NamedQuery(name = "Usuario.getAll", query = "SELECT u FROM Usuario u") })
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Integer id;
+
+    private String nome;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario {" + "id=" + id + ", nome='" + nome + '\'' + '}';
+    }
+}
