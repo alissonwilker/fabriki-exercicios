9d36670cea173f8fcdcc5d239eafc7a16ceb619e
philipp3sousa <47615019+philipp3sousa@users.noreply.github.com>
2019-02-25 19:46:24 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,31 @@
+public class Calculadora {
+
+	public int soma(int x, int y) {
+		return x + y;
+	}
+	public int subtracao(int x, int y) {
+		return x - y;
+	}
+
+	public int multiplicacao(int x, int y) {
+		return x * y ;
+	}
+	public double divisao(int x, int y) {
+		if (x != 0 && y != 0  ) {
+			
+			return x / y;
+		}else {
+			throw new IllegalArgumentException();
+		}
+			
+	}
+	public double raizQuadrada(double x) {
+		return Math.sqrt(x);
+	}
+
+	public double potencia(double x, double y) {
+		return Math.pow(x, y);
+		
+	}
+
+}
