812477be1e9d0ad97f4313ae5636d37e76f30553
welisson123 <47836568+welisson123@users.noreply.github.com>
2019-03-11 21:39:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aluno
+ */
+public class LDP7141 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       int peso = Integer.parseInt(input.nextLine());
+       int exedente = 0;
+       int multa = 0;
+        if (peso > 50) {
+            exedente = peso - 50;
+            multa = exedente * 4;
+        }
+        System.out.println("excedente = " + exedente);
+         System.out.println("multa = " + multa);
+               
+       
+    }
+    
+}
