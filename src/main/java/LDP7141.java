6c0f2a7fda2b6b14e6d9300757a9c7fed04b888a
raimundojorgemoreira <38284541+raimundojorgemoreira@users.noreply.github.com>
2019-03-11 21:44:25 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class LDP7141 {
+	public static void main(String[] args) {
+		int peso;
+		Scanner s = new Scanner(System.in);
+		System.out.println(" Informe o peso do pescado");
+		peso  = s.nextInt();
+		if(peso>50) {
+			System.out.println("excedente = "+(peso-50));
+			System.out.println("multa = "+(peso-50)*4);
+		}
+		else {
+			System.out.println("excedente = 0");
+			System.out.println("multa = 0");
+		}
+	}
+
+}
