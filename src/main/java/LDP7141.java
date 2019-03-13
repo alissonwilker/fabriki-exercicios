a7c46d3cdc8737164ba5b7d2e9783bcdc54ab366
Carlos Eduardo de Oliveira <kaduroga@gmail.com>
2019-03-13 17:49:33 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,27 @@
+public class LDP7141{
+
+	public static void main(String[] args) {
+		int e = 4;
+		int m;
+
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite o peso do peixe:");
+		int p = teclado.nextInt();
+		System.out.println("O peso é :" + p + "kilos");
+		if (p > 50) {
+			p = p - 50;
+			p = p * e;
+			e = p;
+			m = e;
+
+		} else {
+			p = p;
+			e = 0;
+			m = 0;
+		}
+
+		System.out.println("exesso é:" + e + "kilos" + "\n" + "A multa é: R$ "
+				+ m);
+	}
+
+}
