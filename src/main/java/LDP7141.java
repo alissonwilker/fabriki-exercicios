3dd605227945df688fff353da75a84c21b531d3c
Carlos Eduardo de Oliveira <kaduroga@gmail.com>
2019-03-13 23:53:46 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,22 @@
+import java.io.PrinStream;
+import java.util.Scanner;
+
+public class LDP7141 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+       int ex = 0;
+       int mul = 0;
+      int peso = sc.nextInt();
+        if (peso > 50) {
+              ex = peso - 50;
+             mul = ex * 4;
+        }
+         System.out.println("exedente = " + ex);
+          System.out.println("multa = " + mul);
+        }
+       }
+        
