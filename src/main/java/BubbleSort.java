29e301559fbccee76f3dac26b9c09ab12a92f5de
davirodrigues1 <47836631+davirodrigues1@users.noreply.github.com>
2019-03-13 21:23:35 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class BubbleSort {
+
+	private List<Integer> lista;
+
+	public BubbleSort(List<Integer> lista) {
+		this.lista = lista;
+	}
+
+	public List<Integer> getListaOrdenada() {
+		this.ordena();
+		return lista;
+	}
+
+	private void ordena() {
+            troca = 1;
+            while (troca == 1)
+            {
+            troca = 0;
+            for (i = 0; i <= 3; i++)
+            {
+            if (lista[i] > lista[i + 1])
+            {
+            troca = 1;
+            
+        
+	}
+
+	private void troca(int i, int j) {
+	}
+}
