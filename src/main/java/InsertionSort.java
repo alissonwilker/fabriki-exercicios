605e1f840515c74a3a8e94708c6020a2d95bad8e
kennedyoliveiraa <47994928+kennedyoliveiraa@users.noreply.github.com>
2019-03-22 22:41:57 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import static javafx.beans.binding.Bindings.and;
+
+public class InsertionSort {
+	
+	private List<Integer> lista;
+
+	public InsertionSort(List<Integer> lista) {
+		this.lista = lista;
+	}
+
+	public List<Integer> getListaOrdenada() {
+		this.ordena();
+		return lista;
+	}
+
+	private void ordena() {
+            for (int i = 1; i < this.lista.size(); i++) {
+                int eleito = valor(i);
+                int j = i; 
+            while(j>0 and valor[j-1]>eleito)}
+            int j = 0;
+            int valor[j]= int valor = null[
+j-1];
+            j--;
+            int eleito;
+            valor[j]= eleito;
+                }
+
+    private int valor(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+            }
