346a95e6673ca79d85e7fe5e3040dfecbab40953
paulaaraujoferraz <47615125+paulaaraujoferraz@users.noreply.github.com>
2019-03-22 23:39:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class InsertionSort {
+	
+	private List<Integer> lista;
+
+	public InsertionSort(List<Integer> lista) {
+           {
+		this.lista = lista;
+	}
+
+    public List<Integer> getListaOrdenada() {
+            
+		this.ordena();
+		return lista;
+	}
+
+	private void ordena() {
+         int y, i;
+         for (int j=1;j<lista.size();j++){
+             
+             x=lista.get(j);
+             i=j-1;
+             while(i>=0 && lista.get(i) >y){
+                 lista.set(i+1, lista.get(i));
+                 i=i-1;
+             }
+             lista.set(i+1, y);
+         }
+        }
+}
+
+   
