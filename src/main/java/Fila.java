25b49d1e97e8a4d600f0adefc04fb94f79ac5eee
Igor Alexander Barbosa Pereira <igor.pereira@sempreceub.com>
2019-03-25 20:29:29 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fila.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fila {
+	
+	private List<Elemento> fila;
+	
+	/**
+	 * ConstrÃ³i uma fila.
+	 */
+	public Fila() {
+		this.fila = new ArrayList<Elemento>();
+	}
+
+	/**
+	 * Retorna o tamanho da fila.
+	 * @return o tamanho da fila.
+	 */
+	public Integer getTamanho() {
+		return this.fila.size();
+	}
+
+	/**
+	 * Adiciona um elemento na fila.
+	 * @param elemento o elemento adicionado.
+	 */
+	public void entrarNaFila(Elemento elemento) {
+		this.fila.add(elemento);
+	}
+
+	/**
+	 * Retira um elemento da fila.
+	 * @return o elemento retirado.
+	 * @throws Exception exceÃ§Ã£o levantada caso a fila esteja vazia.
+	 */
+	public Elemento sairDaFila() throws Exception {
+		Elemento elemento = this.fila.get(0);
+		this.fila.remove(0);
+		return elemento;
+	}
+
+	/**
+	 * Retorna a posiÃ§Ã£o do elemento na fila. 
+	 * @param elemento o elemento.
+	 * @return a posiÃ§Ã£o do elemento na fila.
+	 */
+	public Integer getPosicaoNaFila(Elemento elemento) {
+		return this.fila.indexOf(elemento);
+	}
+
+	/**
+	 * Retorna o elemento da posiÃ§Ã£o especificada.
+	 * @param posicao a posiÃ§Ã£o especificada.
+	 * @return o elemento da posiÃ§Ã£o especificada.
+	 * @throws IndexOutOfBoundsException exceÃ§Ã£o lanÃ§ada caso a posiÃ§Ã£o seja < 0 ou >= tamanho da fila.
+	 */
+	public Elemento getElementoDaPosicao(Integer posicao) throws IndexOutOfBoundsException {
+		return this.fila.get(posicao);
+	}
+}
