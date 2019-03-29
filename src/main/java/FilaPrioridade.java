650970cfcc26bb904421d66c893402aa85f29832
paulaaraujoferraz <47615125+paulaaraujoferraz@users.noreply.github.com>
2019-03-29 14:49:01 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/FilaPrioridade.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilaPrioridade {
+
+	/**
+	 * ConstrÃ³i uma fila.
+	 */
+	private List<Elemento> filaprioridade;
+ 	public FilaPrioridade() {
+		this.filaprioridade = new ArrayList <Elemento> ();
+ 	}
+	
+	/**
+	 * Retorna o tamanho da fila.
+	 * @return o tamanho da fila.
+	 */
+	public Integer getTamanho() {
+		return this.filaprioridade.size();
+	}
+
+	/**
+	 * Adiciona um elemento na fila de acordo com a sua prioridade. 
+	 * Quanto MENOR a prioridade do elemento, mais ele deve ser colocado no inÃ­cio da fila.
+	 * Se a prioridade for igual, a preferÃªncia Ã© do elemento mais antigo na fila.
+	 * @param elemento o elemento adicionado.
+	 */
+	public void entrarNaFila(Elemento elemento ) {
+		this.filaprioridade.add(elemento);
+			
+
+	}
+
+	/**
+	 * Retira um elemento da fila.
+	 * @return o elemento retirado.
+	 * @throws Exception exceÃ§Ã£o levantada caso a fila esteja vazia.
+	 */
+	public Elemento sairDaFila() throws Exception {
+		if(getTamanho() != 0) {
+			return this.filaprioridade.remove(0);
+		}else {
+			throw new Exception("fila vazia.");
+		}
+	}
+
+	/**
+	 * Retorna a posiÃ§Ã£o do elemento na fila. 
+	 * @param elemento o elemento.
+	 * @return a posiÃ§Ã£o do elemento na fila.
+	 */
+	public Integer getPosicaoNaFila(Elemento elemento) {
+		return this.filaprioridade.indexOf(elemento);
+
+	}
+
+	/**
+	 * Retorna o elemento da posiÃ§Ã£o especificada.
+	 * @param posicao a posiÃ§Ã£o especificada.
+	 * @return o elemento da posiÃ§Ã£o especificada.
+	 * @throws IndexOutOfBoundsException exceÃ§Ã£o lanÃ§ada caso a posiÃ§Ã£o seja < 0 ou >= tamanho da fila.
+	 */
+	public Elemento getElementoDaPosicao(Integer posicao) throws IndexOutOfBoundsException {
+		if(posicao < 0 || posicao >= getTamanho()) {
+			throw new IndexOutOfBoundsException();
+			
+		}
+		return this.filaprioridade.get(posicao);
+		
+	}
+}
