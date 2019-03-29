a0ac2a6bd9ee0c816a7a2e7f0f96b4f89aa8d510
PatrickMarlon <38267623+PatrickMarlon@users.noreply.github.com>
2019-03-29 20:12:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/FilaPrioridade.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilaPrioridade {
+
+    private List<Elemento> fila;
+
+    /**
+     * ConstrÃ³i uma fila.
+     */
+    public FilaPrioridade() {
+        this.fila = new ArrayList<Elemento>();
+    }
+
+    /**
+     * Retorna o tamanho da fila.
+     *
+     * @return o tamanho da fila.
+     */
+    public Integer getTamanho() {
+        return this.fila.size();
+    }
+
+    /**
+     * Adiciona um elemento na fila de acordo com a sua prioridade. Quanto MENOR
+     * a prioridade do elemento, mais ele deve ser colocado no inÃ­cio da fila.
+     * Se a prioridade for igual, a preferÃªncia Ã© do elemento mais antigo na
+     * fila.
+     *
+     * @param elemento o elemento adicionado.
+     */
+    public void entrarNaFila(Elemento elemento) {
+        if(getTamanho() == 0) this.fila.add(elemento);
+        for(int i = 0 ; i < getTamanho() ; i++){
+            if(elemento.getPrioridade() < this.fila.get(i).getPrioridade()) this.fila.add(i, elemento);
+            break;
+        }
+        if(this.fila.contains(elemento) == false){
+            this.fila.add(elemento);
+        }
+    }
+
+    /**
+     * Retira um elemento da fila.
+     *
+     * @return o elemento retirado.
+     * @throws Exception exceÃ§Ã£o levantada caso a fila esteja vazia.
+     */
+    public Elemento sairDaFila() throws Exception {
+        if (getTamanho() != 0) {
+            return this.fila.remove(0);
+        } else {
+            throw new Exception("fila vazia.");
+        }
+    }
+
+    /**
+     * Retorna a posiÃ§Ã£o do elemento na fila.
+     *
+     * @param elemento o elemento.
+     * @return a posiÃ§Ã£o do elemento na fila.
+     */
+    public Integer getPosicaoNaFila(Elemento elemento) {
+        return this.fila.indexOf(elemento);
+    }
+
+    /**
+     * Retorna o elemento da posiÃ§Ã£o especificada.
+     *
+     * @param posicao a posiÃ§Ã£o especificada.
+     * @return o elemento da posiÃ§Ã£o especificada.
+     * @throws IndexOutOfBoundsException exceÃ§Ã£o lanÃ§ada caso a posiÃ§Ã£o
+     * seja < 0 ou >= tamanho da fila.
+     */
+    public Elemento getElementoDaPosicao(Integer posicao) throws IndexOutOfBoundsException {
+        if (posicao < 0 || posicao >= getTamanho()) {
+            throw new IndexOutOfBoundsException();
+
+        }
+        return this.fila.get(posicao);
+    }
+}
