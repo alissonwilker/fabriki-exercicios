5c8f78bdf793d772a67260402b0f622645e67020
Valter Sales <valter.sales@gmail.com>
2019-04-01 20:57:36 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Pilha.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pilha {
+	
+	private List<Integer> pilha;
+	
+	public Pilha() {
+		this.pilha = new ArrayList<Integer>();
+	}
+	
+	public Integer getTamanho() {
+		return this.pilha.size();
+	}
+	
+	public void entrarNaPilha(Integer elemento) {
+		this.pilha.add(elemento);
+	}
+	
+	public Integer sairDaPilha() {
+		
+		if (getTamanho() == 0) {
+			throw new IndexOutOfBoundsException("Pilha vazia");
+		}
+		
+		return this.pilha.remove(getTamanho() - 1);
+	}
+	
+	public Integer getTopo() {
+		return this.pilha.get(getTamanho() - 1);
+	}
+
+}
