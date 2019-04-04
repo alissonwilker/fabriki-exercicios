5c8aad22962d4b7fd234456c6ca17e9c8bc4339c
paulomelo45 <47836664+paulomelo45@users.noreply.github.com>
2019-04-04 15:21:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Pilha.java
@@ -0,0 +1,36 @@
+mport java.util.ArrayList;
+import java.util.List;
+
+
+public class Pilha {
+    
+    private List<Integer> pilha = new ArrayList<Integer>();
+    private int Tamanho;
+	
+	public Integer getTamanho() {
+                
+            
+                return Tamanho;
+	}
+	
+	public void entrarNaPilha(Integer elemento) {
+            
+            pilha.add(elemento);
+		
+	}
+	
+	public Integer sairDaPilha() {
+             
+              
+             
+		return 0;
+	}
+	
+	public Integer getTopo( ) {
+            
+           
+         
+		return 0;
+	}
+
+}
