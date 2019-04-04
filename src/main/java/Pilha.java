9d9c2b854885b743a42f2f799ae97905348dcf8d
davirodrigues1 <47836631+davirodrigues1@users.noreply.github.com>
2019-04-03 21:39:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Pilha.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pilha {
+
+    private List<Integer> pilha;
+    private int posicaoPilha;
+
+    public Integer getTamanho() {
+        return this.pilha.size();
+    }
+
+    public void entrarNaPilha(Integer elemento) {
+        this.pilha.add(elemento);
+
+    }
+
+    public Integer sairDaPilha() throws Exception {
+        if (getTamanho() != 0) {
+            return this.pilha.remove(0);
+        } else {
+            throw new Exception("Mano, a fila está vazia");
+        }
+    
+    }
+	
+
+    public Integer getTopo() {
+        return this.posicaoPilha = -1;
+
+    }
+
+}
