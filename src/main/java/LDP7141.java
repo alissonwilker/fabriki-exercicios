5ce3902816af77612cd0d091a7366ab0ebf27a19
Alisson Wilker <alissonwilker@gmail.com>
2019-04-08 16:20:31 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,26 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7141 {
+    private static Scanner entrada = new Scanner(System.in);
+    private static PrintStream saida = System.out;
+
+    // DECLARAÇÃO DAS VARIÁVEIS INTEIRAS peso, excedente e multa
+    private static int peso, excedente, multa;
+
+    public static void main(String[] args) {
+
+        //LEIA UM INTEIRO E ARMAZENE NA VARIÁVEL peso
+        peso = entrada.nextInt();
+    
+        //PROCESSAMENTO
+        if (peso > 50) { //SE peso FOR MAIOR DO QUE 50...
+            excedente = peso - 50; //CALCULE excedente
+            multa = excedente * 4; //CALCULE multa
+        }
+
+        //ESCREVA 'excedente = <valor de excedente>'
+        saida.println("excedente = " + excedente);
+        saida.println("multa = " + multa);
+    }
+}
