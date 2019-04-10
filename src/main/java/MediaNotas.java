86161049d99a339a6f96e511712dd79bd9826813
pedrocpu2012 <pedrolucas_ti2015@hotmail.com>
2019-04-10 20:26:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,34 @@
+public class MediaNotas {
+	static Scanner entrada = new Scanner(System.in);
+	public static void main(String[] args) {
+	
+		
+		
+		int qtd_notas;
+		//int total_soma_notas = 0;
+		double media = 0;
+				
+			 qtd_notas = entrada.nextInt();
+									
+				int notas[] = new int [qtd_notas];
+				
+				for (int i = 0; i < notas.length; i++) {
+					notas[i]=entrada.nextInt();
+					media+= notas[i];
+				}
+				
+			
+				NumberFormat formatador = NumberFormat.getInstance(new Locale("pt", "BR"));
+
+				formatador.setMinimumFractionDigits(1);
+
+				formatador.setMaximumFractionDigits(1);
+				System.out.println("media = " + formatador.format(media/notas.length));
+				for (int i = 0; i < notas.length; i++) {
+					System.out.println("nota "+(i+1)+" = "+formatador.format(notas[i]));
+				}
+				
+			}
+	}
+
+
