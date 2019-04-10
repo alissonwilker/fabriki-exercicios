8bf02ef30f69e23da2d48b9c6605e200a8be4526
gabrielacosmo <47836656+gabrielacosmo@users.noreply.github.com>
2019-04-10 20:47:39 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MediaNotas {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		//System.out.println("Digite a quantidade de alunos:");
+		int n = s.nextInt();
+		
+		int[] notas = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			notas[i] = s.nextInt();
+		}
+		
+		double soma = 0;
+		for (int i = 0; i < notas.length; i++) {
+			soma = soma +notas[i];
+		}
+		
+		double media = soma/n;
+		System.out.println("media = "+ media);
+		// System.out.println("notas "+[i]+" ="+ notas[i]);
+		
+	}
+
+}
