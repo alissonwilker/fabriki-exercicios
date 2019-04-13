e6f081cc406fc259164cbd149369caea004dca3e
paulaaraujoferraz <47615125+paulaaraujoferraz@users.noreply.github.com>
2019-04-12 21:13:09 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BuscaSequencial.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuscaSequencial {
+
+	private List<Object> lista= new ArrayList<Object>();
+
+	public BuscaSequencial(List<Object> lista) {
+		this.lista = lista;
+	}
+
+	public List<Object> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Object> lista) {
+		this.lista = lista;
+	}
+
+	public int busca(Object o) {
+		for(int i=0; i <this.lista.size(); i++)
+			if(this.lista.get(i).equals(o)) {
+				return this.lista.indexOf(o);
+			
+	}
+		return -1;
+	}
+
+	public int buscaRecursiva (Object o, int indice) {
+		if(this.lista.get(indice).equals(o)) {
+		return indice;		
+	}else if(indice == this.lista.size() -1) {
+		return -1;
+	}else {
+		return buscaRecursiva(o,++indice);
+	}
+	}
+
+}
+	
