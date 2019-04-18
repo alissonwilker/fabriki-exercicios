429caeb435579fcdc1bfde6074b7073350e0bf9e
raimundojorgemoreira <38284541+raimundojorgemoreira@users.noreply.github.com>
2019-04-18 18:16:01 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LDP7251 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		int maior=0;
+		Scanner  S = new Scanner(System.in);
+		
+		while(num != 0) {
+			//System.out.println("Digite um numero inteiro ou numero zero para sair");
+			//for(int i=0; i<3; i++)
+			//System.out.println("Digite um numero inteiro ou numero zero para sair");
+		num = S.nextInt();
+		if(num > maior) {
+			maior = num;
+			num++;
+			
+			
+		}
+		//else if(num< maior) {
+			//System.out.println(""+maior);
+			
+	}
+		System.out.println("maior = "+maior);	
+	}
+
+}
