23597de46d2b1475be02f51601c86b0ccf3be481
33vitor <47836495+33vitor@users.noreply.github.com>
2019-04-22 20:12:02 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+public class LDP7252 {
+    public static void main(String[] args) {
+        BigInteger x=new BigInteger("1"),y=new BigInteger("0"),dois=new BigInteger("2"),um=new BigInteger("-1");
+        for(int i=1;i<64;i++){
+            x=(x.multiply(dois));
+            y=(x.add(x));
+        }
+        y=(y.add(um));
+        System.out.println("totalGraos= "+y);
+        
+    }
+    
+}
