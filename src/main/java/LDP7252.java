64f330c64226f3b41e848e5b419699fc353f9b7f
Kissinho <47836524+Kissinho@users.noreply.github.com>
2019-04-22 19:54:11 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,12 @@
+
+public class LDP7252 {
+	public static void main(String[] args) {
+		long quadrado=1,acumulador=0;
+		for(int i=1;i<=64;i++) {
+			quadrado = quadrado*2;
+			acumulador += quadrado;
+			
+		}
+		System.out.println("totalGraos = "+acumulador);
+	}
+}
