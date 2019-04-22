f64c692a58424cd699ed0d94df6771ee6bcd9fec
pqpsamuk <38284164+pqpsamuk@users.noreply.github.com>
2019-04-22 20:12:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;
+public class LDP7252{
+    public static void main(String[] args) {
+        BigDecimal cont = new BigDecimal(2);
+        BigDecimal graos = BigDecimal.ZERO;  
+        for (int i = 0; i <= 63; i++) {  
+            graos = graos.add(cont.pow(i));  
+        }  
+        System.out.println("totalGraos = "+graos);  
+    }  
+}
