59995ec5616908b083a0ded9481127c20100ed1a
gabrielacosmo <47836656+gabrielacosmo@users.noreply.github.com>
2019-04-22 21:12:31 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,25 @@
+import java.math.BigInteger;
+
+public class LDP7252 {
+
+	public static void main(String[] args) {
+		
+		
+		BigInteger soma_total = new BigInteger ("0");
+		int quadro;
+		BigInteger graos = new BigInteger ("0");
+	    BigInteger base = new BigInteger ("2");
+		
+		for (quadro=0; quadro < 64; quadro++ ) {
+		    	
+	    	graos = base.pow(quadro); // math.pow
+	    	
+	    	soma_total = soma_total.add(graos); // soma_total += graos;
+	    	
+	    }	
+	      
+		System.out.println("totalGraos = "+soma_total);
+	      
+	      	
+	    }
+	}
