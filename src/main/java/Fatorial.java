f01fc3c3afacd963e0ad39c67c556adfb31a4d2d
33vitor <47836495+33vitor@users.noreply.github.com>
2019-04-29 21:14:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Fatorial {
+    public static void main(String[] args) {
+        BigInteger c=new BigInteger("1");
+        BigInteger j=new BigInteger("1");
+        BigInteger UM=new BigInteger("1");
+        int num;
+       Scanner leia=new Scanner(System.in);
+       num=leia.nextInt();
+        if (num==0){
+            System.out.println(1);
+       }
+        else{ 
+       for(int i=1;i<=num;i++){
+          c =c.multiply(j);
+          j=j.add(UM);
+       }
+        System.out.println(c);
+    }
+    }
+}
