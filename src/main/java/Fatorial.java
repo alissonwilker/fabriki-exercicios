3100cc9d77300fc8987712cbe221c08bdbc4175e
hugocesarhc <47836510+hugocesarhc@users.noreply.github.com>
2019-04-29 21:32:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+	    
+	    Scanner entrada = new Scanner (System.in);
+	    
+	    long fatorial;
+	    long numero = entrada.nextInt();
+	    
+	        	    
+	    for (fatorial = 1; numero > 1; numero = numero -1) {
+	    fatorial = fatorial * numero;
+	    
+	    
+		} System.out.println(fatorial);
+	   
+	    
+	}
+}
