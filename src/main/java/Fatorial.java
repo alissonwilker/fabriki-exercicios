9cf5832117bfca64fd3ac5b6fa403a158347e813
pedrocpu2012 <pedrolucas_ti2015@hotmail.com>
2019-05-01 22:48:34 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Fatorial {
+	public static void main(String[]args) {
+		Scanner entrada = new Scanner(System.in);
+
+		BigInteger totalfat = new BigInteger("1");
+		BigInteger ref = new BigInteger("1");
+		BigInteger acumulador = new BigInteger("1");
+		
+		int valor = 0;
+		valor = entrada.nextInt();
+
+		for (int cont = 1; cont <= valor; cont++) {
+			totalfat = totalfat.multiply(acumulador);
+			acumulador = acumulador.add(ref);
+		}
+		
+		System.out.println(totalfat);
+	}
+}
