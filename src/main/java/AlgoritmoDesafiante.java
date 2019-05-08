29e4f7747238c725f2db94db4e5bc9a136fce6d2
Alisson Wilker <alissonwilker@gmail.com>
2019-05-08 02:44:09 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/AlgoritmoDesafiante.java
@@ -0,0 +1,12 @@
+import xadrez.Xadrez;
+import xadrez.alg.AlgoritmoXadrez;
+
+public class AlgoritmoDesafiante implements AlgoritmoXadrez {
+
+    @Override
+    public String jogar(Xadrez xadrez) {
+        return "G8F6";
+    }
+
+
+}
