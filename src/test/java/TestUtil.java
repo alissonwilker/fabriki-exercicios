5209753983283a0bcc2ec17b3cd8caaab74f383c
Alisson Wilker <alissonwilker@gmail.com>
2019-05-11 00:27:57 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+import xadrez.Xadrez;
+
+public class TestUtil {
+    public static List<String> procurarMovimentosXequeMateEmUmLance(Xadrez xadrez) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
