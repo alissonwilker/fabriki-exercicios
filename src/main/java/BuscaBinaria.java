972c089182dbd9a99c93fedbe233f345b4489452
gtoledoribeiro <44214821+gtoledoribeiro@users.noreply.github.com>
2019-05-15 02:16:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BuscaBinaria.java
@@ -0,0 +1,35 @@
+
+import java.util.List;
+
+public class BuscaBinaria {
+
+	private List<Integer> lista;
+
+	public BuscaBinaria(List<Integer> lista) {
+		this.lista = lista;
+	}
+
+	public List<Integer> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Integer> lista) {
+		this.lista = lista;
+	}
+
+	public int buscar(Integer valor) {
+
+	}
+
+	private int buscaBinaria(Integer valor, int inicio, int fim) {
+		
+	}
+
+	private void ordena() {
+
+	}
+
+	private void troca(int i, int j) {
+
+	}
+}
