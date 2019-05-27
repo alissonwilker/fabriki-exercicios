8cc446e3eced573c8910fcf6f6f4f1c723cbfd95
welisson123 <47836568+welisson123@users.noreply.github.com>
2019-05-27 20:10:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Bicicleta extends Veiculo {
+    public Bicicleta(){
+        super.qtdePneus = 2;
+    }
+}
