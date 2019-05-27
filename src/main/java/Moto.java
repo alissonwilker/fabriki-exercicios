c7d9512ee4f1625c428bd2cd7dbfc3acdb9dec70
welisson123 <47836568+welisson123@users.noreply.github.com>
2019-05-27 20:10:27 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Moto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Moto extends Automovel{
+    public Moto(){
+       super.qtdePneus = 2; 
+    }
+
+}   
