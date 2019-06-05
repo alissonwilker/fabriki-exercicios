90120c3404fe8c7d7d40cc42a0f0b8b1e680561b
Endryl Francelino de Souza <32441791+enfraso@users.noreply.github.com>
2019-06-05 19:27:47 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Cliente {
+	
+	private String nome;
+
+	protected Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+}
