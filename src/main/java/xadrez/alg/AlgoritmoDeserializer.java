a02c5d9764868351a93426431c24bcb61f8dfe22
Alisson Wilker <alissonwilker@gmail.com>
2019-06-23 17:16:15 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/xadrez/alg/AlgoritmoDeserializer.java
@@ -0,0 +1,37 @@
+package xadrez.alg;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+public class AlgoritmoDeserializer extends StdDeserializer<AlgoritmoXadrez> {
+    private static final long serialVersionUID = 1L;
+
+    public AlgoritmoDeserializer() {
+        this(null);
+    }
+
+    public AlgoritmoDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public AlgoritmoXadrez deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        JsonNode node = jp.getCodec().readTree(jp);
+        int profundidade = node.get("profundidade").asInt();
+
+        AlgoritmoXadrez algoritmo = null;
+        if (profundidade > 0) {
+            algoritmo = new AlgoritmoMinimax();
+        } else {
+            algoritmo = new AlgoritmoAleatorioMaisXequeMateEmUmLance();
+        }
+        algoritmo.setProfundidade(profundidade);
+        return algoritmo;
+    }
+
+}
