86b7811e29da7c72db374bdf1ec4624cdeb8c912
Alisson Wilker <alissonwilker@gmail.com>
2019-06-23 17:11:37 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/xadrez/pecas/Peao.java
@@ -0,0 +1,28 @@
+package xadrez.pecas;
+
+import xadrez.movimentos.MovimentoAvancaDuas;
+import xadrez.movimentos.MovimentoAvancaUma;
+import xadrez.movimentos.MovimentoCapturaDiagonal;
+import xadrez.movimentos.MovimentoEnPassant;
+
+public class Peao extends Peca {
+    public Peao(Cor cor, String idCasa) {
+        super(Tipo.Peao, cor, idCasa);
+        
+        adicionar(new MovimentoAvancaUma());
+        adicionar(new MovimentoAvancaDuas());
+        adicionar(new MovimentoCapturaDiagonal()); 
+        adicionar(new MovimentoEnPassant());
+    }
+    
+    @Override
+    public String getFenCode() {
+        if (cor == Cor.BRANCA) {
+            return "P";
+        } else {
+            return "p";
+        }
+    }
+
+
+}
