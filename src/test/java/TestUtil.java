cc8debdf6a0d1b86653181223d73ccd6deb0f840
hugocesarhc <47836510+hugocesarhc@users.noreply.github.com>
2019-06-24 22:09:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import xadrez.Xadrez;
+import xadrez.Xadrez.Situacao;
+import xadrez.pecas.Peca;
+
+public class TestUtil {
+    public static List<String> procurarMovimentosXequeMateEmUmLance(Xadrez xadrez) {
+    	List<Peca> pecasDoTurno = xadrez.recuperarPecasDoTurno();
+    	
+    	ArrayList<String> Final = new ArrayList();
+    	for (Peca elementopeca: pecasDoTurno) {
+    		List <String> Totalmovimentos = elementopeca.getMovimentosValidos(xadrez);
+    	for (String movimento: Totalmovimentos) {
+    		
+    		Situacao resultado = xadrez.simularJogada(movimento);
+    		boolean turno = xadrez.isTurnoBrancas();
+    		
+    		if (resultado == Situacao.Pretas_em_xeque && turno == true) {
+    			Final.add(movimento);
+    		} else if(resultado == Situacao.Brancas_em_xeque && turno == false){
+    			Final.add(movimento);
+    		}
+    	}
+    	
+    		
+    }
+    	
+    	return Final;
+    }
+
+}
