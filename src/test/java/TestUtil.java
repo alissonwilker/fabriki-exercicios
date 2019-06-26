120e811223a4fc79381626a96ac861c7781d3a75
Brunossleo <47836548+Brunossleo@users.noreply.github.com>
2019-06-26 20:07:11 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/test/java/TestUtil.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import xadrez.Xadrez;
+import xadrez.Xadrez.Situacao;
+import xadrez.pecas.Peca;
+
+public class TestUtil {
+	public static List<String> procurarMovimentosXequeMateEmUmLance(Xadrez xadrez) {
+		List<Peca> pecas = xadrez.recuperarPecasDoTurno();
+		
+		ArrayList<String> XequeMate = new ArrayList<String>();
+
+		for (Peca h : pecas) {
+			List<String> Moverpeca = h.getMovimentosValidos(xadrez);
+
+			for (String jogada : Moverpeca) {
+
+				Situacao resultado = xadrez.simularJogada(jogada);
+				boolean turno = xadrez.isTurnoBrancas();
+
+				if (resultado == Situacao.Brancas_em_xeque_mate && turno == false) {
+					XequeMate.add(jogada);
+				} else if (resultado == Situacao.Pretas_em_xeque_mate && turno == true) {
+
+					XequeMate.add(jogada);
+				}
+			}
+		}
+		return XequeMate;
+
+	}
+}
