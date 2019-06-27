eca1f4924973645beef17179d01935e2ff3f31f7
raimundojorgemoreira <38284541+raimundojorgemoreira@users.noreply.github.com>
2019-06-26 21:37:24 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/AlgoritmoDesafiante.java
@@ -0,0 +1,71 @@
+
+import java.nio.charset.MalformedInputException;
+import java.util.ArrayList;
+import java.util.List;
+
+import xadrez.Xadrez;
+import xadrez.Xadrez.Situacao;
+import xadrez.alg.AlgoritmoXadrez;
+import xadrez.pecas.Peca;
+
+public class AlgoritmoDesafiante implements AlgoritmoXadrez {
+
+	@Override
+	public String procurarMelhorMovimento(Xadrez xadrez) {
+
+		List<Peca> pecasDoTurno = xadrez.recuperarPecasDoTurno();
+        List<String> VerificarXequemate = procurarMovimentosXequeMateEmUmLance(xadrez);
+		
+		
+		
+		
+		String movimentoValido = null;
+		while (movimentoValido == null) {
+			
+			if(VerificarXequemate != null && !VerificarXequemate.isEmpty()) {
+				movimentoValido = VerificarXequemate.get(0);
+			} else {
+			
+			int numeroAleatorio = (int) (Math.random() * pecasDoTurno.size());
+			Peca peca = pecasDoTurno.get(numeroAleatorio);
+			List<String> movimentosValidos = peca.getMovimentosValidos(xadrez);
+			if (movimentosValidos.size() > 0) {
+				numeroAleatorio = (int) (Math.random() * movimentosValidos.size());
+				movimentoValido = movimentosValidos.get(numeroAleatorio);
+			}}
+		}
+		return movimentoValido;
+	}
+
+	
+	public static List<String> procurarMovimentosXequeMateEmUmLance(Xadrez xadrez) {
+        // TODO Auto-generated method stub
+    List<String> movimentoXeque =new ArrayList();	
+    List<Peca> ListDePecas = xadrez.recuperarPecasDoTurno();
+    	for(int i=0; i < ListDePecas.size(); i++) {
+    		List<String> movimentos = ListDePecas.get(i).getMovimentosValidos(xadrez);
+    		for(int j=0; j < movimentos.size(); j++) {
+    			Situacao situacao = xadrez.simularJogada(movimentos.get(j));
+    		 boolean TurnoBrancas = xadrez.isTurnoBrancas();
+    		 if(TurnoBrancas) {
+    			 if(situacao ==Situacao.Pretas_em_xeque_mate ) {
+    				 movimentoXeque.add(movimentos.get(j));
+    			 }
+    		 }else {
+    			 if(situacao==Situacao.Brancas_em_xeque_mate) {
+    				movimentoXeque.add(movimentos.get(j)); 
+    			 }
+    		 }
+    	}
+    	
+    }
+        return movimentoXeque;
+    }
+
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
