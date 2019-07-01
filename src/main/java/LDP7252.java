90f037a022a080344f83f3d100168eab8225cbdf
pedrocpu2012 <pedrolucas_ti2015@hotmail.com>
2019-06-30 22:11:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,17 @@
+import java.math.*;
+	
+	public class LDP7252 {
+	
+		public static void main(String[] args) {
+			BigInteger quadrado = new BigInteger("1"), acumulador = new BigInteger("0"), dois = new BigInteger("2"), ng = new BigInteger("-1");
+			for(int i = 1; i < 64; i++) {
+				quadrado = (quadrado.multiply(dois));
+				acumulador = (quadrado.add(quadrado));
+				
+				
+			}
+			
+			System.out.println("totalGraos = " +acumulador.add(ng));
+						
+		}
+	}
