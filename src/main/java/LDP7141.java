70430e9280a57e337f3881422abb6de5bc5c1842
deividferreiracampos <47040510+deividferreiracampos@users.noreply.github.com>
2019-08-19 21:28:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,24 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7141 {
+	private static Scanner entrada= new Scanner(System.in);
+	private static PrintStream saida= System.out;
+
+	public static void main(String[] args) {
+		int c=0;
+		int valor=entrada.nextInt();
+	  
+
+			if(valor>50) {
+				valor-=50;
+				c=valor*4;
+				System.out.println("excedente = "+valor+ "\nmulta = "+c);
+				
+				
+			}else {
+				System.out.println("excedente = 0"+"\nmulta = "+c);	
+			}
+	}
+
+}
