746daf34780f70c93ed53159d07a16d745c7bab2
Carlos Eduardo de Oliveira <kaduroga@gmail.com>
2019-09-02 20:48:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+
+class Fatorial {
+    public static void main(String[] args){
+        BigInteger numero = new BigInteger("1", 10);
+        for (int i = 1; i<=5; i++) {
+            numero = numero.multiply( BigInteger.valueOf( i ) );
+        }
+        System.out.println("O fatorial  é igual a " + numero );
+    }
+}
