e9f71b6435e8429e54df4edb0f154024ce2c221a
Rafael Oliveira <38117708+Rafa516@users.noreply.github.com>
2019-09-02 20:02:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fatorial {
+	private static Scanner entrada = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long fat,i, n;
+		
+		n = entrada.nextInt();
+		fat = 1;
+		
+		for( i = 1; i<=n; i++) {
+			fat = fat*i;
+			
+		}
+		System.out.println(fat);
+		
+	}
+
+}
+
+
