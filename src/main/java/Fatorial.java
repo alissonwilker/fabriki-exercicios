404c61edee137e7069707b20df8b116dd1170c36
Carlos Eduardo de Oliveira <kaduroga@gmail.com>
2019-09-02 20:21:29 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,18 @@
+class Fatorial {
+public static void main ( String[] args ) {
+int numero = 6;
+
+System.out.println("Cáculo Fatorial entre 0 e 25." );
+System.out.println("O fatorial de " + numero + " é: " + fatorial(numero ) );
+
+}
+
+public static int fatorial (int numero) {
+int fat = 1;
+for(int i = 1; i <= numero; i++) {
+fat*= i;
+}
+return fat;
+
+}
+}
