53af14216fec6f2938018ee80650355c791fb9b7
deividferreiracampos <47040510+deividferreiracampos@users.noreply.github.com>
2019-09-02 21:09:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.io.PrintStream;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Fatorial {
+	private static Scanner entrada= new Scanner(System.in);
+	private static PrintStream saida= System.out;
+	
+	
+	public static void main(String[] args) {
+	
+	
+		BigInteger mult=BigInteger.valueOf(1);
+		int n,i;
+		n=entrada.nextInt();
+		i=n;
+		
+		for(i= n; i>=1;i--) {
+			mult=mult.multiply(BigInteger.valueOf(i));
+		}
+		
+		saida.print(mult);
+}}
