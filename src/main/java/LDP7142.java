0996150be90ea11b503832b722fcb4d67b3f1f3b
kawaad <54145620+kawaad@users.noreply.github.com>
2019-09-06 20:29:47 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,33 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7142 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+	public static void main(String[] args) {
+		int codigo = entrada.nextInt();
+		int horasTrabalhadas = entrada.nextInt();
+		int porHoraTrabalhada = 10;
+		int porHorasExtras = 20;
+		int salarioNormal = 0;
+		int salarioExcedente = 0;
+		int salarioTotal = 0;
+		int quantHoraExtra = 0;
+
+		//quantHoraExtra = 
+		//salarioTotal = horasTrabalhadas + h;
+		salarioNormal = (horasTrabalhadas - quantHoraExtra) * porHoraTrabalhada;
+		
+		if (horasTrabalhadas > 50) {
+			quantHoraExtra = horasTrabalhadas - 50;
+		}
+		
+		
+		salarioExcedente = quantHoraExtra * porHorasExtras;
+		salarioTotal = salarioExcedente + salarioNormal; 
+		saida.println("codigo = "+ codigo +"\nsalarioExcedente = "+ salarioExcedente +"\nsalarioTotal = "+ salarioTotal);
+		
+
+	}
+
+}
