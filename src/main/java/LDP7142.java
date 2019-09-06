a838b5cb459b275d2a107a19c13e4aad2f173028
PolyanaC <51519638+PolyanaC@users.noreply.github.com>
2019-09-06 20:22:34 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,32 @@
+
+	public class LDP7142 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		int codigo = entrada.nextInt();
+		int hora = entrada.nextInt();
+		int excedente, total;
+		
+		if(hora <= 50) {
+			total = hora * 10;
+			excedente = 0;
+			
+			saida.println("codigo = "+ codigo);
+			saida.println("salarioExcedente = " + excedente);
+			saida.println("salarioTotal = " + total);
+		}else {
+			excedente = (hora - 50) * 20;
+			total = (50 * 10) + excedente; 
+			
+			saida.println("codigo = "+ codigo);
+			saida.println("salarioExcedente = " + excedente);
+			saida.println("salarioTotal = " + total);
+		}
+		
+		
+
+	}
+
+}
+	
