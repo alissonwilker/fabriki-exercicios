bba9236b5707d88e9500ffadeea470d97babf110
AdjairN <adjair.nascimento.204010@gmail.com>
2019-09-09 19:34:33 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,23 @@
+
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7144 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		int numero = entrada.nextInt();
+
+		// System.out.println("Informe um número");
+
+		if (numero % 2 == 0 && numero >= 0) {
+			saida.println("par e positivo = " + numero);
+		} else {
+			saida.println("impar e negativo = " + numero);
+		}
+
+	}
+
+}
