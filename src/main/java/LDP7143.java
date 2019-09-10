60867f1a7be7a673b2b06a47b3af954346c3ece0
Danilo Rocha <53863511+danilo-rocha1@users.noreply.github.com>
2019-09-09 21:16:30 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,28 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7143 {
+    private static final Scanner entrada = new Scanner(System.in);
+    private static final PrintStream saida = System.out;
+
+    public static void main(String[] args) {
+        int n1= entrada.nextInt();
+        int n2= entrada.nextInt();
+        int n3= entrada.nextInt();
+        int n4= entrada.nextInt(); 
+        
+        
+          if (n3*n3>=1000) {
+                
+                    saida.println("c2 = 10000");
+                }
+                else{
+                    saida.println("a = "+n1+"\nb = "+n2+"\nc = " +n3+ "\nd = " +n4);
+                    saida.println("a2 = "+n1*n1+"\nb2 = "+n2*n2+"\nc2 = " +n3*n3+ "\nd2 = " +n4*n4);
+                }
+            
+        }
+  }
+        
+        
+    
