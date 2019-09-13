49d2062d895683670e9d87d4f946128692a76cb9
brunoaguiarmazoni <51519820+brunoaguiarmazoni@users.noreply.github.com>
2019-09-13 20:54:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,20 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7253 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <=100; i++) {
+			
+			if (i%10==0) {
+				saida.println("Multiplo de 10");
+			} 
+			
+		}
+
+	}
+
+}
