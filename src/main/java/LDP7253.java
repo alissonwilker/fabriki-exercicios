2ec2af1f8c6dbe5905a547f31d380fd4b301e710
Danilo Rocha <53863511+danilo-rocha1@users.noreply.github.com>
2019-09-13 21:20:46 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,20 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7253 {
+
+	private static PrintStream saida = System.out;
+	
+	public static void main (String[]args) {
+		
+		int []contagem = new int[100];
+		
+		for(int i = 0; i <100 ; i++) {
+			contagem[i] = i+1;
+		if(contagem[i]%10 == 0) {
+			System.out.println("Multiplo de 10");
+		}
+		}
+	}
+	
+}
