27e4ec15f665b3bfcd8216d10fc4e9ae87599914
Brenda-hub <53834793+Brenda-hub@users.noreply.github.com>
2019-09-13 21:11:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+public class LDP7253 {
+	private static PrintStream saida = System.out;
+	public static void main(String[] args) {
+		int[] n = new int[100]; 
+		for (int i = 0; i < n.length; i++) {
+			if(i % 10 == 0){
+				saida.println("Multiplo de 10");
+
+			}
+		}
+	}
+
+}
