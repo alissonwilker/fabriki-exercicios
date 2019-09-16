2610ded94a5e9c2dd55544d43e3e2f72c77a0364
MrMateusFonteles <54007149+MrMateusFonteles@users.noreply.github.com>
2019-09-16 20:51:38 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,64 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7251 {
+	
+	private static Scanner entrada = new Scanner (System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	int n = entrada.nextInt();
+	int maior = n;
+	
+	if(n !=0) {
+		for(int i = 0; n != 0; i++) {
+			n = entrada.nextInt();
+			if(n >= maior && n != 0) {
+				maior = n;
+
+			}import java.io.PrintStream;
+
+public class maior_do_conjunto {
+	
+	private static Scanner entrada = new Scanner (System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	int n = entrada.nextInt();
+	int maior = n;
+	
+	if(n !=0) {
+		for(int i = 0; n != 0; i++) {
+			n = entrada.nextInt();
+			if(n >= maior && n != 0) {
+				maior = n;
+
+			}
+
+		}	
+		saida.println("maior = "+maior);
+
+	}else {
+		saida.println("maior = 0");
+	}
+
+
+}
+}
+
+		}	
+		saida.println("maior = "+maior);
+
+	}else {
+		saida.println("maior = 0");
+	}
+
+
+}
+}
