7bfaf0eaae3e3f953be65420ad386fafd7a4a98d
PolyanaC <51519638+PolyanaC@users.noreply.github.com>
2019-09-16 20:34:37 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,27 @@
+public class LDP7251 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		int n = entrada.nextInt();
+		int maior = n;
+		
+		if(n != 0) {
+			for(int i = 0; n!= 0; i++) {
+				n= entrada.nextInt();
+				if(maior <= n && n != 0) {
+					maior = n;
+				}			
+			}
+			saida.println("maior = "+ maior);
+			
+			
+		}else {
+			saida.println("maior = 0");
+			
+		}
+				
+		
+	}
+
+}
