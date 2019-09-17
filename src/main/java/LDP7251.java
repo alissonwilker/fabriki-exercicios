6b4f2f04f66eaa25374a8ba54d5fb91f42d67787
Mariana Guedes da Silva <49035807+mariguedes8@users.noreply.github.com>
2019-09-17 06:28:08 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,24 @@
+import java.io.PrintStream;
+import java.until.Scanner;
+
+public class LDP7251 {
+    
+    private static Scanner entrada = new Scanner(System.in);
+    private static PrintStream saida = System.out;
+
+    public static void main(String args[]) {
+     
+     int N=0;
+     int maior = entrada.nextInt();
+     
+    while(maior!=0) {
+     if (maior>N){ 
+        N = maior;
+    }
+}
+         saida.println("maior = "+N);
+        
+   
+     
+   
+}
