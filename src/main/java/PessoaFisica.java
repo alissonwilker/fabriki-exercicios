274e7d6d4997a34efcde79c74d46ac6196cbccfd
Lucas Barros de Araújo <42049529+LucBarros@users.noreply.github.com>
2019-09-18 21:56:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,15 @@
+public class PessoaFisica extends Cliente {
+	
+	private String CPF;
+
+	public PessoaFisica(String nome,String CPF) {
+		super(nome);
+		this.CPF = CPF;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+	
+
+}
