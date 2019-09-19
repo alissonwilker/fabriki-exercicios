a48ea847fdbb91b2ad55888b93e25a2a270e5b30
CaioNathan <42880618+CaioNathan@users.noreply.github.com>
2019-09-18 21:56:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,56 @@
+
+public class ContaBancaria {
+	private int saldo;
+	private Cliente cliente;
+	private int id;
+	private static int contador;
+	
+	
+	
+	public ContaBancaria(Cliente cliente) {
+		// TODO Auto-generated constructor stub
+		contador++;
+		id=contador;
+		this.cliente=cliente;
+	}
+
+	public Object getSaldo() {
+		
+		return saldo;
+	}
+
+	public void depositar(int valorDeposito) {
+		if(valorDeposito>0) {
+			saldo += valorDeposito;
+			}
+		else {
+			System.out.println("Valor indispoível");
+		}
+		
+		
+	}
+
+	public void sacar(int valorSaque) {
+		if (valorSaque>saldo) {
+			System.out.println("Saldo indisponivel");
+		}
+		else {
+			saldo -= valorSaque;
+			
+		}
+		
+		
+	}
+
+	public Object getCliente() {
+		
+		return cliente;
+	
+	}
+
+	public int getId() {
+		
+		return id;
+	}
+
+}
