7d61a826eabb4280a4d674a56a158d0522a7f064
CaioNathan <42880618+CaioNathan@users.noreply.github.com>
2019-09-18 21:56:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,21 @@
+
+public class PessoaFisica extends Cliente{
+	
+	private String cpf;
+
+	public PessoaFisica(String nome, String cpf) {
+
+	super(nome);
+	this.cpf=cpf;
+	}
+	
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+
+
+
+}
