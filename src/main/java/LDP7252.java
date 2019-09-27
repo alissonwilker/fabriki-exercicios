668a306e1357636e0ae123ef114b5828c95042f0
cleber87 <54191103+cleber87@users.noreply.github.com>
2019-09-27 20:23:01 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,20 @@
+import java.io.PrintStream;
+
+public class LDP7252 {
+	private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		
+		double graosCasa=1,casasTabuleiro=64;
+		int graos=1;
+
+		for(int i=1; i<casasTabuleiro;i++){
+			graosCasa = graosCasa*2;
+			graos +=graosCasa;
+		}
+
+		saida.println("totalGraos = "+graos);
+
+	}
+
+}
