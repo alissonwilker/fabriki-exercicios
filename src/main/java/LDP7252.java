4b3dad2b90a82ffd2f0401f8f667c0dd8177b885
Mariana Guedes da Silva <49035807+mariguedes8@users.noreply.github.com>
2019-09-27 20:56:01 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,26 @@
+import java.io.PrintStream;
+import java.math.BigInteger;
+
+
+public class LDP7252 {
+
+    private static final PrintStream saida = System.out;
+    public static void main(String[] args) {
+      
+        BigInteger vetor[] = new BigInteger[64];
+          
+           vetor[0] = BigInteger.valueOf(1);
+          
+        for (int i = 1; i <= 63; i++) {
+            
+            vetor[i] = (vetor[i-1].multiply(BigInteger.valueOf(2)));
+          }
+         BigInteger soma= new BigInteger("1");
+       
+         for(int i =1; i< vetor.length; i++){
+            soma = soma.add (vetor[i]);
+        }
+        
+         saida.println("totalGraos = "+soma);    
+    }
+ }
