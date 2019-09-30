2311859142167079e3a8f815c411da2b1c31b216
Marcos Vasconcellos de Andrade <vascorj2006@gmail.com>
2019-09-30 16:18:38 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+
+
+public abstract class Cliente {
+
+	private String nome;
+		
+	protected Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+		
+}
