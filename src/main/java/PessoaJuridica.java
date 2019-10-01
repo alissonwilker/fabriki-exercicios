cf980fad281ea08560322580caf88e45dc75b416
Lucas Mateus Silva <42013706+lucasmss@users.noreply.github.com>
2019-10-01 16:47:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,18 @@
+public class PessoaJuridica extends Cliente {
+    private String CNPJ;
+
+    public PessoaJuridica(String nome, String CNPJ) {
+        super(nome);
+        this.CNPJ = CNPJ;
+    }
+
+    public String getCNPJ() {
+        return  this.CNPJ;
+    }
+
+	public void setCNPJ(String cNPJ) {
+		CNPJ = cNPJ;
+	}
+
+    
+}
