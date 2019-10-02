eaac24a847f2fb2059bc900dbc66efc0129dc736
Alisson Wilker <alissonwilker@gmail.com>
2019-10-02 01:07:27 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,15 @@
+public class PessoaFisica extends Cliente{
+
+	private String CPF;
+
+	public PessoaFisica(String nome, String cPF) {
+		super(nome);
+		this.CPF = cPF;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+
+	 }
