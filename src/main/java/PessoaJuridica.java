1081eed86dae7034596565e8b4c7c85708fb2bb8
FranklinLimaCom <41466565+FranklinLimaCom@users.noreply.github.com>
2019-10-03 20:23:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,11 @@
+public class PessoaJuridica extends Cliente {
+private String cnpj;
+	public PessoaJuridica(String nome, String cnpj) {		
+		super(nome);
+		this.cnpj = cnpj;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+
+}
