1c605b5c042c9016cbed085e30c8f881e7cff71c
guilhermerodriguesdasilveira <53486686+guilhermerodriguesdasilveira@users.noreply.github.com>
2019-11-24 10:08:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,32 @@
+
+public class Carro extends Automovel {
+	
+	
+	
+	@Override
+	public int getQtdePneus() {
+		return 4;
+	}
+	
+	public void moverParaTras() {
+		if(estaLigado()==false) {
+			ligar();
+				
+				
+		}
+		marchaAFrente=false;
+		velocidade=1;
+	}
+	
+	protected void engatarMarchaARe() {
+		if(marchaAFrente==true) {
+
+			if(estaEmMovimento()==true) {
+				parar();
+			}
+			
+		}
+	}
+	
+
+}
