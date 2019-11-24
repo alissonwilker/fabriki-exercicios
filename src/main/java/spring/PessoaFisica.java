0f4cf60f5df7ad3aaaa70c26dc6bd7274c1a465e
Lucas Mateus Silva <42013706+lucasmss@users.noreply.github.com>
2019-11-24 18:30:49 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,44 @@
+
+@Entity
+@Table(name = "tb_pessoafisica")
+public class PessoaFisica {
+	
+		@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private String cpf;
+		private String nome;
+
+		
+		public PessoaFisica() {}
+
+
+		public PessoaFisica(String nome, String cpf) {
+			this.nome = nome;
+			this.cpf = cpf;
+		}
+		
+		public String getNome() {
+			return nome;
+		}
+
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		
+		public String getCpf() {
+			return cpf;
+		}
+
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+
+		public Object getId() {
+			
+			return cpf;
+		}
+		
+}
