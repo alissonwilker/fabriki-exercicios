d4dd23b939ef0cbad4f02d45900d51169bd55f07
PolyanaC <51519638+PolyanaC@users.noreply.github.com>
2019-11-25 19:34:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,27 @@
+import java.io.PrintStream;
+import java.math.BigInteger;
+
+public class LDP7252 {
+
+private static PrintStream saida = System.out;
+
+public static void main(String[] args) {
+BigInteger casas [] = new BigInteger [64];
+casas [0] = BigInteger.valueOf (1);
+BigInteger soma = BigInteger.valueOf(1);
+
+
+
+for (int i = 1; i <= 63; i++) {
+
+casas [i] = casas[i -1].multiply(BigInteger.valueOf(2));
+soma = soma.add(casas[i]);
+
+
+}
+
+saida.println ("totalGraos = " + soma);
+
+
+}
+}
