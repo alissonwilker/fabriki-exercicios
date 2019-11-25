6ee5d50b44439ffa3bd756d6b27f733d5bd8b96a
oliveira-marcos2 <53592561+oliveira-marcos2@users.noreply.github.com>
2019-11-25 00:11:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,19 @@
+package exercicio;
+
+public class Carro extends Automovel{
+	
+	public Carro() {
+		super.quantidadeDePneus = 4;
+	}
+	
+	protected void	engatarMarchaRe() {
+		super.parar();
+		super.marchaAFrente = false;
+	}
+	
+	public void moverParaTras() {
+		super.ligar();
+		engatarMarchaRe();
+		super.mover();
+	}
+}
