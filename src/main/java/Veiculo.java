47d485c50eac9fe65306b3a2476433fa0c38eda9
MrMateusFonteles <54007149+MrMateusFonteles@users.noreply.github.com>
2019-11-25 19:24:17 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,68 @@
+
+public abstract class Veiculo extends java.lang.Object{
+	
+	//DECLARAÇÃO DE ATRIBUTOS
+	protected int velocidade = 0;
+	protected int qtdePneus;
+	
+	//MÉTODO CONSTRUTOR
+	public Veiculo() {
+		
+	}
+
+	//MÉTODOS GET/SET
+	public int getVelocidade() {
+		return this.velocidade;
+	}
+	
+	public int getQtdePneus() {
+		return this.qtdePneus;
+}
+	public void setVelocidade(int velocidade) {
+		this.velocidade = velocidade;
+		}
+	public void setQtdePneus (int qtdePneus) {
+		this.qtdePneus = qtdePneus;
+	}
+		
+	//MÉTODOS ESPECÍFICOS
+	public void aumentarVelocidade() {		
+		
+		if (getVelocidade() != 0) {
+			setVelocidade(velocidade+1);
+		}
+	}
+	
+	protected boolean estaEmMovimento() {
+		
+		if (getVelocidade() != 0) {
+			return true;
+					}else{
+						return false;
+					}
+					}
+	
+	public void frear() {
+		if (getVelocidade() != 0) {
+			setVelocidade(velocidade-1); 
+		}
+	}
+	
+	protected void mover() {
+		if (getVelocidade() == 0) {
+		setVelocidade(velocidade++);
+	}
+}
+	
+	public void moverParaFrente() {
+		if (getVelocidade() == 0) {
+			setVelocidade(1);
+		}
+	}
+	
+	public void parar() {
+		setVelocidade(0);
+		
+	}
+
+}
