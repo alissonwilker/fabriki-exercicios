b676642404c662c2684dafa51b63a154ed6febb7
deividferreiracampos <47040510+deividferreiracampos@users.noreply.github.com>
2019-11-24 21:26:15 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,45 @@
+
+public abstract class Automovel extends Veiculo {
+ //declaraçao de atributos
+	private boolean	ligado;
+	protected boolean	marchaAFrente;
+	// construçãodo metodo
+	public Automovel() {}
+	
+	public void moverParaFrente() {
+		ligar();
+		engatarMarchaAFrente();
+		super.moverParaFrente();
+		
+	}
+	public void ligar() {
+		if(ligado==false) {
+			ligado=true;
+		}
+	}
+	public void desligar() {
+		if (ligado==true) {
+			parar();
+			ligado=false;
+		}
+	}
+	public boolean estaLigado() {
+		if(ligado==true) {
+			return true;
+		}else {
+			return false;}
+	}
+	
+	public boolean estaEmMarchaAFrente() {
+		if(marchaAFrente==true) {
+			return true;
+		}else {
+			return false;}
+	}
+	protected void engatarMarchaAFrente() {
+		if(marchaAFrente==false) {
+			marchaAFrente= true; 
+		}
+	}
+	
+}
