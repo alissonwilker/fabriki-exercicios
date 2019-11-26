99170fa6732cd3faf32518c6c4353c62483ba7f5
marinhothiago <42653753+marinhothiago@users.noreply.github.com>
2019-11-26 13:23:00 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,50 @@
+/*PessoaFisica*/
+package spring;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoafisica")
+public class PessoaFisica {
+	
+	private String cpf;
+	private String nome;
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public PessoaFisica(String nome, String cpf) {
+		// TODO Auto-generated constructor stub
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public PessoaFisica() {
+		// TODO Auto-generated constructor stub
+	}
+}
