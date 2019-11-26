3232b22def3e4b47b054ccc51cba31c299656acc
marinhothiago <42653753+marinhothiago@users.noreply.github.com>
2019-11-26 13:22:56 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRepository.java
@@ -0,0 +1,8 @@
+/*PessoaFisicaRepo*/
+package spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Integer>{
+
+}
