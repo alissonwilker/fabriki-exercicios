70a2f097d1c28ae753e018a8c5894f5dc1d1dabb
augustochaves23 <42880517+augustochaves23@users.noreply.github.com>
2019-11-29 15:44:31 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,16 @@
+public class PessoaJuridica extends Cliente {
+
+	private String CNPJ;
+
+	public PessoaJuridica(String nome, String CNPJ) {
+		super(nome);
+		this.CNPJ = CNPJ;
+	}
+
+	public String getCNPJ() {
+		return CNPJ;
+	}
+
+
+}
+
