5c648113d93299e5d73a827b6d2abccbd88c721d
guinsf <38284477+guinsf@users.noreply.github.com>
2020-08-10 21:14:04 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,27 @@
+public class MediaNotas.java {
+
+	 
+	public static void main(String[] args) {
+		
+		       
+			Scanner input = new Scanner(System.in);  
+		double nota;     
+		double somaNotas = 0;     
+		double media;     
+		int qtdnotas =0;
+		
+		System.out.print("Digite a quantidade de notas que deseja calcularª: ");
+		qtdnotas =  input.nextInt();
+		         
+		for ( int i = 1; i <= qtdnotas; i++)     {    
+			System.out.print("Digite a "+i+"ª nota: ");     
+			nota = input.nextDouble();    
+			somaNotas = somaNotas + nota;     }   
+			media = somaNotas / qtdnotas;     
+		
+		    
+				System.out.printf("Aluno aprovado. Media %s. ", media);    
+			
+		}
+
+	}
