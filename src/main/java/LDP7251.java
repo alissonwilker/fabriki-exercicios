443e676d73214fac96eab3b87a47191249f0a6fe
guinsf <38284477+guinsf@users.noreply.github.com>
2020-08-10 22:11:44 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,35 @@
+	import java.util.Scanner;
+
+public class LDP7251{
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int n;
+		int maior = 0;
+		int num;
+		
+		do {
+			System.out.print(": "); 
+			num =  input.nextInt();
+			if (num>maior) {
+				
+				maior=num;}
+			
+			if (num<0) {
+				maior=num;}
+			}
+		
+	
+	
+	    while((num >0 || num <0));
+	    
+	     	
+	    	
+	    
+			
+		System.out.print("Maior =" +maior);
+		
+	}
+	
+}
