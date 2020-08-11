969e4b30afee488f47a45951752ee0eb25c8cda3
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2020-08-10 21:54:58 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MediaNotas {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        int qtNotas = scanner.nextInt();
+        
+        int [] notas = new int [qtNotas];
+        
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = scanner.nextInt();
+        }
+        int somaNotas = 0;
+                
+        for (int i = 0; i < notas.length; i++) {
+            somaNotas += notas[i]; 
+        }
+        int media = somaNotas/2;
+       
+        System.out.println("media: "+media);
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("nota"+i+" " + notas[i]);
+                  
+        }
+       
+    }
+}
