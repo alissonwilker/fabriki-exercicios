c6fab23eb5e6a64dbc5ace07c42ada2eb0e73029
Elizeu Grosskopf Schlottfeldt Neto <42324527+Cloker98@users.noreply.github.com>
2020-08-24 14:50:11 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+class Cliente {
+    //Declaração das variavéis
+    private String nome;
+    
+    //Método Construtor
+    protected Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    //Métodos Get e Set
+    public String getNome() {
+        return nome;
+    }   
+}
