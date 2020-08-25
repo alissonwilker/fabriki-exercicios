fb714f1a38bdc9387a161ec8d846fd29ffd4bfca
Kissinho <kissinho@live.com>
2020-08-25 16:14:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,14 @@
+
+public class PessoaJuridica extends Cliente {
+	String cnpj;
+
+	public PessoaJuridica(String nome, String cnpj) {
+		// TODO Auto-generated constructor stub
+		super(null);
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+}
