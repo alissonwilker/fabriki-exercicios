dc71e92694563a5a13892f6356d2ad769f7e694a
Aurélio Vinícius França dos Santos <58404045+aureliovinifs@users.noreply.github.com>
2020-08-25 20:59:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,23 @@
+
+
+public class PessoaFisica extends Cliente {
+	private String cpf;
+
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+
+
+
+}
