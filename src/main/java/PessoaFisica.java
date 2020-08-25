5a1a82c0ddc297233a1d75915c322f11278aa7f2
Kissinho <kissinho@live.com>
2020-08-25 16:14:06 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,16 @@
+
+public class PessoaFisica extends Cliente {
+	String cpf, nome;
+
+	public PessoaFisica(String nome, String cpf) {
+		// TODO Auto-generated constructor stub
+		super(null);
+		
+		
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+}
