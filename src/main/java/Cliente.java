ad7b079110052abebd76603f54044d4bd069d630
Aurélio Vinícius França dos Santos <58404045+aureliovinifs@users.noreply.github.com>
2020-08-25 20:31:47 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,19 @@
+
+public abstract class Cliente {
+	private String nome;
+
+	public Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	
+	
+
+}
+
+
