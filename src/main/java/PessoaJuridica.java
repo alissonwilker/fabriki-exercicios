5a9d87310c7d38086c3446543cea6c0ed28f3a75
Aurélio Vinícius França dos Santos <58404045+aureliovinifs@users.noreply.github.com>
2020-08-25 20:29:41 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,17 @@
+package projetolp2;
+
+public class PessoaJuridica extends Cliente {
+	private String cnpj;
+
+	public PessoaJuridica(String nome, String cnpj) {
+		super(nome);
+		this.cnpj = cnpj;
+	}
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	
+	
+}
