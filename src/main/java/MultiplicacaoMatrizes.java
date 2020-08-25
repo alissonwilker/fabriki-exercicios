782b06dc34992d8d3a3ca25f0ae224eeb66413cf
guinsf <38284477+guinsf@users.noreply.github.com>
2020-08-25 16:13:35 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MultiplicacaoMatrizes.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class MultiplicacaoMatrizes{
+
+	public static void main(String[] args) {
+	
+				
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o numero de linhas matriz A");
+		int linha= scanner.nextInt();
+		System.out.print("Digite o numero de colunas matriz A");
+		int coluna= scanner.nextInt();
+		
+		int [][] MatrizA = new int[linha][coluna];
+		
+		 
+		int linha2= 2;
+		System.out.print("Digite o numero de colunas matriz B");
+		int coluna2= scanner.nextInt();
+		
+		int [][] MatrizB = new int[linha2][coluna2];
+		
+	
+		for (int i=0; i<linha; i++) {
+			for (int j=0; j<coluna; j++) {
+				MatrizA[i][j] = scanner.nextInt();
+			}
+		}
+	
+		System.out.println("");
+		for (int i=0; i<linha2; i++) {
+			for (int j=0; j<coluna2; j++) {
+				 MatrizB[i][j] = scanner.nextInt();
+			}
+		}
+
+		for (int i=0; i<linha; i++) {
+			for (int j=0; j<coluna; j++) {
+			
+				System.out.print(MatrizA[i][j] +" ");
+				
+			}
+			
+			System.out.println("");
+		}
+		
+		System.out.println("");
+	
+		for (int i=0; i<linha2; i++) {
+			for (int j=0; j<coluna2; j++) {
+			
+				System.out.print(MatrizB[i][j] +" ");
+				
+			}
+			
+			System.out.println("");
+		}
+		
+		
+			
+			int [][] matrizC = new int[linha][coluna2];
+			for (int i = 0; i<linha; i++) {
+				for (int j = 0; j<coluna2; j++) {
+					for (int k = 0; k<linha2; k++) {
+						
+						matrizC[i][j] += MatrizA[i][k] * MatrizB[k][j];
+						
+				}
+				
+			}
+			
+		}
+			
+			for (int i=0; i<linha2; i++) {
+				for (int j=0; j<coluna2; j++) {
+				
+					System.out.print(matrizC[i][j] +" ");
+					
+				}
+	
+			}
+	
+			
+				
+		
+	
+	}
+}
