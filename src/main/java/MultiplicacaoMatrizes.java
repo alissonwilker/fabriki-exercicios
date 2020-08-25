eff94c089d978a89fafbfc707f07c48cc59acf4f
JoaoZ7 <61168104+JoaoZ7@users.noreply.github.com>
2020-08-24 21:28:36 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MultiplicacaoMatrizes.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public MultiplicacaoMatrizes {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int linhasA = scanner.nextInt();
+		int colunasA = scanner.nextInt();
+		int colunasB = scanner.nextInt();
+		int matrizA[][] = new int[linhasA][colunasA];
+		int matrizB[][] = new int[colunasA][colunasB];
+		int resultado[][] = new int[matrizA.length][matrizB[0].length];
+		
+		for (int i = 0; i < linhasA; i++) {
+			for (int j = 0; j < colunasB; j++) {
+				matrizA[i][j] = scanner.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < colunasA; i++) {
+			for (int j = 0; j < colunasB; j++) {
+				matrizB[i][j] = scanner.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < matrizA.length; i++) {
+			for (int j = 0; j < matrizB[0].length; j++) {
+				for (int k = 0; k < colunasA; k++) {
+				resultado[i][j] += matrizA[i][k] * matrizB[k][j];
+				}
+				
+			}
+		}
+		
+		for (int i = 0; i < resultado.length; i++) {
+			for (int j = 0; j < resultado[0].length; j++) {
+				System.out.println(resultado[i][j]);
+			}
+			
+		}
+		
+		
+	}
+
+}
