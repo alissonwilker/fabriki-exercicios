5109f2f6708052b2d6ccbac95e6bd3531aa338db
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-08-26 11:04:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,15 @@
+import contaBancaria.clientes.Cliente;
+
+public class PessoaJuridica extends Cliente {
+
+    private String cnpj;
+
+    public PessoaJuridica(String nome, String cnpj) {
+        super(nome);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+}
