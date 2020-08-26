ec5eaf8a2699e0e04bf2969b64625c06e759ff2e
hugocesarhc <47836510+hugocesarhc@users.noreply.github.com>
2020-08-25 21:24:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,17 @@
+public abstract class Cliente {
+
+	protected String nome;
+	
+	protected Cliente(String nome) {
+		nome = this.nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
