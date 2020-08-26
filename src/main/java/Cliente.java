f852a2761bd26bc4fc37f8d3aae53a0c8e8e96e7
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-08-26 09:50:04 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+package contaBancaria.clientes;
+
+public abstract class Cliente {
+
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
