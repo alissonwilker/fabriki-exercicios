23fa3693202f23ed236d310c17a8aadcfe7a5d34
mariadenise <61212327+mariadenise@users.noreply.github.com>
2020-08-25 23:47:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+public abstract class Cliente {
+	protected String nome;
+
+	
+	public Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+	public String getNome(){
+		return this.nome;
+	}
+	public void setNome(String nome){
+		 this.nome = nome;
+	}
+}
