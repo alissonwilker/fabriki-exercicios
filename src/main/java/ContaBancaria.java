61e44fe428e63fb39513b41d55c00c6c35686800
mariadenise <61212327+mariadenise@users.noreply.github.com>
2020-08-26 01:18:38 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,48 @@
+public class ContaBancaria {
+	
+	private Cliente cliente;
+	private static int contador;
+	private int id;
+	private int saldo;
+
+	 
+	
+	public ContaBancaria(Cliente cliente) {
+		this.cliente = cliente;
+		contador = contador+1;
+		id = contador;
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void depositar(int valor) {
+		if(valor>0) {
+			saldo = saldo + valor;
+		}
+		
+	}
+
+	public int sacar(int valor) {
+		if(valor <=saldo) {
+			saldo = saldo-valor;
+		}
+		return this.saldo;
+   }
+		
+	
+	public Cliente getCliente() {
+		// TODO Auto-generated method stub
+		return cliente;
+	}
+	
+	public int getId() {
+		
+		return id;
+		
+	}
+	public int getSaldo() {
+		
+		return saldo;
+	}
+
+}
