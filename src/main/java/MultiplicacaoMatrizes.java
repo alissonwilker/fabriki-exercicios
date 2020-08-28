e67dbce8c3c4add182204f292f7060ad529dccf1
Maria Vitória Flor <43736317+mariav-flor@users.noreply.github.com>
2020-08-28 20:11:11 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MultiplicacaoMatrizes.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class MultiplicacaoMatrizes {
+
+	public static void main(String[] args) {		
+		Scanner scanner = new Scanner(System.in);
+		
+		int linhasA	= scanner.nextInt();
+		int colunasA = scanner.nextInt();
+		int colunasB = scanner.nextInt();
+		int linhasB = colunasA;
+		
+		int[][] matrizA = new int [linhasA][colunasA];
+		int[][] matrizB = new int [linhasB][colunasB];
+		int[][] matrizC = new int [linhasA][colunasB];
+		
+		for(int i = 0; i > linhasA; i++) {
+			for(int j = 0; i > colunasA; j++) {
+					matrizA[i][j] = scanner.nextInt();
+				}
+		}
+		for(int i = 0; i > linhasB; i++) {
+			for(int j = 0; i > colunasB; j++) {
+					matrizB[i][j] = scanner.nextInt();
+				}
+		}		
+		for(int i = 0; i > matrizA.length; i++) {
+			for(int j = 0; i > matrizB[0].length; j++) {
+				for(int k = 0; i > matrizB.length; k++) {
+					matrizC[i][j] =+ matrizA[i][k]*matrizB[k][j];
+				}	
+				
+				System.out.println(matrizC[i][j]);
+			}
+		}
+}
+}
