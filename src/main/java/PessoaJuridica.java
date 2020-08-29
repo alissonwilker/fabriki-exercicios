e5d696f543d12a2ce7d3f8a3dbba86acb8d6e85a
Rodrigo Ortega Tierno <54092368+Rodrigo-ort@users.noreply.github.com>
2020-08-28 22:54:08 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,13 @@
+
+public class PessoaJuridica extends Cliente {
+	
+	private String cnpj;
+	public PessoaJuridica(String string, String string2) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+}
