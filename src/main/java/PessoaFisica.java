c026d5959c29004f322522855a120f185ed72f4f
Rodrigo Ortega Tierno <54092368+Rodrigo-ort@users.noreply.github.com>
2020-08-28 22:54:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,15 @@
+
+public class PessoaFisica extends Cliente {
+	
+	private String cpf;
+	
+	public PessoaFisica(String string, String string2) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+}
