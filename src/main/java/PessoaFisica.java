02cbd766f771b6ce5ed680cc48860d2ff343ca24
Brunossleo <47836548+Brunossleo@users.noreply.github.com>
2020-08-28 22:46:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,10 @@
+public class PessoaFisica extends Cliente {
+	private String cpf;
+	protected PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+}
