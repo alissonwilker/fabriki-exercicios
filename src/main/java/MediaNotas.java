5554ab514ab31a76a4b5c4e49714029315df26f0
AlmirMalheiros <56326125+AlmirMalheiros@users.noreply.github.com>
2020-09-04 13:01:02 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,35 @@
+public class MediaNotas {
+
+	public static void main(String[] args) {
+		
+		//Variáveis		
+		NumberFormat formatador = NumberFormat.getInstance(new Locale("pt", "BR"));
+		formatador.setMinimumFractionDigits(1);
+		formatador.setMaximumFractionDigits(2);		
+		Scanner scan = new Scanner(System.in);
+		int n,nt=0;
+		double media=0;		
+		
+		//Lendo valor do usuario
+		n = scan.nextInt(); 
+		
+		//Bloco de validaçção e execução
+		if(n>0) { 
+		double[]notas = new double[n];
+		for(int i=0;i<n;i++) {	
+			notas[i] = scan.nextDouble();
+			media+= notas[i];			
+		}	
+			media /=(double)n;
+			
+			System.out.println("media = " + formatador.format(media));			
+			
+			for(int i=0;i<n;i++) {				
+				nt++;				
+				System.out.println("nota "+ nt +" = "+ formatador.format(notas[i]));}}
+		else {
+			System.out.println("Valor inicial deve ser > 0");}	
+		
+	}
+		
+ }
