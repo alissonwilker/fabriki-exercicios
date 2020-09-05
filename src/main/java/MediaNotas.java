0720cb6c2e59fa52bad28101f1b25df5e24472ac
kamilaamaraal <69153874+kamilaamaraal@users.noreply.github.com>
2020-09-04 23:24:46 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,58 @@
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MediaNotas {
+
+   
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        NumberFormat formatador = NumberFormat.getInstance(new Locale("pt", "BR"));
+
+        int nota;
+        List notas = new ArrayList();
+        double soma = 0;
+        double media;
+        int qtdeNotas;
+
+        System.out.println();
+        qtdeNotas = entrada.nextInt();
+        int contagem = 0;
+        
+        if (qtdeNotas > 0) {
+
+            do {
+
+                System.out.println();
+                nota = entrada.nextInt();
+                notas.add(nota);
+                soma += nota;
+
+                media = (soma / qtdeNotas);
+
+                formatador.setMinimumFractionDigits(1);
+                formatador.setMaximumFractionDigits(1);
+
+                contagem++;
+                
+            } while (contagem < qtdeNotas);
+            
+            System.out.println("media = " + formatador.format(media));
+            
+            for (int i = 0; i < notas.size(); i++) {
+                
+                System.out.println("nota "+(i+1)+" = "+formatador.format(notas.get(i)));
+            }
+
+            
+
+
+        } else {
+            System.out.println("Digite um valor maior que 0");
+        }
+        
+    }
+}
