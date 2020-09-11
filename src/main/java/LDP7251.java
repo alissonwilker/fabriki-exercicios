31aac62c90d22be1a7add90563b389b26f19b1ee
pripaula2002 <61168280+pripaula2002@users.noreply.github.com>
2020-09-10 23:00:14 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LDP7251 { 
+	public static void main(String[] args) {
+ try (Scanner leitor = new Scanner(System.in)) {
+	
+    float numero = 0;
+    float maior; 
+    			
+          maior=numero;
+	  while ((numero !=0)){
+		  if(numero>maior){
+		  }		 
+	}
+ }
+	System.out.println("maior="+ maior);		  
+}
+}
