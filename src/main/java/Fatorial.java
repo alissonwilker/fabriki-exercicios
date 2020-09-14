e65bda4d226244b0529c3c4ca9a5a9dd57e3d5d2
Plaxcaster <56280285+Plaxcaster@users.noreply.github.com>
2020-09-14 19:16:15 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		long numero = scanner.nextInt();
+		long result = 1;
+		while(numero>=2) {
+			result = result *numero;
+			numero = numero-1;
+		}
+		
+		System.out.println(result+"\n");
+
+	}
+
+}
