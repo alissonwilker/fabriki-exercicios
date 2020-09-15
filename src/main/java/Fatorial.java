e368beb5eaa0d70852a5fe97aedc0ebd8ea2c162
Felipe Barros <felipeb4c@gmail.com>
2020-09-15 00:22:37 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int numero = sc.nextInt();
+		int fatorial = numero;
+		
+		if(numero == 0) {
+			fatorial = 1;
+		} else {
+			while(numero > 1) {
+				fatorial = fatorial * (numero - 1);
+				numero--;
+			}
+		}
+		System.out.println(fatorial);
+		
+	}
+}
