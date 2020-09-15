b47f8338f61cc3bb6f42bdb58d425ce490c7f35f
Josué de Paulo Viana <josuep.viana@gmail.com>
2020-09-15 20:53:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main (String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int f = x; 
+        if(x == 0){
+            System.out.println("1");
+            return;
+        }else if(x <= 25){
+            while (x > 1){
+                f = f *(x-1); x--;
+            }
+        }
+        System.out.println(f); 
+    }
+}
