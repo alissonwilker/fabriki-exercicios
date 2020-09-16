2e988fc3096680878b91118365a9dbce6c1826cd
guinsf <38284477+guinsf@users.noreply.github.com>
2020-09-16 12:19:12 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class LDP7142 {
+
+	public static void main(String[] args) {
+						
+		int numHrTrab;
+		int exced;
+		int sal;
+		int e;
+		int saltotal;
+		
+		Scanner scanner = new Scanner(System.in);
+		int cod =  scanner.nextInt();
+		numHrTrab =  scanner.nextInt();
+		
+		if (numHrTrab>50) {
+			
+			e=numHrTrab-50;
+			exced=e*20;
+			sal=50 *10;
+			saltotal = sal+exced;
+		
+			System.out.println("codigo = "+cod);
+			System.out.println("salarioExcedente = "+exced);
+			System.out.println("salarioTotal = "+saltotal);
+			
+		}
+			else { 
+				exced=0;
+				saltotal=numHrTrab*10;
+			
+			
+			System.out.println("codigo = "+cod);
+			System.out.println("salarioExcedente = "+exced);
+			System.out.println("salarioTotal = "+saltotal);
+		
+		}
+
+	}
+}
