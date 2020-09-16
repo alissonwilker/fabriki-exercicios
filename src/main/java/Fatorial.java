0e3683548af88aa87688fe9fad9bffe3ae30a158
pripaula2002 <61168280+pripaula2002@users.noreply.github.com>
2020-09-15 23:32:32 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		try (Scanner ent = new Scanner(System.in)) {
+			int num = ent.nextInt();
+		
+		System.out.println("" +fatorial(num));
+	}
+	}
+			public static BigInteger fatorial(int n){
+			if(n == 0) return BigInteger.ONE;
+				BigInteger aux = BigInteger.ONE;
+				for (int i = 2; i <= n; i++){
+			BigInteger indice = BigInteger.valueOf(i);
+				aux = aux.multiply(indice); 
+			
+			 }
+			  return aux;
+			}
+}
