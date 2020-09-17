1bda88a87c3c7f212fa5e88563b0d2bd75793654
CarlosVictor01 <69177786+CarlosVictor01@users.noreply.github.com>
2020-09-16 22:13:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Fatorial {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+        BigDecimal fatorial = new BigDecimal(numero);
+
+        if (numero == 0) {
+            fatorial = new BigDecimal(1);
+        } else {
+            for (int i = numero; i > 1; i--) {
+                fatorial = fatorial.multiply(new BigDecimal(i - 1));
+            }
+        }
+
+        System.out.println(fatorial);
+    }
+}
