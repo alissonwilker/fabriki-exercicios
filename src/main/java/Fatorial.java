5bbcdfc102db70f1a07848d8115be4aeb4fbe5e3
RaquelLAB <61168116+RaquelLAB@users.noreply.github.com>
2020-09-17 16:45:43 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		 Scanner sc  = new Scanner(System.in);
+		 int num;
+		 
+		 num = sc.nextInt();
+		 
+		 if (num==0) {
+			 System.out.println("1");
+		 }
+		 
+		 else if (num>0 && num<26) {
+			 int a = num-1;
+			 while(a>0) {
+				num = num*a;
+				a--;}}
+		
+		 System.out.println(num);
+	}
+}
