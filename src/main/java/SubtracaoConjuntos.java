840aaa3fff312114844dc1332c7c8c7aa66416f7
guinsf <38284477+guinsf@users.noreply.github.com>
2020-09-24 16:14:26 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+
+public class SubtracaoConjuntos {
+
+	 public static void main(String[] args) {
+	
+	        Set<Integer> conjuntoA = new HashSet<>();
+	        Set<Integer> conjuntoB = new HashSet<>();
+	         
+	        Scanner scanner = new Scanner(System.in);
+	        int numConjuntos = 1;  
+	            
+	            do { 
+	            	numConjuntos = scanner.nextInt();
+	                conjuntoA.add(numConjuntos);
+	             
+	            }while (numConjuntos!=0);
+	            
+			         do{
+			                
+			           numConjuntos = scanner.nextInt();
+			           conjuntoB.add(numConjuntos);
+		                
+				         if(numConjuntos==0){
+				         conjuntoA.removeAll(conjuntoB);
+				                
+				         System.out.println(conjuntoA);
+						  }
+				         
+					   }while (numConjuntos!=0);
+		      
+	                
+	}
+}
