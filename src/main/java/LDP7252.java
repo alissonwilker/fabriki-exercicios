058971df08ebb1fee806ef84f724e3c348fc8b90
ANDRE210383 <69171291+ANDRE210383@users.noreply.github.com>
2020-09-28 19:40:58 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class LDP7252 {
+	
+		private static double primeiro, segundo;
+
+		public static void main(String[] args) {
+			Scanner entrada = new Scanner(System.in);
+			double graosCasa=1,casasTabuleiro=64;
+			long graos=1;
+
+			for(long x=1;x<casasTabuleiro;x++){
+				graosCasa = graosCasa*2;
+				graos +=graosCasa;
+			}
+
+			System.out.println("totalGraos =  "+graos);
+
+		}
+	}
