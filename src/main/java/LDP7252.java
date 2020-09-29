e3fdd97bdaed1578062d647105e3aec770e5165f
kamilaamaraal <69153874+kamilaamaraal@users.noreply.github.com>
2020-09-29 14:40:48 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,11 @@
+import java.math.BigDecimal;  
+
+public class LDP7252 {  
+      
+    public static void main(String[] args) {  
+      
+        System.out.println(new BigDecimal(2).pow(64).subtract(BigDecimal.ONE));  
+           
+    }  
+      
+} 
