9b204d00b662a94759765c395fe141b4bf4cab95
Aurélio Vinícius França dos Santos <58404045+aureliovinifs@users.noreply.github.com>
2020-09-30 13:25:03 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,28 @@
+public abstract class Cliente {
+    /**
+     * O nome do cliente.
+     */
+    private String nome; 
+
+    /**
+     * Construtor que recebe o nome do cliente.
+     * 
+     * @param nome
+     *            o nome do cliente.
+     */
+    protected Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * Recupera o nome do cliente.
+     * 
+     * @return o nome do cliente.
+     */
+    public String getNome() {
+        return nome;
+    }
+
+	
+    
+}
