d8d448f848aa89fe929c48fe5fb58754a73d0e56
IsaTorres993 <54218304+IsaTorres993@users.noreply.github.com>
2020-10-02 15:14:48 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class LDP7252 {
+	public static void main(String[] args) {
+		BigInteger graos = new BigInteger("2");
+		int casas = 64;
+		BigInteger totalGraos = graos.pow(casas);
+		System.out.println("totalGraos = " + totalGraos);
+	}
+}	
