530b48f8e62b48a5c543ba28749e5a72ca761d48
Rodrigo Ortega Tierno <54092368+Rodrigo-ort@users.noreply.github.com>
2020-10-04 00:17:47 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,16 @@
+
+public abstract  class Cliente {
+
+	//atributos
+	protected String nome;
+	
+	protected Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	
+}
