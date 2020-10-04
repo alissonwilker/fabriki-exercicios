d89bec53eb67a16e9b41faeaf4b8fc42eeb01b4b
Rodrigo Ortega Tierno <54092368+Rodrigo-ort@users.noreply.github.com>
2020-10-04 00:39:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BancoServlet.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/BancoServlet")
+public class BancoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String paramNomeCliente = request.getParameter("nomeCliente");
+		String paramTipoCliente = request.getParameter("tipoCliente");
+		String paramCpfCnpj = request.getParameter("cpfCnpj");
+		String paramOperacao = request.getParameter("operacao");
+		String valorDeposito = request.getParameter("valorDeposito");
+		String valorSaque = request.getParameter("valorSaque");
+		//int paramValorDeposito = Integer.parseInt(valorDeposito);
+		//int paramValorSaque = Integer.parseInt(valorSaque);
+		Cliente cliente;
+		if (paramTipoCliente.equals("pessoaFisica")) {
+			cliente = new PessoaFisica(paramNomeCliente, paramCpfCnpj);
+		} else {
+			cliente = new PessoaJuridica(paramNomeCliente, paramCpfCnpj);
+		}
+		ContaBancaria conta = new ContaBancaria(cliente);
+
+		String resposta = "";
+		//int valor = 0;
+		switch (paramOperacao) {
+		case "recuperarNomeCliente":
+			resposta = cliente.getNome();
+			break;
+		case "recuperarCpfCliente":
+			if (paramTipoCliente.equals("pessoaFisica")) {
+				resposta = ((PessoaFisica) cliente).getCpf();
+			}
+			break;
+		case "recuperarCnpjCliente":
+			if (paramTipoCliente.equals("pessoaJuridica")) {
+				resposta = ((PessoaJuridica) cliente).getCnpj();
+			}
+			break;
+		case "recuperarSaldo":
+			resposta = Integer.toString(conta.getSaldo());
+			break;
+		case "depositarValor":
+			conta.depositar(Integer.parseInt(valorDeposito));
+			resposta = Integer.toString(conta.getSaldo());
+			break;
+		case "sacarValor":
+			conta.sacar(Integer.parseInt(valorSaque));
+			resposta = Integer.toString(conta.getSaldo());
+			break;
+		case "depositarSacarValor":
+			conta.depositar(Integer.parseInt(valorDeposito));
+			conta.sacar(Integer.parseInt(valorSaque));
+			resposta = Integer.toString(conta.getSaldo());
+			break;
+		}
+
+		request.setAttribute("resposta", resposta);
+		RequestDispatcher reqDispatcher = request.getRequestDispatcher("resposta.jsp");
+		reqDispatcher.forward(request, response);
+
+	}
+}
