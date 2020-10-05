e339f6b0b12b63beac5def9076af975f61e14a96
maycobrito <42880612+maycobrito@users.noreply.github.com>
2020-10-04 23:17:19 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BancoServlet.java
@@ -0,0 +1,48 @@
+
+
+	import java.io.IOException;
+
+	import javax.servlet.RequestDispatcher;
+	import javax.servlet.ServletException;
+	import javax.servlet.annotation.WebServlet;
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
+
+	@WebServlet("/BancoServlet")
+	public class BancoServlet extends HttpServlet {
+		public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+			String paramNomeCliente = request.getParameter("nomeCliente");
+			String paramTipoCliente = request.getParameter("tipoCliente");
+			String paramCpfCnpj = request.getParameter("cpfCnpj");
+			String paramOperacao = request.getParameter("operacao");
+
+			Cliente cliente;
+			if (paramTipoCliente.equals("pessoaFisica")) {
+				cliente = new PessoaFisica(paramNomeCliente, paramCpfCnpj);
+			} else {
+				cliente = new PessoaJuridica(paramNomeCliente, paramCpfCnpj);
+			}
+
+			String resposta = "";
+			switch (paramOperacao) {
+			case "recuperarNomeCliente":
+				resposta = cliente.getNome();
+				break;
+			case "recuperarCpfCliente":
+				if (paramTipoCliente.equals("pessoaFisica")) {
+					resposta = ((PessoaFisica)cliente).getCpf();
+				}
+				break;
+			}
+			
+			request.setAttribute("resposta", resposta);
+			RequestDispatcher reqDispatcher = request.getRequestDispatcher("resposta.jsp");
+			reqDispatcher.forward(request, response);
+		}
+	}
+
+
+
+
+
