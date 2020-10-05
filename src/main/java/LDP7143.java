1dc4555f254c8f76d4c655cfcca193274bc4a9c5
ANDRE210383 <69171291+ANDRE210383@users.noreply.github.com>
2020-10-05 19:54:13 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,42 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+		public class LDP7143 {
+		    
+		    public static void main(String[] args) {
+		        //@SuppressWarnings("resource")
+				Scanner ent = new Scanner(System.in);
+		        int a, b, c, d;
+		        int quadrado1, quadrado2, quadrado3, quadrado4;
+		        
+		      //  for(i = 0; i > 0; i++){
+		        a = 1;
+		        b = 2;
+		        c = 3;
+		        d = 4;
+		        
+		        quadrado1 = a * a;
+		        quadrado2 = b * b;
+		        quadrado3 = c * c;
+		        quadrado4 = d * d;
+		        
+		        if (quadrado3 >= 1000) { 
+		        
+		        System.out.println(quadrado3 );
+		        }
+		        
+		        
+		        System.out.println(a=1);
+		        System.out.println(b=2);
+		        System.out.println(c=3);
+		        System.out.println(d=4);
+		        
+		        
+		        System.out.println("a2=" + quadrado1 );
+		        System.out.println("b2=" + quadrado2 );
+		        System.out.println("c2=" + quadrado3 );
+		        System.out.println("d2=" + quadrado4 );
+		    }
+		}
+		        
