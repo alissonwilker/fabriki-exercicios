4160d5edc0c40772867cc69086bde30c78a5d1f1
pripaula2002 <61168280+pripaula2002@users.noreply.github.com>
2020-10-08 01:13:59 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,27 @@
+package lp2;
+
+public class LDP7143 {
+	 public void Main(String[] Args){
+	   int[] numeros = new int[4];
+	   int[] quadradoDosNumeros = new int[numeros.length];
+	 for(int i=0;i<numeros.length;i++){
+	      }
+
+	      for(int i=0;i<numeros.length;i++){
+	         int quadradoDoNumero = 0;
+	         for(int j=0;j<numeros[i];j++){
+	            quadradoDoNumero += numeros[i];
+	         }
+	         quadradoDosNumeros[i] = quadradoDoNumero;
+	      }
+	      if(quadradoDosNumeros[2]>=1000){
+
+	      }else{
+
+	     }
+	    System.out.println("a,a2 = " +numeros);
+	  	System.out.println("b,b2 = " +numeros);
+	  	System.out.println("c,c2 = " +numeros);
+	  	System.out.println("d,d2 = " +numeros);
+	   }
+}
