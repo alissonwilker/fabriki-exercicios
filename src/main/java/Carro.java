43b74b3d4f494400837d4db6dcf8be4ebb7ab2de
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2020-10-12 23:55:39 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,22 @@
+public class Carro extends Automovel {
+
+    public Carro() {
+    }
+
+    boolean estaLigado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean estaEmMarchaAFrente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void moverParaTras() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void desligar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
