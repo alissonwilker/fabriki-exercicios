a6af05759aebc6efd2f62e152ae6db275e6f1eb0
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2020-10-12 23:55:41 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,47 @@
+public abstract class Automovel extends Veiculo {
+    
+    Object getQtdePneus() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    void desligar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    boolean estaEmMovimento() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    boolean estaLigado() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    void ligar() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    void aumentarVelocidade() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    Object getVelocidade() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    void frear() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    void moverParaFrente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean estaEmMarchaAFrente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void parar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
