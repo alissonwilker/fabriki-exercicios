2e8d33f3831a6db2ae908d11702c561aafe8925c
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2020-10-12 23:55:45 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Moto.java
@@ -0,0 +1,20 @@
+public class Moto extends Automovel {
+
+    public Moto() {
+        
+    }
+
+  
+    boolean estaLigado() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean estaEmMarchaAFrente() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void desligar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
