72e4d1d3026cfadaf84ccc05d2cf70cd0e958e82
Matheus Santana De Brito <57404789+matheuss890@users.noreply.github.com>
2020-10-15 19:45:35 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,38 @@
+public class Bicicleta {
+
+	public Object getQtdePneus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getVelocidade() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean estaEmMovimento() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void frear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void parar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
