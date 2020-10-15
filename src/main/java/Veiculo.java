5523d26fb76f6045ffa2aa0ce25d51f31f5b107a
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2020-10-15 15:08:13 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,38 @@
+public abstract class Veiculo
+        extends java.lang.Object {
+
+    protected int velocidade;
+    protected int qtdePneus;
+
+    int getVelocidade() {
+        return velocidade;
+    }
+    
+    int getQtdePneus() {
+        return qtdePneus;
+    }
+    
+    void aumentarVelocidade() {
+        System.out.println("Aumenta a velocidade do veiculo, caso esteja em movimento.");
+    }
+    
+    void moverParaFrente() {
+        System.out.println("Movimenta o veiculo para frente, caso esteja parado.");
+    }
+
+    void frear() {
+        System.out.println("Diminui a velocidade do veiculo, caso esteja em movimento.");
+    }
+
+    void parar() {
+        System.out.println("Para o movimento do veiculo, freando o veiculo ate que a velocidade seja zero.");
+    }
+
+    protected void mover() {
+        System.out.println("Coloca o veiculo em movimento, caso esteja parado.");
+    }
+    protected boolean estaEmMovimento() {
+        return true;
+    }
+
+}
