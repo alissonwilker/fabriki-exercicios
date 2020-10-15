dd8d6222e3d375cfe2229970a47f6d38a5b23783
Matheus Santana De Brito <57404789+matheuss890@users.noreply.github.com>
2020-10-15 19:45:34 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Moto.java
@@ -0,0 +1,48 @@
+public class Moto {
+
+	public Object getQtdePneus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void desligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getVelocidade() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean estaEmMovimento() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void frear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean estaLigado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean estaEmMarchaAFrente() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
