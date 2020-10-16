0f3684bae048de95d9084c5a659556a7d777bf92
Matheus Santana De Brito <57404789+matheuss890@users.noreply.github.com>
2020-10-15 21:50:49 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,46 @@
+public class Veiculo {
+	
+	protected int velocidade;
+	protected int qtdePneus;
+
+	public int getVelocidade() {
+		// TODO Auto-generated method stub
+		return velocidade;
+	}
+
+	public int getQtdePneus() {
+		// TODO Auto-generated method stub
+		return qtdePneus;
+	}
+	
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void frear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void parar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void mover() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	protected boolean estaEmMovimento() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
