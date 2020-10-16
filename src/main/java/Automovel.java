001bf21612780a85338e76f8a208c9bb263b71f9
pripaula2002 <61168280+pripaula2002@users.noreply.github.com>
2020-10-16 17:51:14 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,72 @@
+public class Automovel {
+
+	public Object getQtdePneus() {
+		// TODO Auto-generated method stub
+		return 4;
+	}
+
+	public void desligar() {
+		// TODO Auto-generated method stub
+		if (estaLigado() == true) {
+		int velocidade =0;
+		if (velocidade > 0) {
+			parar();
+}
+}
+}
+	
+	public boolean estaEmMovimento() {
+		return estaEmMovimento();
+	}
+
+	public boolean estaLigado() {
+		// TODO Auto-generated method stub
+		return estaLigado();
+	}
+
+	public Object getVelocidade() {
+		// TODO Auto-generated method stub
+		return getVelocidade();
+	}
+
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+		if (estaLigado() == false) {
+			ligar();
+			}
+	}
+
+	public void frear() {
+		// TODO Auto-generated method stub
+		if (estaEmMarchaAFrente() == false) {
+			frear();
+			}
+	}
+
+	public boolean estaEmMarchaAFrente() {
+		// TODO Auto-generated method stub
+		return estaEmMarchaAFrente();
+	}
+
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+		if (estaLigado() == true) {
+			aumentarVelocidade();
+			}
+	}
+
+	public void ligar() {
+		// TODO Auto-generated method stub
+		if (estaLigado() == false) {
+}	
+}	
+	public void parar() {
+		// TODO Auto-generated method stub
+		if (estaLigado() == true) {
+			int velocidade =0;
+			if (velocidade > 0) {
+				parar();	
+	}
+}
+	}
+}
