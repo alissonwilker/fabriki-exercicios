d4a052f189686832a6d49ac547ae6930b44d77c8
Matheus Santana De Brito <57404789+matheuss890@users.noreply.github.com>
2020-10-15 21:50:48 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,17 @@
+public class Carro {
+	
+	public Object carro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void moverParaTras() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void engatarMachaARe() {
+		
+	}
+
+}
