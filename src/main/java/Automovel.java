14e2a0af00b1a29e9a86bcfd062ce3d70e613c28
AlmirMalheiros <56326125+AlmirMalheiros@users.noreply.github.com>
2020-10-16 19:37:38 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,38 @@
+public abstract class Automovel extends Veiculo {
+    protected boolean marchaAFrente;
+    private boolean ligado;
+    public void moverParaFrente() {
+        if (estaLigado() == false) {
+            ligar();
+        }
+        if (estaEmMarchaAFrente() == false) {
+            engateMarchaAFrente();
+        }
+        super.moverParaFrente();
+    }
+    public void ligar() {
+        if (ligado == false) {
+            ligado = true;
+        }
+    }
+    public void desligar() {
+        if (estaLigado() == true) {
+            if (velocidade > 0) {
+                parar();
+            }
+            ligado = false;
+        }
+    }
+    public boolean estaLigado() {
+        return ligado;
+    }
+    public boolean estaEmMarchaAFrente() {
+        return marchaAFrente;
+    }
+    protected void engateMarchaAFrente() {
+        if (marchaAFrente == false) {
+            if (velocidade > 0) {
+                parar();}
+            marchaAFrente = true;}
+    }
+}
