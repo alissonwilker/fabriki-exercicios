a0e2c9c4315c9d241c7f9baa413032d8cc86bd39
Matheus Santana De Brito <57404789+matheuss890@users.noreply.github.com>
2020-10-15 21:50:50 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,35 @@
+public class Automovel {
+	
+	protected boolean marchaAfrente;
+	private boolean ligado;
+
+	
+	public void moverParaFrente() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void ligar() {
+
+	}
+	
+	public void desligar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public boolean estaLigado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public boolean estaEmMarchaAFrente() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	protected void engatarMarchaAFrente() {
+		
+	}
+
+}
