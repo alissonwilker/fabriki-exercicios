1f3f69d51cc1705f9b3255cbc60c6aa11bfee8ec
AlmirMalheiros <56326125+AlmirMalheiros@users.noreply.github.com>
2020-10-16 19:22:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,25 @@
+/src/main/java/Carro.java:
+
+   public class Carro extends Automovel {
+    public Carro() {
+        qtdePneus = 4;
+    }
+    public void moverParaTras() {
+        if (estaLigado() == false) {
+            ligar();
+        }
+        if (estaEmMarchaAFrente() == true) {
+            engateMarchaARe();
+        }
+        if (estaEmMovimento() == false) {
+            mover();
+        }
+    }
+    protected void engateMarchaARe() {
+        if (marchaAFrente == true) {
+            if (estaEmMovimento() == true) {
+                parar();
+            }
+            marchaAFrente = false;}
+    }
+}
