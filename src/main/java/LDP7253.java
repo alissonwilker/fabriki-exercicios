777284d7be114ce15aabfe4687994198d3482f0b
ANDRE210383 <69171291+ANDRE210383@users.noreply.github.com>
2020-10-19 20:24:11 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,13 @@
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+
+{ for (int i = 0; i <= 100; ++i) 
+
+	System.out.println(i % 10 == 0 ? i + " - Multiplo de 10" : i);
+} 
+}
+}
