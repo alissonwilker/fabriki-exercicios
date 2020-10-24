58b38091ce3bf2392fab5b85ad5ef2a8eadaf988
Rafael Moura <rafaelmoura2929@gmail.com>
2020-10-24 17:06:52 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRestController.java
@@ -0,0 +1,52 @@
+package spring;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pessoaFisica")
+@CrossOrigin
+public class PessoaFisicaRestController {
+
+	@Autowired
+	private PessoaFisicaRepository repository;
+
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<PessoaFisica> getPessoaFisica() {
+		return repository.findAll();
+	}
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Optional<PessoaFisica> getPessoaFisica(@PathVariable Integer id) {
+		return repository.findById(id);
+	}
+
+	@PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody PessoaFisica savePessoaFisica(@RequestBody PessoaFisica pessoaFisica) {
+		return repository.save(pessoaFisica);
+	}
+
+	@PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody PessoaFisica updatePessoaFisica(@PathVariable String id,
+			@RequestBody PessoaFisica pessoaFisica) {
+		return repository.save(pessoaFisica);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public void deletePessoaFisica(@PathVariable Integer id) {
+		repository.deleteById(id);
+	}
+}
