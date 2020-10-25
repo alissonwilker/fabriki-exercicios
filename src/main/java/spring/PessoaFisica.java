7a7cc8868a375be733eba457f0ae3f31d8e59318
Rodrigo Ortega Tierno <54092368+Rodrigo-ort@users.noreply.github.com>
2020-10-24 22:21:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,32 @@
+package spring;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Classe que representa uma pessoa fisica.
+ *
+ */
+@Entity
+@Table(name = "tb_pessoafisica")
+
+public class PessoaFisica extends Cliente{
+	private String cpf;
+	
+	public PessoaFisica() {}
+	
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+		}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
