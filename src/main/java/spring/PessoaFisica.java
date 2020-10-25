74dbc69ef11e0cafab7675a95cfcef29dd3f9a04
Johnatan Brayan <johnatanbrayan1@gmail.com>
2020-10-25 15:38:29 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,31 @@
+package spring;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_fisica")
+public class PessoaFisica extends Cliente {
+
+  private String cpf;
+  
+  public PessoaFisica() {}
+
+	public PessoaFisica(String nome, String cpf) {
+    super(nome);
+		this.cpf = cpf;
+  }
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	// public String getNome() {
+	// 	return nome;
+  // }
+  
+}
