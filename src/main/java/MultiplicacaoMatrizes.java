b54399a1ef1413b955a055b139ce3a2cd27d8766
Rafael <66041171+Rafael1984rpm@users.noreply.github.com>
2021-01-25 15:55:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MultiplicacaoMatrizes.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+public class MultiplicacaoMatrizes
+{
+private static Scanner entrada = new Scanner(System.in);
+private static PrintStream saida = System.out;
+public static void main(String args[])
+{
+int l1, c1,c2,i,j,k,sum;
+Scanner in = new Scanner(System.in);
+l1 = in.nextInt();
+c1 = in.nextInt();
+c2 = in.nextInt();
+{
+int mat1[][] = new int[l1][c1]; 
+int mat2[][] = new int[c1][c2]; 
+int res[][] = new int[l1][c2];
+for (i= 0; i < l1; i++)
+{ 
+for (j= 0; j < c1; j++)
+mat1[i][j] = in.nextInt();
+}
+for (i= 0; i < c1; i++)
+{ 
+for (j= 0; j < c2; j++)
+mat2[i][j] = in.nextInt();
+}
+for (i= 0; i < l1; i++)
+for (j= 0; j < c2; j++)
+{
+sum=0;
+for (k= 0; k < c1; k++)
+{
+sum +=mat1[i][k]*mat2[k][j];
+}
+res[i][j]=sum;
+}
+for (i= 0; i < l1; i++)
+{
+for (j= 0; j < c2; j++)
+System.out.print(res[i][j]+"\n");
+System.out.println();
+}
+}
+}
+}
