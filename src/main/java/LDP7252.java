065650022cfa7e15d9bb8889146ac4dd51a40162
Ana-Sheila <74937489+Ana-Sheila@users.noreply.github.com>
2021-02-22 15:59:47 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,17 @@
+public class LDP7252 {
+
+	public static void main(String[] args) {
+
+		double totalGraos = 1;
+		double totalCasa = 1;
+		int casas = 64; //Quantidade de casas no tabuleiro
+
+		for (int i = 2; i <= casas; i++) {
+			
+			totalCasa = 2*totalCasa;
+			totalGraos = totalGraos + totalCasa;
+			
+		}
+		//System.out.println( "totalGraos = " + totalGraos); 
+	}
+}
