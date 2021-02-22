31387f6df80d87910ad1e0330ba9e19dcb5b974d
julioCesarRocha <74937504+julioCesarRocha@users.noreply.github.com>
2021-02-22 16:59:48 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,21 @@
+import java.math.BigDecimal;
+
+public class LDP7252 {
+
+	public static void main(String[] args) {
+					
+		BigDecimal dois = new BigDecimal(2);
+		
+	
+		BigDecimal soma = BigDecimal.ZERO;
+		
+		for (int i = 0; i <=63; i++) { //VERIFICAR TROCA POR 64
+			
+			
+			soma = soma.add(dois.pow(i));
+			
+		}
+			
+		System.out.println("totalGraos= " +soma);
+}
+}
