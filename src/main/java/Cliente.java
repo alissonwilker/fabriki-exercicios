6766207e5ac8e71acc6885b0a5d1e9bcc0e17ef8
Alyson Gomes <75434878+Alysongds@users.noreply.github.com>
2021-03-04 15:25:50 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,12 @@
+public abstract class Cliente extends java.lang.Object {
+	private java.lang.String nome;
+
+	protected Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public java.lang.String getNome() {
+		return nome;
+	}
+}
