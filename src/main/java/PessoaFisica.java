88ce7dbd5b99835efe3ed08fdf91bec5ae6548ad
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2021-03-04 15:32:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,14 @@
+public class PessoaFisica extends Cliente{
+
+	private String cpf;
+	
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+}
