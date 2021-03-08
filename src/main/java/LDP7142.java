ad1de529bbdc0df9d9255173662b2c074a164921
Rafael <66041171+Rafael1984rpm@users.noreply.github.com>
2021-03-08 15:11:53 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7142.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LDP7142{
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int C,H,E;
+			E=0;
+			C = entrada.nextInt();
+			H = entrada.nextInt();
+			if(H>50){
+				E = H-50;
+			}
+			System.out.println("código = " + C);
+			System.out.println("salarioExcedente = " +(E*20));
+			System.out.println("salarioTotal = " +(E*20+(H-E)*10));
+	}
+}
