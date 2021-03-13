977c55e7ac996563abf3b97b91b613968a871ba9
guilhermerodriguesdasilveira <53486686+guilhermerodriguesdasilveira@users.noreply.github.com>
2021-03-12 23:18:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,31 @@
+package spring;
+
+import javax.persistence.Column;
+
+public class PessoaFisica extends Cliente {
+	
+	
+    @Column
+    private String cpf; 
+
+    public PessoaFisica() {}
+
+
+    public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+   
+    public String getCpf() {
+        return cpf;
+    }
+
+  
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
+
+}
