c2528852c5f440fc068503ea86437cc3e1eb237c
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2021-03-12 22:41:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,34 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoaFisica")
+public class PessoaFisicas{
+
+    @Id
+    @Column(updatable = true, nullable = false, unique = true)
+    private String cpf;
+    private String name;
+
+
+	public PessoaFisica(String cpf, String name) {
+		this.cpf = cpf;
+                this.name = name;
+	}
+   
+     public PessoaFisica() {
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
