4a492e16ef2549ae41334200d17f67b1ea1f5588
tiago-siqueira <39934523+tiago-siqueira@users.noreply.github.com>
2021-03-13 19:32:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,39 @@
+public class PessoaFisica extends PessoaFisicaRestController {
+
+	private String cpf = "";
+	//private String nome= "";
+	PessoaFisica pessoafisica = new PessoaFisica();
+
+	public PessoaFisica() {
+
+	}
+
+public PessoaFisica(pessoafisica, cpf) {
+		return this.cpf;
+		return PessoaFisica pessoafisica;
+	}
+
+public String getCpf() {
+	return cpf;
+}
+
+public void setCpf(String cpf) {
+	this.cpf = cpf;
+}
+
+public PessoaFisica getId() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public PessoaFisica getName() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public void setName(PessoaFisica name) {
+	// TODO Auto-generated method stub
+
+}
+
+}
