918a1d1ac20e7fa1bb61be047e3ea4ac7981df88
guilhermerodriguesdasilveira <53486686+guilhermerodriguesdasilveira@users.noreply.github.com>
2021-03-12 23:18:09 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRepository.java
@@ -0,0 +1,16 @@
+package spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repositorio de entidades <code>ContaBancaria</code>.
+ */
+
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Integer> {
+
+}
+
+
+
+
+
