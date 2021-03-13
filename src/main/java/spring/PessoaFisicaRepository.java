a03e32594a26814e3d83e17bfb7d9bb68f2673e2
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2021-03-12 22:54:14 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRepository.java
@@ -0,0 +1,5 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisicas, Integer> { }
