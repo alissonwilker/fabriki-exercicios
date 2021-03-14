f3781597a68ec614f8e52fd646f98b0dacc8440a
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2021-03-13 21:57:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,59 @@
+package spring;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoaFisica")
+public class PessoaFisica extends Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+    @Column(updatable = true)
+    private String cpf;
+
+
+	public PessoaFisica(String cpf, String nome) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+               public PessoaFisica() {
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaFisica other = (PessoaFisica) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		return true;
+	}
+}
