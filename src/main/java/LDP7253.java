0cb74e4a6407b6b4c2fe83039b143c3f62338022
Rafael <66041171+Rafael1984rpm@users.noreply.github.com>
2021-03-17 19:08:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class LDP7253
+{
+	public static void main(String[] args) {       
+	int Mult;
+	    for (int i = 0; i<= 100; i++){
+	     if(i % 10 == 0){
+	         System.out.println("Multiplo de 10");
+	     }
+	    }
+	}
+}
