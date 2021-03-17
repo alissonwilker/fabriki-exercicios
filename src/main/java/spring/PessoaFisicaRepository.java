4c43754e86b90884105bfce71085c8ef40bb7c1c
maycobrito <42880612+maycobrito@users.noreply.github.com>
2021-03-17 19:34:06 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRepository.java
@@ -0,0 +1,8 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Interface PessoaFisicaRepository
+ */
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Integer>{
+
+}
