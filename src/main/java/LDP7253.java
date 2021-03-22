9b4f1029fd7bb813e9bba175c8318f0f8033c775
Gilvan Gomes <58432723+gilvaz1@users.noreply.github.com>
2021-03-22 17:12:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,13 @@
+public class LDP7253 {
+
+	public static void main(String[] args) {
+
+		int Contagem = 1;
+		for (int contador = 101; Contagem < contador; Contagem++) {
+
+			if (Contagem % 10 == 0) {
+				System.out.println("Multiplo de 10");
+			}
+		}
+	}
+}
