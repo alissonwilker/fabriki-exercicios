b2e3aa1019b9b4bc7de38744b11a998812835368
guinsf <38284477+guinsf@users.noreply.github.com>
2021-05-24 19:56:08 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,18 @@
+src/main/java/PessoaFisica.java
+
+
+public class PessoaFisica extends Cliente {
+ private static final long serialVersionUID = 1L;
+ private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+
+   public String getCpf() {
+       return cpf;
+   }
+
+}
