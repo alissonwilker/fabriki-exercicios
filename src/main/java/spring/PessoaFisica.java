51e660dd4111577d3ecb619e8f28c05a8cd3d7cf
Felipe Barros <felipeb4c@gmail.com>
2021-05-24 20:18:08 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,27 @@
+package spring;
+
+import javax.persistence.Entity;
+
+@Entity
+public class PessoaFisica extends Cliente{
+
+	private String cpf;
+	
+	public PessoaFisica() {
+		
+	}
+
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
