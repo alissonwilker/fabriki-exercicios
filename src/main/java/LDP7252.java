5a20575197bf3a80a1285e381aff4811887d1720
Alisson Wilker <alissonwilker@gmail.com>
2021-05-25 22:03:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author Vitor
+ */
+public class LDP7252 {
+
+    
+    public static void main(String[] args) {
+       
+      
+        double totalGraos=1;
+        
+       for(int i=1;i<=64;i++){
+         
+       totalGraos=Math.pow((1*2), i);
+       
+       totalGraos++;
+       }
+       
+        System.out.println("totalGraos = "+totalGraos);
+    }
+    
+}
