4d78ef041ddce6a9fbdc927b8d9dbc1a92990bbd
luucasfreitas <72211846+luucasfreitas@users.noreply.github.com>
2021-05-27 20:25:33 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SomaDoisNumeros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+	public static void main(String[] args) {
+		Scanner lerNumeros = new Scanner(System.in);
+		int n1 = lerNumeros.nextInt();
+		int n2 = lerNumeros.nextInt();
+		
+		int soma = n1 + n2;
+		
+        System.out.printf("A soma dos números é %d", soma);
+		
+		
+	}	
+}
