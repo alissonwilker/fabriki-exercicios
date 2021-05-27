28d6d1944482b91d56f4589182ef08a3c0a11cbf
Guilherme Marçal Calisto <58859672+MarcalFilosofo@users.noreply.github.com>
2021-05-27 20:09:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SomaDoisNumeros.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		System.out.println("Digite o primeiro número: ");
+		String n1 = entrada.nextLine();
+
+		System.out.println("Digite o segundo número: ");
+		String n2 = entrada.nextLine();
+		
+		double numero1 = Double.parseDouble(n1);
+		double numero2 = Double.parseDouble(n2);
+
+		double total = numero1 + numero2;
+		
+		System.out.println("Total: " + total);
+		entrada.close();
+
+	}
+}
