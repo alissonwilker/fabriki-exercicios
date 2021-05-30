d3eff1b3432a82b8a0609b00c60f34c32ad41f10
jeffersonxramalho <84602708+jeffersonxramalho@users.noreply.github.com>
2021-05-30 16:20:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SomaDoisNumeros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SomaDoisNumeros
+ {
+public static void main(String[] args) 
+{
+Scanner scanner = new Scanner(System.in);
+
+int n1 = lernumeros.nextlnt();
+int n2 = lernumeros.nextlnt();
+int soma = n1+n2;
+
+System.out.println(soma);
+
+}
