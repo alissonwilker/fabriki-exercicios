0ba6e9c8bd02c6391ce244d507f6b65bdcc41d55
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2021-06-10 19:08:53 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class LDP7144 {
+
+	public static void main(String[] args)
+	{
+
+        Scanner in;
+        in = new Scanner(System.in);
+        int x;
+        int y;
+        x = in.nextInt();
+        
+        System.out.println(x % 2 == 0);
+	}
+}
