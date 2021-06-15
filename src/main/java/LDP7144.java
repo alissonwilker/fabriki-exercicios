4bad780e6fe72702f70085fbc4bfa2e9175f8d47
alineom <84157249+alineom@users.noreply.github.com>
2021-06-14 21:01:03 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class  LDP7144 {
+public static void main(String[] args) {	
+	int a = 0;
+	Scanner num = new Scanner( System. in );
+	System.out.print("Informe um número inteiro: ");
+	a= num.nextInt();
+	if ((a % 2 == 0) && (a>0)) {
+	System.out.println("PAR E POSITIVO");
+	} else if ((a % 2 == 0) && (a<0)) {
+	System.out.println("PAR E NEGATIVO");
+	} else if ((a % 2 != 0) && (a>0)) {
+		System.out.println("IMPAR E POSITIVO");
+	} else if ((a % 2 != 0) && (a<0)) {
+		System.out.println("IMPAR E NEGATIVO");
+	} else if (a == 0)  {
+		System.out.println("PAR E POSITIVO");
+	}
+}
+}
