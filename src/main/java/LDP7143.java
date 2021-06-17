58fae96172d2733a95cfd95187cc6305ba96c937
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2021-06-17 19:00:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LDP7143 {
+
+	private static Scanner in;
+
+	public static void main(String[] args)
+	{
+		int a,b,c,d;
+		int c2;
+		in = new Scanner(System.in);
+		a = in.nextInt();
+		b = in.nextInt();
+		c = in.nextInt();
+		d = in.nextInt();
+		
+		c2 = (int) Math.pow(c, 2);
+		
+		if(c2 >= 1000) {
+			System.out.println(c2);
+		}else {
+			System.out.println("a2 = " + (int) Math.pow(a, 2));
+			System.out.println("b2 = " +(int) Math.pow(b, 2));
+			System.out.println(c2);
+			System.out.println("d2 = " + (int) Math.pow(d, 2));
+		}
+	}
+}
