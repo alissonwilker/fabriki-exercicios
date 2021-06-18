71a67e893df0f6d769dfcc262995c0fd1a192752
CarlosVictor02 <83988580+CarlosVictor02@users.noreply.github.com>
2021-06-17 22:20:44 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,16 @@
+public class PessoaJuridica extends Cliente {
+	
+	private String cnpj;
+
+	public PessoaJuridica(String cnpj, String nome) {
+		
+		super (nome);
+		this.cnpj=cnpj;
+		
+	}
+	
+	public String getCnpj() {
+		return this.cnpj;
+	}
+
+}
