6c003190905cc36d53b409b082a7c2c89b7c0760
rodrigosabcunha <79269308+rodrigosabcunha@users.noreply.github.com>
2021-06-20 16:11:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LDP7143 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner LerNumero  = new Scanner(System.in);
+		
+			int N1 = LerNumero.nextInt();
+			int N2 = LerNumero.nextInt();
+			int N3 = LerNumero.nextInt();
+			int N4 = LerNumero.nextInt();
+		
+		
+			int Q1 = N1 * N1;
+			int Q2 = N2 * N2;
+			int Q3 = N3 * N3;
+			int Q4 = N4 * N4;
+			
+				if(Q3 >= 1000) {
+					System.out.println(Q3);
+					
+				} else {
+					System.out.println("O quadrado de N1  é igual " + Q1);
+					System.out.println("O quadrado de N2  é igual " + Q2);
+					System.out.println("O quadrado de N3  é igual " + Q3);
+					System.out.println("O quadrado de N4  é igual " + Q4);
+		
+	    }
+	}
+}
