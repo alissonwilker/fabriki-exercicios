d1962edbec8bad86764fd616108f7852a0619920
Rodrigo Dourado da Silva <72164704+RodrigoDouradoS@users.noreply.github.com>
2021-06-20 10:47:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,29 @@
+
+public class ContaBancaria {
+
+	public ContaBancaria(Cliente pessoaFisica) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getSaldo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void depositar(int valorDeposito) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sacar(int valorSaque) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Object getCliente() {
+		//// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
