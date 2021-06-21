6502ecc013c5f4f8661856859d25af32c5b1264c
jeffersonxramalho <84602708+jeffersonxramalho@users.noreply.github.com>
2021-06-21 16:32:21 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LDP7143 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner LerNumero  = new Scanner(System.in);
+
+			int A = LerNumero.nextInt();
+			int B = LerNumero.nextInt();
+			int C = LerNumero.nextInt();
+			int D = LerNumero.nextInt();
+
+
+
+
+				if(C * C >= 1000) {
+				System.out.println("c2 = " + C * C);
+
+				} else {
+					     System.out.println("a = "+A);
+					     System.out.println("b = "+B);
+					     System.out.println("c = "+C);
+					     System.out.println("d = "+D);
+					     System.out.println("a2 = "+ (A*A) );
+					     System.out.println("b2 = "+ (B*B) );
+					     System.out.println("c2 = "+ (C*C) );
+					System.out.println("d2 = "+ (D*D) );
+	    }
+	}
+}
