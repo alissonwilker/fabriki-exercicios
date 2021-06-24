76f048b1af42bfe37f09ab46d25bcd8c9ee8b135
Guilherme Marçal <58859672+MarcalFilosofo@users.noreply.github.com>
2021-06-24 17:42:25 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,16 @@
+public class LDP7141 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int quilosPeixes = entrada.nextInt();
+		
+		int excedente = (quilosPeixes - 50 < 0 ? 0 : quilosPeixes - 50);
+		
+		int multa = excedente * 4;
+		
+		System.out.println("excedente = " + excedente);
+		System.out.println("multa = " + multa);
+		
+		entrada.close();
+	}
+}
