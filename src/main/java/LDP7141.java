1d6cc48376bbe2bbffceebd347b1f4a67954aac3
wendelmaxuel <72840731+wendelmaxuel@users.noreply.github.com>
2021-06-26 23:11:41 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,18 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7141 {
+	private static Scanner entrada = new Scanner(System.in);
+	private static PrintStream saída = System.out;
+	
+	public static void main(String[] args) {
+		pesos = input('Digite a quantidade:')
+				if pesos > 50:
+				    multa = (pesos - 50) * 4
+				    print ('4,00: {}'.format(multa))
+				else:
+				    print ('20')
+
+	}
+
+}
