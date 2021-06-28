3aaad4965a7c7c3a3058fe28016c6151a98ba16c
Vplayer-coder <84549124+Vplayer-coder@users.noreply.github.com>
2021-06-28 19:55:49 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LDP7141 {
+    public static void main(String[] args) throws Exception {
+        Scanner Entrada = new Scanner(System.in);
+        int Peso = Entrada.nextInt();
+        Entrada.close();
+        int Exc = Peso - 50;
+        int Mu = Exc * 4;
+
+        if(Peso <= 50){
+            System.out.println("excedente = 0");
+            System.out.println("multa = 0");
+        }
+        else {
+            System.out.println("excedente = " Exc);
+            System.out.println("multa = " Mu);
+        }
+    }
+}
