121b5c4b15ff93ac86e9a279a5d98ef431af75ae
wendelmaxuel <72840731+wendelmaxuel@users.noreply.github.com>
2021-07-19 23:08:32 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,13 @@
+public class LDP7251 {
+  public static void main(String[] args) {
+	  for(int x = 0; x <= 10; x ++)
+	  {
+		  System.out.println();
+		  System.out.print("Numero");
+		  if(x == 5)
+		  {
+			  continue;
+			  }
+		  System.out.print(x);}
+		  }
+	  }
