86c8f55eaf125bbefcbcdaa0a529601ac043f704
Josué Viana <josuep.viana@gmail.com>
2021-08-02 19:36:58 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,12 @@
+public class PessoaFisica extends Cliente {
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
