6f4c11cdda122057caa102aefc024c6a8e62da38
vitorgaspar72 <84208595+vitorgaspar72@users.noreply.github.com>
2021-08-04 20:56:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,24 @@
+import java.math.BigDecimal;
+
+public class LDP7252 {
+
+    public static void main(String[] args) {
+
+
+        int i;
+        double totalGraos =1;
+        double inicio = 1;
+
+
+        for(i = 2; i <= 64; i++){
+
+            totalGraos = totalGraos +(Math.pow(2,i));
+
+        }
+        BigDecimal somatorio =BigDecimal.valueOf( totalGraos);
+
+        System.out.println("totalGraos = "+ somatorio.toBigInteger());
+
+
+
+    }
