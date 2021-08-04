cf84a82fa93673a53cbc26ee2a58b068c95c4ebf
Guilherme Marçal <58859672+MarcalFilosofo@users.noreply.github.com>
2021-08-04 20:35:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,12 @@
+
+public class LDP7252 {
+	public static void main(String[] args) {
+		Double graosDeTrigo = 1.0;
+		Double somatorio = 0.0;
+		for (int i=1; i<64;i++) {
+			graosDeTrigo = 2 * graosDeTrigo;
+			somatorio += graosDeTrigo;
+		}
+		System.out.println(somatorio);
+	}
+}
