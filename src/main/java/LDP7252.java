609dc5e99c6ec6d77a6e5f11c2a19de0f9ecc401
luucasfreitas <72211846+luucasfreitas@users.noreply.github.com>
2021-08-04 21:39:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,12 @@
+public class LDP7252
+{
+	public static void main(String[] args) {
+	    
+	    long numero = 1;
+
+	    for(int i = 1; i < 64; i++){
+	        numero = numero*2;
+	    }
+        System.out.println("totalGraos = "+numero);
+	}
+}
