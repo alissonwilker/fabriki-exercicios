3fd1afcbbbb827c7942c9c67b52675b91bcd46c9
oliveira-marcos2 <53592561+oliveira-marcos2@users.noreply.github.com>
2021-08-07 23:05:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,12 @@
+public class PessoaFisica extends Cliente {
+	public String cpf;
+
+	public PessoaFisica(String string, String string2) {
+		
+	}
+
+	public String getCpf() {
+		return this.cpf;
+	}
+
+}
