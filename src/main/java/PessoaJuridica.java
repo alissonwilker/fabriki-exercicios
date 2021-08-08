f689e221329137fb1151e9caeb52d68bac3b15ab
MrMateusFonteles <54007149+MrMateusFonteles@users.noreply.github.com>
2021-08-08 18:59:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,14 @@
+public class PessoaJuridica extends Cliente {
+
+private String cnpj;
+
+public String getCnpj() {
+	return cnpj;
+}
+
+public PessoaJuridica(String nome, String cnpj) {
+	super(nome);
+	this.cnpj = cnpj;
+}
+
+}
