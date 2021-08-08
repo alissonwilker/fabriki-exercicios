ac4c6ed8b411009acb7d856b41423e9db3c2a2e3
MrMateusFonteles <54007149+MrMateusFonteles@users.noreply.github.com>
2021-08-08 19:33:58 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+
+public abstract class Cliente {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	protected Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+}
