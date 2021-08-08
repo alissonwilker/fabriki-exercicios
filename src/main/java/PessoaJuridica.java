e02eebd4e1ee1e7c8b777c472fa37c3047184719
oliveira-marcos2 <53592561+oliveira-marcos2@users.noreply.github.com>
2021-08-07 23:05:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,12 @@
+public class PessoaJuridica extends Cliente {
+	public String cnpj;
+
+	public PessoaJuridica(String string, String string2) {
+		
+	}
+
+	public String getCnpj() {
+		return this.cnpj;
+	}
+
+}
