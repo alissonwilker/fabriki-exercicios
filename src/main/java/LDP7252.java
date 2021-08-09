bb1b0898cdccf529e9a0024bd8928844d44ec962
Jefferson Ramalho <dev.jeffersoon@gmail.com>
2021-08-09 20:43:19 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+public class LDP7252 {
+
+    public static void main(String[] args) {
+        BigDecimal DOIS = new BigDecimal(2);
+        BigDecimal soma = BigDecimal.ZERO;  
+
+        for (int i = 0; i <= 63; i++) {  
+
+            soma = soma.add(DOIS.pow(i));  
+
+        }  
+
+        System.out.println("totalGraos = "+soma);  
+    }
+
+}
