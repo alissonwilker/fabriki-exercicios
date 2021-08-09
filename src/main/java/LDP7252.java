04328c0aa299f7bf4168ffa60f91b20c27f4d67a
rodrigosabcunha <79269308+rodrigosabcunha@users.noreply.github.com>
2021-08-09 19:47:29 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+public class LDP7252 {
+
+          public static void main(String[] args) {
+                     BigInteger graos = new BigInteger (" 1 ") ;
+                     BigInteger soma = new BigInteger (" 0 ") ;
+                           soma.add(graosDeTrigo); 
+                           for (int i = 0 ; i < = 63 ; i++)  {
+                             graosDetrigo = graosDeTrigo.multiply(new BigInteger("2")) ;
+                             soma = soma.add (graosDeTrigo);
+                     }
+
+                  
+                    System.out.println( "totalGraos = "+soma.add(new BigInteger("1"))) ;
+
+              }
+
+      }
+                     
