bb95fd7299271898f697b3bca6c55ffd58c4e319
brunoaguiarmazoni <51519820+brunoaguiarmazoni@users.noreply.github.com>
2021-08-15 05:12:15 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,18 @@
+
+public class PessoaFisica extends Cliente {
+
+	private String cpf;
+	
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	
+
+}
