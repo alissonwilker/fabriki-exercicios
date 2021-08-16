ed2f603e1911a62fa3e9cd5fa2c58c988dd3d575
rodrigosabcunha <79269308+rodrigosabcunha@users.noreply.github.com>
2021-08-16 19:39:26 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,44 @@
+import java.util.Arraylist;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class SubtracaoConjuntos {
+
+       public static void main(String[] args) {
+            List<Integer>A = new ArrayList<>();
+            List<Integer>B = new ArrayList<>();
+
+       Scanner entrada = new Scanner(System.in);
+        Integer num;
+        do { 
+                num = entrada.nextInt();
+                A.add(num);
+             }while (num != 0);
+
+         do { 
+                num = entrada.nextInt();
+                B.add(num);
+             }while (num != 0);
+
+              List<Integer> AB = new ArrayList<>();
+
+          for (Integer elementoA : A) { 
+             boolean estaEmA = false;
+          for (Integer elementoB : B) {
+               if(elementoB == elementoA) {
+                   estaEmA = true;
+           }
+     }
+               if(!estaEmA) {
+                   AB.add(elementoA);
+
+           }
+     }
+          for (Integer elementoAB : AB) {
+                System.out.println(elementoAB);
+
+           }
+      }
+  }  
+              
