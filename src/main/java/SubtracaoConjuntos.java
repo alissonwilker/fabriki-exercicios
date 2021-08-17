ed744bbb0d89dc30e09baeae7c33e855fdec0fef
alineom <84157249+alineom@users.noreply.github.com>
2021-08-16 23:36:09 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,27 @@
+package sintaxe_variaveis_fluxo;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class SubtracaoConjuntos{
+			public static void main(String[] args) {
+		    	Scanner valores = new Scanner(System.in);
+		    	int num = valores.nextInt();
+		    	ArrayList<Integer> conjuntoA = new ArrayList<Integer>();
+		    	ArrayList<Integer> conjuntoB = new ArrayList<Integer>();
+		    	while (num!=0){
+		        	conjuntoA.add(num);	 		
+		        	num = valores.nextInt();
+		        }   
+	        		    	    	int num2 = valores.nextInt();
+		    	while (num2!=0){
+		        	conjuntoB.add(num2);	 		
+		        	num2 = valores.nextInt();
+		        } 	
+		    			    	valores.close();
+		    			    	for(int i = 0; i < conjuntoB.size(); i++) {
+		    		if (conjuntoA.contains(conjuntoB.get(i))) {
+		    			conjuntoA.remove(conjuntoB.get(i));
+		    			}
+		    	}
+		      	for(int i = 0; i < conjuntoA.size(); i++) {
+		        	  System.out.println(conjuntoA.get(i));}
+			}}
