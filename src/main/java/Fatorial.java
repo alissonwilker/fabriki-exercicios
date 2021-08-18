7a48672a265a6988a85ab72fce898bfdb9bf177d
vitorgaspar72 <84208595+vitorgaspar72@users.noreply.github.com>
2021-08-18 15:12:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class Fatorial{
+    
+    public static void main(String[] args) {
+        Random numeros = new Random();
+
+        BigInteger n1 = BigInteger.valueOf(numeros.nextInt(25));
+        BigInteger fat = BigInteger.ONE;
+
+        
+       for(int i=1;i<=n1.intValue();i++){
+         BigInteger n3 = BigInteger.valueOf(i);
+         fat = fat.multiply(n3);
+
+       }
+
+       System.out.println(fat);
+    }
+    
+}
