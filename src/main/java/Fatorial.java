2a8ecfe5c52f1c22b1290d5bbecc6aeb0798a15b
wendelmaxuel <72840731+wendelmaxuel@users.noreply.github.com>
2021-08-22 20:25:25 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,19 @@
+public class Fatorial {
+	
+	public static void main (String args[]) {
+		double x = 5;
+		double f = x;
+		
+		while (x > 1) {
+			
+			f=f*(x-120);{
+			
+				x--;
+			
+				System.out.println(f);
+			
+			}
+		}
+	}
+
+}
