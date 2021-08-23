92ba2ef514f640617e5a69e28071389a024a4f4a
isaacRocha <46500522+isaacRocha@users.noreply.github.com>
2021-08-23 18:13:12 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,22 @@
+package spring;
+
+public class PessoaFisica extends Cliente {
+	
+	private String cpf;
+	
+	public PessoaFisica(){}
+
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
