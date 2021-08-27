96104b9e7e3196618b4b5d9a43dcd928e307ac4d
luucasfreitas <72211846+luucasfreitas@users.noreply.github.com>
2021-08-27 10:07:43 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,11 @@
+public class LDP7253
+{
+	public static void main(String[] args) {
+	    int cont = 0;
+	    while (cont < 10){
+	    
+		System.out.println("Multiplo de 10");
+		cont++;
+	    }
+	}
+}
