b194506791c67ce7465e80510c70fb53bc8052a5
RaquelLAB <61168116+RaquelLAB@users.noreply.github.com>
2021-08-26 22:54:34 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,29 @@
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+
+@Column
+@Table(name = "tb_pessoafisica")
+public class PessoaFisica extends Cliente {
+
+    private String cpf;
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(String nome, String cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    
+}
