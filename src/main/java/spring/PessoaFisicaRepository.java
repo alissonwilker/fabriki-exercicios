47ab80a37dcc74284fea7b993aa0dcfbb0432148
oliveira-marcos2 <53592561+oliveira-marcos2@users.noreply.github.com>
2021-08-28 11:07:24 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRepository.java
@@ -0,0 +1,8 @@
+package spring;
+
+/**
+ * Repositorio de entidades <code>PessoaFisica</code>.
+ */
+public interface PessoaFisicaRepository extends org.springframework.data.jpa.repository.JpaRepository<PessoaFisica,Integer>{
+	
+}
