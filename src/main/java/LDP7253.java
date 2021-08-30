b209bf3b10f65d4ce7a39aab09a3ff4c27411276
Samuel-Gouvea <84049041+Samuel-Gouvea@users.noreply.github.com>
2021-08-30 15:53:37 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,11 @@
+public class LDP7253 {
+    public static void main(String[] args) {
+        int cont = 0, num = 0, soma = 0;
+        while(cont < 100){
+            soma += num +10;
+            System.out.println("Multiplo de 10");
+            cont += 10;
+        }
+    }
+
+}
