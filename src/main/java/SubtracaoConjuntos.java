cc2151006f1fad4970a1ed97c73f849f363ea3fc
Vplayer-coder <84549124+Vplayer-coder@users.noreply.github.com>
2021-09-01 22:31:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SubtracaoConjuntos {
+    private static Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Set<Integer> conjuntoA = new HashSet<>();
+        Set<Integer> conjuntoB = new HashSet<>();
+
+        preencherConjunto(conjuntoA);
+
+        preencherConjunto(conjuntoB);
+
+        entrada.close();
+
+        conjuntoA.removeAll(conjuntoB);
+
+        imprimirConjunto(conjuntoA);
+    }
+
+    private static void imprimirConjunto(Set<Integer> conjunto) {
+        Iterator<Integer> iterador = conjunto.iterator();
+        while (iterador.hasNext()) {
+            System.out.println(iterador.next());
+        }
+    }
+
+    private static void preencherConjunto(Set<Integer> conjunto) {
+        int elemento;
+        while ((elemento = entrada.nextInt()) != 0) {
+            conjunto.add(elemento);
+        }
+    }
+}
