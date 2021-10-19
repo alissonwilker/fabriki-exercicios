04aee1498f6ff6de826d326afe6bfbd2bb1cda49
fabioalves95472 <82236304+fabioalves95472@users.noreply.github.com>
2021-10-19 20:44:01 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,9 @@
+public class LDP7252 {
+    public static void main (String[] args) {
+    double qtdGraos = 1;
+    for (int i=0; i<63; i++) {
+        qtdGraos = qtdGraos * 2;
+    }
+    System.out.println("totalGraos = "+qtdGraos);
+    }
+}
