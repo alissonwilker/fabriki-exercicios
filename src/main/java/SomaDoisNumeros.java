f98cb12dff966a985e5f13f1bf3085ff266e74c7
Raffael88 <91996712+Raffael88@users.noreply.github.com>
2021-11-29 22:45:19 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SomaDoisNumeros.java
@@ -0,0 +1,26 @@
+package exemplo001;
+
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+		public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int num1, num2, soma;
+		
+		System.out.println("Digite um número inteiro: ");
+		num1 = teclado.nextInt();
+		
+		System.out.println("Digite um outro número inteiro: ");
+		
+		num2 = teclado.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println(soma);
+
+		
+		
+		
+	}
