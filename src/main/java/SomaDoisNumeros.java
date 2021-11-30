49716b6dd650e6a8e40f3003865ad6a6131a89a2
HenriqueBolzan <91273796+HenriqueBolzan@users.noreply.github.com>
2021-11-29 23:39:36 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SomaDoisNumeros.java
@@ -0,0 +1,15 @@
+public class SomaDoisNumeros {
+
+                     Public static void main(String[] args){
+                                Scanner teclado = new Scanner(System.in);
+                                int n1, n2, resultado;
+                               
+                              System.out.ptinln("Digite o primeiro número: ");
+                              n1 = teclado.nextInt();
+
+                              System.out.ptinln("Digite o segundonúmero: ");
+                              n2 = teclado.nextInt();
+
+                               System.out.println(resultado = n1+n2);
+     }
+}
