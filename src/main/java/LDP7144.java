6452e1b07f303324e208822dceb191f1a8161855
Isamara Galvão <79230443+isamarags@users.noreply.github.com>
2021-12-02 10:37:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LDP7144 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int n1;
+		System.out.printf("Digite o valor do numero: ");
+		n1 = in.nextInt();
+
+		if(n1 % 2 == 0 ) {
+			System.out.printf("NÚMERO PAR" + " E ");	
+		}
+		else {
+			System.out.printf("NÚMERO ÍMPAR" + " E ");	
+		}
+		if(n1 >= 0) {
+			System.out.printf("POSITIVO\n");	
+		}
+		else {
+			System.out.printf("NEGATIVO\n");	
+		}
+
+	}
+
+}
