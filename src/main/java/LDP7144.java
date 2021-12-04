400e523c7ecf20480acb07b655c204195b58c77b
Wald <92488364+waldtsi@users.noreply.github.com>
2021-12-04 16:59:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LDP7144{
+	private static Scanner teclado = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int num = teclado.nextInt();
+		teclado.close();
+		if((num>0) && (num%2==0)){
+			System.out.println("PAR E POSITIVO");
+		}
+		if((num>0) && (num%2!=0)){
+			System.out.println("ÍMPAR E POSITIVO");
+		}
+		if((num<0) && (num%2==0)){
+			System.out.println("PAR E NEGATIVO");
+		}
+		if((num<0) && (num%2!=0)){
+			System.out.println("ÍMPAR E NEGATIVO");
+		}
+	}
+}
