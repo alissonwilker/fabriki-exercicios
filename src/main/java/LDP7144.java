c41318b3dfd429509623f6a1c2f92aef68bc6d98
GABRIEL PEIXOTO GUIMARÃES <91996679+Guimaboy14@users.noreply.github.com>
2021-12-05 16:40:43 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LDP7144 {
+		public static void main(String[] args) {
+			Scanner entrada = new Scanner(System.in);
+			int numero;
+			
+				System.out.println();
+				numero = entrada.nextInt();
+
+	                        if(numero % 2 == 0){
+	                                 System.out.print("PAR ");
+	                                 }
+				else{
+					System.out.print("ÍMPAR");
+				}
+				if(numero<0){
+					System.out.print(" E NEGATIVO");
+					
+				}else{ 
+					System.out.print(" E POSITIVO"); 
+	}
+	}
+	}
