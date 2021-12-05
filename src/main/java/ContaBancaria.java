36e850e8cbcb439eef81f8598ef4cc9736e6d643
LucasMoreirac <74937584+LucasMoreirac@users.noreply.github.com>
2021-12-05 18:30:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,42 @@
+public class ContaBancaria {
+	
+	private Cliente cliente;
+	private static int contador;
+	private int id;
+	private int saldo;
+	
+	public ContaBancaria(Cliente cliente) {
+		super();
+		ContaBancaria.contador++;
+		this.id = ContaBancaria.contador;
+		this.cliente = cliente;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+	
+	public int depositar(int valor) {
+		if(valor >= 0) {
+			this.saldo += valor;
+		}
+		
+		return this.saldo;
+	}
+	
+	public int sacar(int valor) {
+		if(valor >=0 && valor < this.saldo) {
+			this.saldo -= valor;
+		}
+		return this.saldo;
+	}
+	
+}
