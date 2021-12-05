aae9d94016b55bb36232e9c50d8e8fb30c3cbe1c
GuilhermeLucas2299 <91921374+GuilhermeLucas2299@users.noreply.github.com>
2021-12-05 07:02:13 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,11 @@
+public class PessoaJuridica extends Cliente {
+	private java.lang.String cnpj;
+    public PessoaJuridica(java.lang.String nome, java.lang.String cnpj) {
+		public java.lang.String getCnpj();
+	}
+	private void getCnpj() { 
+			}
+
+	
+
+}
