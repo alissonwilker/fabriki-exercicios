9394915a0569d25cea557026a2cc5af94f36a346
wendelmaxuel <72840731+wendelmaxuel@users.noreply.github.com>
2021-12-06 09:06:56 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LDP7144 {
+		public static void main(String[] args) throws IOException {
+			Scanner leitor = new Scanner(System.in);
+			int numero = leitor.nextInt();
+
+	            if(numero % 2 == 0){
+	            System.out.println("PAR");
+	            }
+				else{
+				 System.out.println("ÍMPAR") ;
+				}
+				if(numero<0){
+				System.out.println(" E NEGATIVO");
+			}
+				else{ 
+				System.out.println(" E POSITIVO"); 
+		}
+	}
+}
