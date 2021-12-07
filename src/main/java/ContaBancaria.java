94f448472283ee19e35d69376e81ab30fdc174a9
Renata Manso <81649216+renatamanso@users.noreply.github.com>
2021-12-07 20:11:01 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,58 @@
+public class ContaBancaria extends java.lang.Object {
+    //atributos
+    private int id;
+    private Cliente cliente;
+    private int saldo;
+    private static int contador;
+
+
+    //métodos Personalisados
+    public void depositar(int valorDeposito) {
+
+        if(valorDeposito >= 0) {
+            this.setSaldo(this.getSaldo() + valorDeposito);
+            System.out.println("Déposito realizado na conta de: " + this.getCliente());
+            System.out.println("Saldo da conta bancaria: " + this.getSaldo());
+        }else {
+            System.out.println("Deposito inválido!");
+        }
+    }
+
+    public void sacar(int valorSaque) {
+        
+        if(this.getSaldo() >= valorSaque) {
+            this.setSaldo(this.getSaldo() - valorSaque);
+            System.out.println("Saque realizado com sucesso!");
+            System.out.println("Saldo da conta bancaria: " + this.getSaldo());
+        }else {
+            System.out.println("Saldo inuficiente para saque: -R$" + (valorSaque - this.getSaldo()));
+        }
+    }
+
+
+    //métodos especiais
+    public ContaBancaria(Cliente cliente) {
+        this.setCliente(cliente);
+        
+        contador = 2;
+    }
+
+    public int getId(){
+    	return contador --;
+    }
+    
+ 
+    public int getSaldo(){
+        return this.saldo;
+    }
+    public void setSaldo(int saldo){
+        this.saldo = saldo;
+    }
+    
+    public Cliente getCliente(){
+        return this.cliente;
+    }
+    public void setCliente(Cliente cliente){
+        this.cliente = cliente;
+    }
+}
