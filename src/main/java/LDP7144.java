da2ea0f9a4d16b0b801e2edd41d1ea15bb4013a4
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2021-12-06 21:17:00 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,24 @@
+	import java.util.Scanner;
+
+	public class LDP7144 {
+	
+				Scanner entrada = new Scanner(System.in);
+				int numero;
+				
+					System.out.println("Digite um número inteiro: ");
+					numero = entrada.nextInt();
+
+		                        if(numero % 2 == 0){
+		                                 System.out.print("par ");
+		                                 }
+					else{
+						System.out.print("impar");
+					}
+					if(numero<0){
+						System.out.print(" negativo");
+						
+					}else{ 
+						System.out.print(" positivo"); 
+		}
+		}
+		}
