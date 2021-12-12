1926a472a0f655e2845934e5a26daf74f8bfdaf3
Plaxcaster <56280285+Plaxcaster@users.noreply.github.com>
2021-12-11 22:59:31 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,15 @@
+public class PessoaFisica extends Cliente {
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf  = cpf;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+}
