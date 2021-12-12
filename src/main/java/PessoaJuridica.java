2d0cdacb813f0e815109a527b80e39c41a65704c
Plaxcaster <56280285+Plaxcaster@users.noreply.github.com>
2021-12-11 22:59:31 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,15 @@
+public class PessoaJuridica extends Cliente {
+	    private String cnpj;
+
+	    public PessoaJuridica(String nome, String cnpj) {
+	        this.nome = nome;
+	        this.cnpj  = cnpj;
+	    }
+
+	    public String getCnpj() {
+	        return this.cnpj;
+	    }
+	    public String getNome() {
+	        return this.nome;
+	    }
+	}
