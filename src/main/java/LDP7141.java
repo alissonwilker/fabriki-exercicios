cd806abab4eb2e6d8c935d494bb7c2a2550ac5ac
Giovana Rocha <91993098+gigir0cha@users.noreply.github.com>
2021-12-13 22:50:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,19 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class LDP7141 {
+	private static Scanner entrada = new Scanner (System.in);
+private static PrintStream saida = System.out;
+
+	public static void main(String[] args) {
+		int peixes = entrada.nextInt();
+		int dife = peixes - 50; 
+		int mul = dife * 4;
+	if(peixes >= 50) {System.out.println("excedente = 0\r\n"
+			+ "multa = 0");}
+	if (peixes <= 50) {System.out.println("excedente =" + dife   
+			+ "multa =" + mul);}
+
+	}
+
+}
