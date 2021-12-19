d376b7a88dbdbb57e941ae6d478cd7d4427cd30d
Pedro Carvalho <91221176+OpKSpatY@users.noreply.github.com>
2021-12-19 13:22:00 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LDP7251 {
+	private static Scanner entrada = new Scanner(System.in);
+		public static void main(String[] args) {
+		int ver=0;
+		float num, maior;
+		System.out.println("Entre um numero");
+		maior = entrada.nextInt();
+		if (maior == 0) {
+			ver = 1;
+		}
+		laco1:while (ver== 0) {
+			System.out.println("Entre outro numero");
+			num = entrada.nextInt();
+			if (num == 0) {
+				if (num >= maior) {
+					maior = num;
+				}
+				break laco1;
+			}
+			else {
+				if (num >= maior) {
+					maior = num;
+				}
+			}
+		}
+	System.out.println("maior = "+maior);
+	}
+}
