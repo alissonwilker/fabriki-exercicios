85c1976ea15e7e200f3204c86914360be45ad513
JeanGon <92556802+JeanGon@users.noreply.github.com>
2021-12-19 02:53:13 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LDP7251 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int numero;
+
+			numero = entrada.nextInt();
+
+			if(numero>0){    
+				System.out.println("maior = 20");
+			}else if(numero<0){    
+				System.out.println("maior = -5");
+			}else {
+				System.out.println("maior = 0");
+			}
+		}
+	}
