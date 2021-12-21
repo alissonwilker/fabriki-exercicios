c9eea0aca522e8483342d76b5e929a912a291e5f
wendelmaxuel <72840731+wendelmaxuel@users.noreply.github.com>
2021-12-20 21:41:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class LDP7251 {
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		int maior = 0;
+		int menor = 0;
+		int qteNumerosDigitados;
+		int arrayValores[];
+
+		Scanner sc = new Scanner(System.in);
+
+		qteNumerosDigitados = sc.nextInt();
+
+		arrayValores = new int[qteNumerosDigitados];
+
+		for (int i = 0; i < qteNumerosDigitados; i++) {
+
+			arrayValores[i] = sc.nextInt();
+
+			maior = arrayValores[i];
+			menor = arrayValores[i];
+		}
+
+		for (int i = 0; i < qteNumerosDigitados; i++) {
+
+			if (arrayValores[i] > maior) {
+				maior = arrayValores[i];
+
+			} else if (arrayValores[i] < menor) {
+				menor = arrayValores[i];
+
+			}
+		}
+		System.out.println("Maior = " + maior);
+		System.out.println("Menor = " + menor);
+
+	}
+}
