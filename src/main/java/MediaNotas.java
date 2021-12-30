5d0ffc2de0a202890a14d2db7cafb8914d4d7075
Pedro Carvalho <91221176+OpKSpatY@users.noreply.github.com>
2021-12-30 12:36:24 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,38 @@
+// Este é o código normal, sem uso do new Locale
+
+
+import java.util.Scanner;
+
+public class MediaNotas {
+	private static Scanner entrada = new Scanner(System.in);
+		public static void main(String[] args) {
+			Scanner sc = new Scanner (System.in);
+			int qtd, soma=0;
+			laco1:while (true){
+				System.out.println("Digite a quantidade de notas que deseja ler: ");
+				qtd = sc.nextInt();
+				if(qtd>0) {
+					break laco1;
+				}
+			}
+			String[] valores = new String[qtd];
+			for (int i=0; i < qtd;	i++) {
+				float nota;
+				nota = sc.nextInt();
+				while (nota < 0) {
+					nota = sc.nextInt();
+				}
+				String str = String.valueOf(nota);
+				str = str.replace('.', ',');
+				valores[i] = str;
+				soma += nota;
+			}
+			
+			double media = soma / qtd;
+			System.out.println("media = " + media);
+			int j = 0;
+			while (j<qtd) {
+				System.out.println("nota "+(j+1)+" = " + valores[j++]);
+			}
+		}
+}
