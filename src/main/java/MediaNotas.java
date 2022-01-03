2c3f3590bd13afe87bed5031ae235e0185a706ab
marcelagianini <91996348+marcelagianini@users.noreply.github.com>
2022-01-03 19:36:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MediaNotas {
+
+	public static void main(String[] args) {
+
+
+int[] nums = new int[5]; 
+	nums[0] = 9;
+	nums[1] = 10;
+	nums[2] = 5;
+	nums[3] = 6;
+	nums[4] = 7;
+
+	int media = 0;
+	for(int i = 0; i<nums.length; i++) {
+		
+		media += nums[i];
+	}
+
+	float total = (float) media / nums.length;
+	
+	System.out.println("A média é: " +total);
+	
+	
+	
+	
+	}
