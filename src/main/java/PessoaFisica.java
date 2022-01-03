d88cdcb50d22773af697cdce36ec60acc31d9c63
Alvaro-Miranda <74937462+Alvaro-Miranda@users.noreply.github.com>
2022-01-02 21:20:53 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,13 @@
+public class PessoaFisica extends Cliente {
+	
+	private java.lang.String cpf;
+
+	public PessoaFisica(java.lang.String nome, java.lang.String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+	public java.lang.String getCpf() {
+		return cpf;
+	}
+}
