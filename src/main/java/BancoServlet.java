ff6d56806676773e3bbe17beaf1a13a128d95668
GuilhermeLucas2299 <91921374+GuilhermeLucas2299@users.noreply.github.com>
2022-01-09 08:27:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BancoServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class BancoServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Object cliente = null;
+		request.setAttribute("cliente", cliente);
+		request.getRequestDispatcher("resposta.jsp").forward(request, response);
+	}
+}
