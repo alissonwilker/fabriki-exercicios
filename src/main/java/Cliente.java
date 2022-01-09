81e7e7bc1706ba140087091b7b86ec835e3e8242
GuilhermeLucas2299 <91921374+GuilhermeLucas2299@users.noreply.github.com>
2022-01-09 08:27:04 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,13 @@
+public abstract class Cliente extends java.lang.Object{
+	private java.lang.String nome;
+	protected Cliente(java.lang.String nome) {
+		this.nome = nome;
+	}
+	public java.lang.String getNome(){
+		return nome;
+	
+	}
+
+	
+
+}
