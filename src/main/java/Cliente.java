3d77bf9d7636f0f1b3cd8554d0569d860d573d0e
Leonardo Teixeira <leosilva0201@yahoo.com.br>
2022-01-09 23:19:24 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,17 @@
+
+public class Cliente {
+	
+	 String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String recuperarNomeCliente(){
+		 return this.getNome();
+	 }
+}
