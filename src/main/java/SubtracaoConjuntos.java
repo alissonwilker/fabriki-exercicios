92d072aa10870eb585eea6b92c1f569cc2221c86
Ronald Matheus Batista Vieira <92834707+RonaldMatheus@users.noreply.github.com>
2022-01-17 16:01:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,24 @@
+public class SubtracaoConjuntos{
+
+	public static void main(String[] args) {
+		int A [][] = new int[][] {{1,2,3}};
+		int B [][] = {{2}};
+		int[][] C = new int[A.length][B[0].length];
+		for(int s = 0; s<A.length;s++) {
+			for(int r = 0; r<B[0].length;r++) {
+				C[s][r] -= A[s][s] - B[r][r];
+		
+			}
+		}
+	System.out.print("sub{");
+	for (int s = 0; s < C.length; s++) {
+		System.out.print("{");
+		for (int r = 0; r < C.length;r++) {
+			System.out.print("" + C[s][r]+".");
+		}
+		System.out.print("}.");
+	}
+	System.out.print("}");
+	}
+
+}
