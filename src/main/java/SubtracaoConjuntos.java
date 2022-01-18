65694a30063fea46bfa5a0728e1d7674a7c01dba
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-01-17 22:00:27 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,29 @@
+package aula10;
+
+
+public class SubtracaoConjuntos {
+
+	public static void main(String[] args) {
+		
+		final int [][] A = {
+				{1, 2},
+				{3, }
+		};
+		final int  [][] B = {
+				{2, }
+		};
+		
+		int [][] AxB = new int[A.length][B[1].length];
+		
+		for (int i=0; i< AxB.length; i++) {
+			for (int j = 0; j < AxB[0].length; j++) {
+				for (int k = 0; k < A[0].length; k++) {
+				
+			}
+		}
+		
+
+	}
+
+	}
+}
