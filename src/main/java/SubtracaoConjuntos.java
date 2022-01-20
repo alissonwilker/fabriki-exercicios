90b0bb3ee95db28f2cb27b6fd45f158cc93dba08
Derrico1 <92489330+Derrico1@users.noreply.github.com>
2022-01-20 17:16:37 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SubtracaoConjuntos {
+
+	public static void main(String [] args) {
+		final Scanner entrada = new Scanner(System.in);
+		int conjuntoa;
+		int conjuntob;
+		Set A = new HashSet(); 
+		Set B = new HashSet(); 
+
+		do { 
+			conjuntoa = entrada.nextInt();
+			A.add(conjuntoa); 
+		}while (conjuntoa!=0);
+		do { 
+			conjuntob = entrada.nextInt();
+			B.add(conjuntob); 
+		}while (conjuntob!=0);
+
+		A.removeAll(B);
+		A.forEach(System.out::println);
+	}
+}
