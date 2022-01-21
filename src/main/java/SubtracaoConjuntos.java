3f8c798d0346d41b1f2e14ea20971cace1a965f1
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-01-20 22:32:36 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class SubtracaoConjuntos {
+	public static void main(String[] args) {
+		int cont=0;
+		int contB=0;
+		int[] valoresA = new int[100];
+		Scanner sc = new Scanner (System.in);
+		laco1:while(true) {
+			cont += 1;
+			
+			valoresA[cont] = sc.nextInt();
+			if (valoresA[cont] == 0) {
+				cont = 0;
+				break laco1;
+			}
+			else {
+				contB += 1;
+				continue;
+			}
+		}
+		int[] valoresB = new int[(contB)];
+		for(cont=1; cont < valoresB.length; cont++) {
+			
+			
+			valoresB[cont] = sc.nextInt();			
+			if (valoresB[cont] == 0)  {
+				cont = valoresB.length;
+			}
+			else {
+				continue;
+			}
+		}
+		int[] valoresC = new int[(100)];
+		int contC = 0;
+		for (int i = 0; i < valoresA.length; i++) {
+			for (int j = 0; j < valoresB.length; j++) {
+				if (valoresA[i] == valoresB[j]) {
+					valoresC[i] = valoresA[i]; 
+				}
+			}
+		}
+		
+		for (int k = 0; k < valoresC.length; k++) {
+			if (valoresC[k] != 0) {
+				System.out.println(valoresC[k]);
+			}
+		}
+
+		}
+
+}
