05778bea3bc7f8de7233b0f4312602a0f6a8d073
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-01-20 21:53:48 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,26 @@
+package aula10;
+
+import java.util.*;
+public class SubtracaoConjuntos {
+
+public static void main(String[] args){
+
+
+	final int[] A = {1,2,3};
+	final int[] B = {2};
+	 	
+	for (int i=0; i < B.length; i++) { 
+		
+		for (int j=0; j < A.length; j++) { 
+			
+			if (A[j]!=B[i]) { 
+			
+				System.out.println(A[j]); 
+				}
+		}
+	}
+	
+	
+}
+}
+	
