9202d748a5f25832c1005bc626123fb76deda6d8
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-01-20 23:01:40 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,39 @@
+package aula10;
+import java.util.*;
+public class SubtracaoConjuntos {
+	public static void main(String[] args) {
+
+
+	   
+	    	
+	   	  System.out.println("* CALCULANDO DIFERENÇA ENTRE DOIS CONJUNTOS *");
+	    	int[] listaA = new int[3];
+	    	int[] listaB = new int[1];
+
+	    	    
+	    	  Scanner leitor = new Scanner(System.in);
+	         
+	    	  	  for (int i=0;i<listaA.length;i++) {
+	    	  		System.out.println(" ");
+	    	  		  listaA[i]=leitor.nextInt();
+	    	  }
+	    	  	  
+	    	  	  for (int i=0;i<listaB.length;i++) {
+	    	  		  System.out.println("" );
+	    	  		  listaB[i]=leitor.nextInt();
+	    	  }
+	    	 	
+	    	   	  System.out.println("* DIFERENÇA ENTRE OS DOIS CONJUNTOS A-B *");
+		  
+	    	  	  
+	    	for (int i=0; i < listaB.length; i++) { 
+	    		
+	    		for (int j=0; j < listaA.length; j++) { 
+	    			
+	    			if (listaA[j]!=listaB[i]) { 
+	    			
+	    				System.out.println(listaA[j]); 
+	    				}
+	    		}
+	    	}
+	    	
