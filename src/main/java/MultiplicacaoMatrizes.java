d47f7b4ce6e9e4cf1b939d60220da2e0d614a019
Pedro Carvalho <91221176+OpKSpatY@users.noreply.github.com>
2022-01-24 11:33:44 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MultiplicacaoMatrizes.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class MultiplicacaoMatrizes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+
+		int linhaA = 0, colunaA=0, colunaB=0, linhaB=1;
+		System.out.println("Digite a quantidade de linhas");
+		linhaA = sc.nextInt();
+		System.out.println("Digite a quantidade de colunas");
+		colunaA = sc.nextInt();
+		
+		int[][] matrizA = new int [linhaA][colunaA];
+
+		System.out.println("Digite a quantidade de colunas");
+		colunaB = sc.nextInt();
+		
+		
+		int[][] matrizB = new int [linhaB][colunaB];
+		
+		for (int i=0; i < linhaA; i++) {
+			for (int j=0; j <colunaA; j++) {
+				matrizA[i][j] = sc.nextInt();
+			}
+		}
+		
+		for (int i=0; i <linhaB; i++) {
+			for (int j=0; j < colunaB; j++) {
+				matrizB[i][j] = sc.nextInt();
+			}
+		}
+		
+		//for (int i=0; i < linhaA; i++) {
+		//	for (int j=0; j <colunaA; j++) {
+		//		System.out.print(matrizA[i][j] + " ");
+		//	}
+		//	System.out.println("");
+		//}
+		
+		//System.out.println();
+		
+		//for (int j=0; j <colunaB; j++) {
+		//	System.out.print(matrizB[j] + " ");
+		//}
+		
+		//System.out.println();
+			
+		int[][] matrizC = new int[linhaA][colunaB];
+		
+		for (int i=0; i < linhaA; i++) {
+			for (int j=0; j < colunaB; j++) {
+				for (int k=0; k < linhaB; k++) {
+					matrizC[i][j] += matrizA[i][k] * matrizB[k][j]; 
+				}
+ 
+			}
+		}
+		
+		for (int i=0; i < linhaA; i++) {
+			for (int j=0; j <colunaB; j++) {
+				System.out.println(matrizC[i][j]);
+			}
+		}
+
+			
+		
+		System.out.println();
+		
+	
+			
+			
+
+		
+		
+		
+	}
+}
