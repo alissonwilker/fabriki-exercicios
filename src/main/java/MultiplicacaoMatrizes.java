6977eae16ecc871714505a005d4d79804a8089bc
Jonathan Henrique <90664237+JonathanSousa1@users.noreply.github.com>
2022-01-23 22:28:02 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MultiplicacaoMatrizes.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class MultiplicacaoMatrizes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner digitar = new Scanner(System.in);
+			
+		int linhasA, colunasA, colunasB;
+		int i,j,k;
+		
+		linhasA = digitar.nextInt();
+		colunasA = digitar.nextInt();
+		colunasB = digitar.nextInt();
+		
+		int[][] matriz_A = new int[linhasA][colunasA];
+		int[][] matriz_B = new int[linhasA][colunasB];
+		int[][] multi = new int[matriz_A.length][matriz_B.length];
+		
+		
+		
+		for(i=0; i<linhasA; i++)
+		{
+			for(j=0; j<colunasA; j++)
+			{
+				matriz_A[i][j]=digitar.nextInt();		
+			}
+		}
+		
+		for(i=0; i<linhasA; i++)
+		{
+			for(j=0; j<colunasB; j++)
+			{
+				matriz_B[i][j]=digitar.nextInt();			
+			}
+		}
+		
+		for (i = 0; i < matriz_A.length; i++) {
+			  for (j = 0; j < matriz_B.length; j++) {
+				  for (k = 0; k < matriz_B.length; k++)
+				multi[i][j]+= matriz_A[i][k] * matriz_B[k][j];
+				System.out.println(multi[i][j]);
+		}
+	}
+}
+	}
