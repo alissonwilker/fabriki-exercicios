82a9f8c3e7033b60d0b88aa03a949e687d3aeb63
Jonathan Henrique <90664237+JonathanSousa1@users.noreply.github.com>
2022-01-29 12:31:37 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		int num, result=1;
+		
+		num = entrada.nextInt();
+			if (num>=0 && num<=25) {
+			 for(; num>=1; --num) {
+				 result *= num;
+		} System.out.println(result);
+			}	
+			
+}
+	}
+
