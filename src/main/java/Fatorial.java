c526e024c24f766df78bff33ffb990133e50244c
Sabrina Alves Delpache <sasa2605.alves@gmail.com>
2022-01-30 17:35:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args){
+
+        Scanner ent = new Scanner(System.in);
+        int num, fat = 1;
+        int cont = 1;
+
+        do{
+            num = ent.nextInt();
+            ent.close();
+            for(int i = 1;i <= num; i++){
+                fat = fat * i;
+            }
+
+            cont++;
+
+        }while(cont < 2);
+        System.out.println(fat);
+    }
+}
