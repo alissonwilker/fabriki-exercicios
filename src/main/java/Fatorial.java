36e6de3fb5ccc189f96bd4f2759f559b6e2b6163
Pedro Carvalho <91221176+OpKSpatY@users.noreply.github.com>
2022-01-31 11:43:32 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fatorial {
+	public static void main (String[] args) {
+		int num, res;
+		Scanner sc = new Scanner(System.in);
+		//System.out.println("Informe o número desejado para realizar o fatorial:");
+		num = sc.nextInt();
+		sc.close();
+		res = fatorial(num);
+		System.out.println(res);
+		
+	}
+	
+	public static int fatorial (int a) {
+		int anterior=a;
+		for(int cont = a - 1; cont > 0; cont--) {
+			anterior = anterior * cont;
+		}	
+		
+		return anterior;
+	}
+}
