6537317015a2e3fc4567ddea1c8ae32242330a70
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-01-31 18:35:47 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,20 @@
+package aula11;
+
+
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		
+		int numero= 25;
+		long fatorial = 0;
+		int i = 0;
+		
+		while (i <= numero);
+		i++;
+		
+		System.out.println(o fatorial de "+ numero + " é " + fatorial" );
+				
+
+		}
+	}
