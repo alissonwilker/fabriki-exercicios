9cf6ffa4a0f38e9f1f9d2268f49de78266f5b4ac
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-01-31 20:01:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,27 @@
+package aula11;
+
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner (System.in);
+	
+		int n=25, fat=1;
+		System.out.println("número que deseja calcular o fatorial?" );
+		n = input.nextInt();
+		
+		if(n<=25) {
+			for (int i = 1; i <= n; i++) {
+			fat = fat * i;	
+		}
+			System.out.println("Fatorial é: " +fat );
+		}else {	
+		
+		System.out.println("número a partir de zero" );	
+
+		}
+	}
+}
+
+
