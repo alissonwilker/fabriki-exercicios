c6566cfc3186e7f9556d7e388c20f1d972965d93
GABRIEL PEIXOTO GUIMARÃES <91996679+Guimaboy14@users.noreply.github.com>
2022-02-05 15:10:12 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LDP7141 {
+	private static Scanner entrada = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int peso;
+		
+		peso = entrada.nextInt();
+		
+		if(peso<=50) {
+			System.out.println("excedente = 0");
+			System.out.println("multa = 0");
+		}else {
+			System.out.println("excedente = " + (peso-50));
+			System.out.println("multa = " + ((peso-50) *4));
+		}
+
+	}
+
+}
