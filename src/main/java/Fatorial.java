35b345b241ca1366199e122152749c26332f45ba
marcelagianini <91996348+marcelagianini@users.noreply.github.com>
2022-02-09 17:24:08 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,19 @@
+public class Fatorial {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		byte valor;
+		BigInteger fatorial = BigInteger.ONE;
+		
+		valor = entrada.nextByte();
+		entrada.close();
+		
+		for (byte i = 2; i <= valor; i++) {
+			fatorial = fatorial.multiply(BigInteger.valueOf(i) );
+		
+		}
+		
+		System.out.println(fatorial);
+	}
+
+}
