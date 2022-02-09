754811f05865d5a2a5d53fb8e16703841284fdac
Giovana Rocha <91993098+gigir0cha@users.noreply.github.com>
2022-02-09 07:12:39 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+		long x = 15511210043330985984000000;
+		long fat = x;
+		while (x > 1);
+		fat = fat *(x-1);
+		x--; 
+		}
+		System.out.println(fit);
+		
+		sc.close();
+	}
