7b4432f3b8be51037e4e6fdcd2afd7ab7745849f
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-02-10 17:56:38 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,35 @@
+package fim;
+
+import java.util.Scanner;
+
+public class LDP7144 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int valor = entrada.nextInt();
+		
+			if ( (valor % 2 == 0)) {
+				System.out.println( valor + " é um número PAR  POSSITIVO");
+			}else  {	
+			System.out.println( valor + " é um número PAR  NEGATIVO");
+
+				if ( (valor >= 0)) {
+					System.out.println( valor + " é um número impar ePOSSITIVO");
+				}else  {	
+				
+		
+
+}
+
+
+				{
+
+	}
+
+			}
+	}
+}
+	
+
+
+
