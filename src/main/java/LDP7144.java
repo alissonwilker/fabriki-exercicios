858fa343c1caf3e529310709f21ab205f3e0caae
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-02-10 17:38:52 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,29 @@
+package fim;
+
+import java.util.Scanner;
+
+public class LDP7144 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int valor = entrada.nextInt();
+		
+		for ( int i = 1; i <= 40; i++ ) {
+			if ( i % 2 == 0 )
+			System.out.println( i + " é um número PAR.");
+			else
+			System.out.println( i + " é um número IMPAR.");
+			}
+		
+		
+
+}
+
+
+	}
+
+
+	
+
+
+
