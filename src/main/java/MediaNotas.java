516f5c1fc1f516e3dd0db80bdfef8780b476053e
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-02-09 21:18:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,39 @@
+package fevereiro;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MediaNotas {
+
+	public static void main(String[] args) {
+		Scanner entrada  = new Scanner (System.in);
+		double soma = 0;
+		int todasnotas = entrada.nextInt();
+		int [] notas = new int [ todasnotas];
+		
+	
+		
+		int i = 0;
+		while (i < todasnotas) {
+		notas[i] = entrada.nextInt();
+		soma += notas[i];
+		i++;
+		}
+		
+		double media = soma /   todasnotas;
+		NumberFormat formatador = NumberFormat.getInstance(new Locale("pt", "BR"));
+		
+		formatador.setMinimumFractionDigits(1);
+		formatador.setMaximumFractionDigits(1);
+		
+		System.out.println("media = " + formatador.format(media));
+		while (  i < todasnotas) {
+			System.out.println("nota" + (i+1) + "=" +  formatador.format (notas[i]));	
+			
+		}
+		
+	}
+	
+
+}
