66c9c565d637b761c5583473f877e83272b2583e
isabelle-and <89953552+isabelle-and@users.noreply.github.com>
2022-02-09 23:13:29 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class LDP7251 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		int maior = n;
+		while (n != 0) {
+			n = sc.nextInt();
+			if (n!=0 && n>maior) {
+				maior = n;
+			}
+		}
+		System.out.println("maior = "+maior);
+		sc.close();
+	}
+
+}
+
