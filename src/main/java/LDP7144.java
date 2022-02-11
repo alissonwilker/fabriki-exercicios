490d869ab1eb2ced61a166229c9e6059d2c52010
joaoocastro <91996876+joaoocastro@users.noreply.github.com>
2022-02-10 23:08:41 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,27 @@
+package fim;
+
+import java.util.Scanner;
+
+public class LDP7144 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int valor;
+		
+		String negativoEpositivo, imparEpar;
+		
+		System.out.print("Digite um número: ");
+		valor = entrada.nextInt();
+		
+		if (valor % 2 == 0) {
+			System.out.println(" par e ");
+		}else  {	
+			System.out.println(" par e negativo");
+		} if (valor >= 0) {	
+			System.out.println("  positivo");
+		}else {
+			System.out.println(" negativo");
+		}
+	}
+
+}
