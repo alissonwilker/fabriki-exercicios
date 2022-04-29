f982b668af81131f7bbe5564a2d6b0e223a86558
Ynara lira ventura <79769248+ynaralira@users.noreply.github.com>
2022-04-28 21:00:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SomaDoisNumeros.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+class SomaDoisNumeros{
+ 
+   public static void main( String[] args ) {
+      int num1 num2 = 0;
+      Scanner s = new Scanner( System.in );
+      System.out.println( "Introduce dos números: ");
+      num1 = s.nextInt();
+      num2 = s.nextInt();
+   }
+ 
+}
