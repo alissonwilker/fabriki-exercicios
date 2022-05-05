36c761908b75cb4fdbd6f7ab0a966dbffdef96c6
EVA-00-PROTO <103697909+EVA-00-PROTO@users.noreply.github.com>
2022-05-05 20:27:23 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7143.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LDP7143 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int num = entrada.nextInt();
+		int num2 = entrada.nextInt();
+		int num3 = entrada.nextInt();
+		int num4 = entrada.nextInt();
+		if (num3 * num3 >= 1000) {
+
+			System.out.println("c2 = " + num3 * num3);
+		} else {
+			System.out.println("a = " + num);
+			System.out.println("b = " + num2);
+			System.out.println("c = " + num3);
+			System.out.println("d = " + num4);
+			System.out.println("a2 = " + num * num);
+			System.out.println("b2 = " + num2 * num2);
+			System.out.println("c2 = " + num3 * num3);
+			System.out.println("d2 = " + num4 * num4);
+
+		}
+		entrada.close();
+	}
+}
