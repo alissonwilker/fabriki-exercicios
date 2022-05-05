ec5c77221552ece2bb03abc7c633454e2b62577e
maurosergioifb2021 <92488272+maurosergioifb2021@users.noreply.github.com>
2022-05-04 21:57:06 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7144.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class LDP7144{
+	public static void main(String[] args) {		
+		Scanner dados  = new Scanner(System.in);
+
+		int numero = dados.nextInt();
+		if (numero>0 && numero % 2 == 0) {
+			System.out.println("Esse numero é positivo e par!!");
+		}
+		
+		else if (numero>0 && numero % 2 == 0) {
+			System.out.println("Esse numero é positivo e impar!!");}
+		
+		
+		else if 
+		(numero<0 && numero % 2 == 0) {
+			System.out.println("Esse numero é negativo e par!!");
+		}
+		
+		else if (numero<0 && numero % 2 == 1) {
+			System.out.println("Esse numero é positivo e impar!!");}
+		
+		else if (numero == 0) {
+			System.out.println("Esse numero é exatamente 0");
+		}
+		
+		
+		
+		}
+	
+	
+		
+	}
