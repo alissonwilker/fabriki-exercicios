e300ec96281a56d98311dfe7493669503b040ce1
Tatiana Casado <88239560+Tdrcmelo@users.noreply.github.com>
2022-05-11 21:19:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7141.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class LDP7141 {
+	public static void main(String[] args) {
+		 Scanner entrada = new Scanner(System.in);
+			int pesopeixe = entrada.nextInt();
+			int pesoexc = (pesopeixe-50);
+			int multa = (pesoexc * 4);
+			
+			if(pesopeixe > 50) {
+				System.out.println("excedente = " + pesoexc);
+				System.out.println("multa = " + multa);
+				
+				
+			}
+			
+			else if(pesopeixe <= 50) {
+				System.out.println("excedente = " + 0 );
+				System.out.println("multa = " + 0);
+				
+			}
+	
+	
+	
+
+}
+}
