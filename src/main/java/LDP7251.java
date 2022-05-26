03e0b6fb734765265976f3350de0cdd6cc2520e7
Lucas Macêdo Pessoa <educacional@lucasmacedo.org>
2022-05-25 21:05:42 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7251.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LDP7251 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int numero = entrada.nextInt();
+		int maior;
+	 	numero = entrada.nextInt();
+	 	maior = numero;
+		while (numero != 0) {
+		      if (numero>maior) {
+		    	  maior = numero;
+		      }
+		  	 numero = entrada.nextInt();
+		    }
+	      System.out.println("maior = " + maior); 
+	}
+}
