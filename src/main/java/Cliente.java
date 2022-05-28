0eed7f4b91d9564f20b83a4c89ceb02378feafd2
Guilherme Marçal <58859672+MarcalFilosofo@users.noreply.github.com>
2022-05-27 21:27:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,20 @@
+
+
+public abstract class Cliente {
+	private String nome;
+
+	protected Cliente(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
