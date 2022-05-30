4d8b09dad73f5b2b7b32a4a7e9c77295c3ab4a6c
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2022-05-30 15:12:04 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+package Class;
+
+public abstract class Cliente {
+
+	private String nome;
+	
+	protected Cliente(String _nome) {
+		this.nome = _nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+}
