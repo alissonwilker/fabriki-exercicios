da6a13889a68a3b121b2d72b66d53c3149fe37ec
pedroromao26 <103156543+pedroromao26@users.noreply.github.com>
2022-06-02 16:15:02 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,13 @@
+public class LDP7253 {
+	public static void main (String [] Args){
+		int Contagem=1;
+		for (int contador=101; Contagem<contador;Contagem++){
+		 
+		System.out.printf(" %d \n",Contagem);
+		if (Contagem %10 ==0){
+		System.out.printf("Múltiplo de 10 \n",Contagem);
+		}
+		} 
+		}
+
+}
