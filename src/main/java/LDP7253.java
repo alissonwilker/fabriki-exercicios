788539dcd197a5fc9840681976a5e893960ec630
Tatiana Casado <88239560+Tdrcmelo@users.noreply.github.com>
2022-06-02 12:16:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,13 @@
+public class LDP7253 {
+	public static void main(String[] args) {
+	    laco1: for (int i = 1; i <= 100; ++i) { 
+	      if (i % 10 == 0) {
+	    	  System.out.println("Multiplo de 10");
+	      }
+	     
+	    }
+	  }
+	}
+
+
+
