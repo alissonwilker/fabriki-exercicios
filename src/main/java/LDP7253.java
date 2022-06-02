92ea536fd3fcb269fd848e93b21b2f2b950949e2
marciofilhooo <96481317+marciofilhooo@users.noreply.github.com>
2022-06-02 18:38:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class LDP7253.java {
+    public static void main(String[] args) { 
+     for (int i = 0; i <= 100; ++i) { 
+         
+         System.out.println(i % 10 == 0 ? i + " - Multiplo de 10" : i); 
+        
+        } 
+    }
+}
