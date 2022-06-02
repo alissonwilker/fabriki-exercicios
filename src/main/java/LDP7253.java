eac50d29bb4c18a73084eabd890bbb9f12b2145a
Pritempone <103539007+Pritempone@users.noreply.github.com>
2022-06-01 22:36:54 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,13 @@
+public class LDP7253 {
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) { 
+            if (i % 10 == 0 ) {
+            	System.out.println("Multiplo de 10");
+//           } else {
+//             	System.out.println(i); 
+            }
+	}
+}
+}
