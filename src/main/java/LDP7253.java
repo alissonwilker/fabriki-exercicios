f0984ede6c5738e94abe0f5307ea34ac4ca50213
Gustavo Garcia <103156511+Gustavo3361@users.noreply.github.com>
2022-06-02 19:20:48 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,15 @@
+public class LDP7253 {
+	  public static void main (String[] args) {
+       
+                           for(int i =1 ; i <=100 ; i++ ){
+                               int y = i % 10;
+			       if (y == 0){
+
+         System.out.println("Multiplo de 10");
+
+		}	
+			
+        }
+
+}
+}
