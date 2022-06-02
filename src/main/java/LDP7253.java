0121d5f5d6af39d1dc9e7a3c0712e87558dbfb59
ronaldo-cruzeiro <103541296+ronaldo-cruzeiro@users.noreply.github.com>
2022-06-02 19:29:25 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,10 @@
+public class LDP7253 {
+public static void main (String [] Args){
+
+    for (int contador=1; contador <= 100; contador++){
+     if (contador %10 == 0){
+	     System.out.println("Multiplo de 10");
+}
+} 
+}
+}
