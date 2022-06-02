2f349c6072c1815acb4126baf80744d05a015520
adison91 <103541164+adison91@users.noreply.github.com>
2022-06-02 16:54:05 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LDP7253 {	
+	public static void main(String[] args) {
+		int numero; 
+		int multiplo = 10;
+		for(numero = 1; numero <= 100; numero++);
+
+			if(numero % multiplo == 0) {
+				System.out.println("Multiplo de 10");
+			}
+
+	}
+
+}
