bd888ec4ee4b5e010c68c6904d035f09d60218b3
John Cavalcante <70822584+JohnCavalcante@users.noreply.github.com>
2022-06-01 23:30:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7253.java
@@ -0,0 +1,14 @@
+public class LDP7253 {
+
+	public static void main(String[] args) {
+		int n;
+		n = 0;
+		
+		for( int i = 0; i < 100; i++){
+			
+			if ((i % 10) == 0){
+				System.out.println("Multiplo de 10");
+			}
+		}	
+	}
+}
