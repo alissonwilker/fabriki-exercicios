629ea58da77d085f3c85273293ac03524e058c87
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2022-06-02 22:35:34 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,16 @@
+
+
+public class PessoaJuridica extends Cliente {
+
+	private String cnpj;
+	
+	public PessoaJuridica(String _nome, String _cnpj) {
+		super(_nome);
+		this.cnpj = _cnpj;
+	}
+
+	public String getCpf() {
+		return cnpj;
+	}
+
+}
