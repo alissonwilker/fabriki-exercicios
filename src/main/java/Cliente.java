37e96492d27abc86dbb9743798a30a0706d0de72
Danilo Rocha <53863511+danilo-rocha1@users.noreply.github.com>
2022-06-03 16:04:50 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente {
+protected String nome;
+
+	public Cliente() {
+
+	}
+
+	public Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+}
