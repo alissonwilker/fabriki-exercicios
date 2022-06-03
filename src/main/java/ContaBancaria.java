6bf39e5a54c10ce7f125e79ad6e14b0820834e44
Leonardo Teixeira <leosilva0201@yahoo.com.br>
2022-06-03 17:07:06 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,44 @@
+
+public class ContaBancaria extends Cliente {
+
+	private Cliente	cliente;
+	
+	private static int contador;
+	
+	private int	id;
+	
+	private int	saldo;
+	
+	
+	protected ContaBancaria(Cliente cliente) {
+		super(cliente);
+		id++;
+	}
+	
+	public int depositar(int valorDeposito) {
+		if (valorDeposito>0) {
+			saldo = saldo+ valorDeposito;
+		}
+		return valorDeposito;
+	}
+
+	public Object getSaldo() {
+		return this.saldo;
+	}
+
+	public int sacar(int valorSaque) {
+		if(saldo>=valorSaque && valorSaque>0) {
+			saldo = saldo - valorSaque;
+		}
+		return valorSaque;
+	}
+
+	public Cliente getCliente() {
+		return this.cliente;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+}
