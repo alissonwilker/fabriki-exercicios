4fe4e01725d3e669ba95d93b6a27a01702b15ff7
mhcp5673 <42880611+mhcp5673@users.noreply.github.com>
2022-06-02 22:09:13 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,14 @@
+package main;
+
+public abstract class Cliente {
+
+	private String nome;
+
+	protected Cliente(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+}
