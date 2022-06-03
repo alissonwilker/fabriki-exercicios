dc4714943535e5cd585d9895d4afae2d4cd34649
Leonardo Teixeira <leosilva0201@yahoo.com.br>
2022-06-03 17:50:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,18 @@
+
+public abstract class Cliente {
+	
+	private String nome;
+
+	protected Cliente(String nome) {
+		nome = this.nome;
+	}
+
+	public Cliente(Cliente pessoaJuridica) {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	} 
+
+}
