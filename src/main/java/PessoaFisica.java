aa032bda4799983bb6acfd59f1662d51337f5d4b
Ian Gabriel <iangabrielrodrigues@gmail.com>
2022-06-03 16:26:52 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,14 @@
+
+public class PessoaFisica extends Cliente {
+	
+	private String cpf;
+
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+}
