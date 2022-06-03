8a247e0eb94ca10ced613240dfe56006addc70cd
Leonardo Teixeira <leosilva0201@yahoo.com.br>
2022-06-03 17:50:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,14 @@
+
+public class PessoaFisica extends Cliente{
+	
+	private String cpf;
+	
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+}
