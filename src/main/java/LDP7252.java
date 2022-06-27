2c1e162b9bc6c097729479722554a7ed2e0d565c
fernando-felix <103541087+fernando-felix@users.noreply.github.com>
2022-06-27 00:58:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class LDP7252 {
+
+	public static void main(String[] args) {
+		
+		BigInteger atual = BigInteger.valueOf(0);
+		BigInteger anterior = BigInteger.valueOf(1); 
+		
+		for (int i=0; i<64; i++) {
+			atual = atual.add(anterior);
+			anterior = anterior.multiply(BigInteger.valueOf(2));
+			
+		}
+		System.out.println("totalGraos = "+atual);
+	}
+
+}
