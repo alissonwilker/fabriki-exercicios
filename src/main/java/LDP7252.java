b0ae6b916c217ad79b7251390a59496cd53fcdd1
leonardokdfx <103157645+leonardokdfx@users.noreply.github.com>
2022-06-30 20:14:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,15 @@
+
+public class LDP7252 {
+	public static void main(String[] args) {
+		double graosCasa=1,casasTabuleiro=64;
+		int graos=1;
+
+		for(int x=1;x<casasTabuleiro;x++){
+			graosCasa = graosCasa*2;
+			graos +=graosCasa;
+		}
+
+		System.out.println("totalGraos = "+graos);
+
+	}
+}
