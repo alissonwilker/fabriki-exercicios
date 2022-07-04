e6f3e7b1536305b8af20a42f6b29bd53299c3d76
leonardokdfx <103157645+leonardokdfx@users.noreply.github.com>
2022-07-04 17:50:44 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/LDP7252.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+
+public class LDP7252 {
+	
+		    public static void main(String[] args) {		        		        
+		        BigInteger totalGraos = BigInteger.valueOf(2).pow(64).subtract(BigInteger.ONE);		           
+		        System.out.println ("totalGraos = " + totalGraos);    
+		    }
+		}
