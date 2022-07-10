fff9328f8a51661daf59f54ae30dedcc51816acd
Guilherme Marçal <58859672+MarcalFilosofo@users.noreply.github.com>
2022-07-09 22:09:20 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaFisica.java
@@ -0,0 +1,20 @@
+
+
+public class PessoaFisica extends Cliente{
+	private String cpf;
+
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
