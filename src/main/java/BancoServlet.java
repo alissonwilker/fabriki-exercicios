a28d7e4f5ca596585b62924f522777033b7fecc3
Guilherme Marçal <58859672+MarcalFilosofo@users.noreply.github.com>
2022-07-09 23:34:58 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BancoServlet.java
@@ -0,0 +1,131 @@
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import java.io.*;
+import java.net.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+/**
+ * Servlet implementation class Controlador
+ */
+@WebServlet("/BancoServlet")
+public class BancoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    // @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		String nome = request.getParameter("nomeCliente");
+		String tipoCliente = request.getParameter("tipoCliente");
+		String cpfCnpj = request.getParameter("cpfCnpj");
+		String operacao = request.getParameter("operacao");
+		String stringValorDeposito = request.getParameter("valorDeposito");
+		String stringValorSaque = request.getParameter("valorSaque");
+		
+		Integer valorDeposito = Integer.parseInt(stringValorSaque);
+		Integer valorSaque = Integer.parseInt(stringValorSaque);
+
+		if(tipoCliente == "pessoaFisica"){
+			PessoaFisica cliente = new PessoaFisica(nome, cpfCnpj);
+			ContaBancaria cb = new ContaBancaria(cliente);
+		} else if(tipoCliente == "pessoaJuridica"){
+			PessoaJuridica cliente = new PessoaJuridica(nome, cpfCnpj);
+			ContaBancaria cb = new ContaBancaria(cliente);
+		}
+
+
+		// RequestDispatcher rd = getServletContext().getRequestDispatcher("/fabriki-exercicios.jsp");
+		switch (operacao) {
+			case "recuperarNomeCliente":
+								
+				// request.setAttribute( "resposta",    cliente.getNome() );
+				// rd.forward(request,response);
+				out.println("<div id='resposta'>" + cliente.getNome() + "</div>");
+				break;
+			case "recuperarCpfCliente":
+				PessoaFisica pf = new PessoaFisica(nome, cpfCnpj);
+				if(tipoCliente == "pessoaFisica"){
+					try {
+						// out.println("<div id='resposta'>" + cliente.getCpf() + "</div>");
+						out.println("<div id='resposta'>" +  pf.getCpf() + "</div>");
+						
+						// request.setAttribute( "resposta",    cliente.getCpf() );
+						// rd.forward(request,response);
+					} catch (Exception e) {
+					}
+				} else {
+					out.println("<div id='resposta'></div>");
+				}
+				break;
+			case "recuperarCnpjCliente":
+				PessoaJuridica pj = new PessoaJuridica(nome, cpfCnpj);
+				if(tipoCliente == "pessoaJuridica"){
+					try {
+						// out.println("<div id='resposta'>" + cliente.getCnpj() + "</div>");
+						out.println("<div id='resposta'>" + pj.getCnpj() + "</div>");
+						
+						// request.setAttribute( "resposta",    cliente.getCnpj() );
+						// rd.forward(request,response);
+					} catch (Exception e) {
+					}
+				} else {
+					out.println("<div id='resposta'></div>");
+
+				}
+				break;
+			case "recuperarSaldo":
+			
+				// request.setAttribute( "resposta",    cb.getSaldo() );
+				// rd.forward(request,response);
+				out.println("<div id='resposta'>" + cb.getSaldo() + "</div>" );
+				break;
+			case "depositarValor":
+				cb.depositar(valorDeposito);
+				
+				// request.setAttribute( "resposta",    cb.getSaldo() );
+				// rd.forward(request,response);
+				out.println("<div id='resposta'>" + cb.getSaldo()  + "</div>");
+				break;
+			case "sacarValor":
+				cb.sacar(valorSaque);
+				
+				// request.setAttribute( "resposta",    cb.getSaldo() );
+				// rd.forward(request,response);
+				out.println("<div id='resposta'>" + cb.getSaldo() + "</div>");
+				break;
+			case "depositarSacarValor":
+				cb.depositar(valorDeposito);
+				cb.sacar(valorSaque);
+				
+				// request.setAttribute( "resposta",    cb.getSaldo() );
+				// rd.forward(request,response);
+				out.println("<div id='resposta'>" + cb.getSaldo() + "</div>");
+				break;
+		
+			default:
+				break;
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
