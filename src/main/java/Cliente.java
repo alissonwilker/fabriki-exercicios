782a0a47dfcec0d5c25b26384c5e6a7030f7ecc1
claudio-lucas-00 <71655296+claudio-lucas-00@users.noreply.github.com>
2022-07-17 11:31:00 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente {
+ private String nome;
+
+ protected Cliente(String nome) {
+  super();
+  this.nome = nome;
+ }
+
+ public String getNome() {
+  return nome;
+ }
+
+ public void setNome(String nome) {
+  this.nome = nome;
+ }
+
+
+}
