bedcd6e6e8ba63f6b8390884979adb834ce90199
alineom <84157249+alineom@users.noreply.github.com>
2022-07-17 12:24:46 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BancoServlet.java
@@ -0,0 +1,115 @@
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/BancoServlet")
+
+public class BancoServlet extends HttpServlet {
+	private static final Cliente PessoaFisica = null;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String paramNomeCliente = request.getParameter("NomeCliente");
+
+		String paramtipoCliente = request.getParameter("tipoCliente");
+
+		String paramcpfCnpj = request.getParameter("cpfCnpj");
+
+		String paramoperacao = request.getParameter("operacao");
+
+		if (paramtipoCliente.equals("pessoaFisica")) {        	 
+			PessoaFisica pessoaFisica = new PessoaFisica(paramNomeCliente,paramcpfCnpj);	if (paramoperacao.equals("operacaoRecuperarNomeCliente")) { 
+				request.setAttribute("resposta", pessoaFisica.getNome()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoRecuperarCpfCliente")) { 
+				request.setAttribute("resposta", pessoaFisica.getCpf()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("recuperarSaldo")) {
+				ContaBancaria conta = new ContaBancaria(pessoaFisica);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoDepositarValor")) {
+				ContaBancaria conta = new ContaBancaria(pessoaFisica);
+				int valorDeposito = Integer.parseInt(request.getParameter("valorDeposito"));
+				conta.depositar(valorDeposito);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoSacarValor")) {
+				ContaBancaria conta = new ContaBancaria(pessoaFisica);
+				int valorSaque = Integer.parseInt(request.getParameter("valorSaque"));
+				conta.sacar(valorSaque);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoDepositarSacarValor")) {
+				ContaBancaria conta = new ContaBancaria(pessoaFisica);
+				int valorDeposito = Integer.parseInt(request.getParameter("valorDeposito"));
+				conta.depositar(valorDeposito);
+				int valorSaque = Integer.parseInt(request.getParameter("valorSaque"));
+				conta.sacar(valorSaque);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+		}
+		else {
+			PessoaJuridica pessoaJuridica= new PessoaJuridica(paramNomeCliente,paramcpfCnpj);    	  
+			
+			if (paramoperacao.equals("operacaoRecuperarNomeCliente")) { 
+				request.setAttribute("resposta", pessoaJuridica.getNome()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoRecuperarCnpjCliente")) { 
+				request.setAttribute("resposta", pessoaJuridica.getCnpj() ); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+			if (paramoperacao.equals("recuperarSaldo")) {
+				ContaBancaria conta = new ContaBancaria(pessoaJuridica);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoDepositarValor")) {
+				ContaBancaria conta = new ContaBancaria(pessoaJuridica);
+				int valorDeposito = Integer.parseInt(request.getParameter("valorDeposito"));
+				conta.depositar(valorDeposito);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoSacarValor")) {
+				ContaBancaria conta = new ContaBancaria(pessoaJuridica);
+				int valorSaque = Integer.parseInt(request.getParameter("valorSaque"));
+				conta.sacar(valorSaque);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+
+			if (paramoperacao.equals("operacaoDepositarSacarValor")) {
+				ContaBancaria conta = new ContaBancaria(pessoaJuridica);
+				int valorDeposito = Integer.parseInt(request.getParameter("valorDeposito"));
+				conta.depositar(valorDeposito);
+				int valorSaque = Integer.parseInt(request.getParameter("valorSaque"));
+				conta.sacar(valorSaque);
+				request.setAttribute("resposta", conta.getSaldo()); 
+				RequestDispatcher dispatcher = request.getRequestDispatcher("resposta.jsp"); 
+				dispatcher.forward(request, response);}
+		}
+	}
+}
