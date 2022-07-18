6d8e45556822b306f9cb2682e358446726c96dc7
alineom <84157249+alineom@users.noreply.github.com>
2022-07-17 22:12:28 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/BancoServlet.java
@@ -0,0 +1,96 @@
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.*;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/BancoServlet")
+
+public class BancoServlet extends HttpServlet {	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		String paramtipoCliente = request.getParameter("tipoCliente");
+		String paramoperacao = request.getParameter("operacao");
+		String VALOR_DEPOSITO = request.getParameter("valorDeposito");
+		String VALOR_SAQUE = request.getParameter("valorSaque");
+	        Object resposta = null;
+		
+		
+		if (paramtipoCliente.equals("pessoaFisica")) {
+			String CPF_CLIENTE = request.getParameter("cpfCnpj");
+			String NOME_CLIENTE_PESSOA_FISICA = request.getParameter("nomeCliente");	
+			PessoaFisica pessoaFisica = new PessoaFisica(NOME_CLIENTE_PESSOA_FISICA, CPF_CLIENTE);
+			
+			ContaBancaria conta = new ContaBancaria(pessoaFisica);
+			
+			if (paramoperacao.equals("recuperarNomeCliente")) { 			
+				request.setAttribute("resposta", pessoaFisica.getNome());
+				}
+			if (paramoperacao.equals("recuperarCpfCliente"))  { 
+				request.setAttribute("resposta", pessoaFisica.getCpf());
+				}
+			if (paramoperacao.equals("recuperarSaldo")) {
+				request.setAttribute("resposta", conta.getSaldo()); 
+				}
+			if (paramoperacao.equals("depositarValor")) {
+				conta.depositar(Integer.parseInt(VALOR_DEPOSITO));
+				request.setAttribute("resposta", conta.getSaldo()); 
+				}
+			if (paramoperacao.equals("sacarValor")) {
+				conta.sacar(Integer.parseInt(VALOR_SAQUE));
+				request.setAttribute("resposta", conta.getSaldo()); 
+				}
+			if (paramoperacao.equals("depositarSacarValor")) {
+				conta.depositar(Integer.parseInt(VALOR_DEPOSITO));
+				conta.sacar(Integer.parseInt(VALOR_SAQUE));
+				request.setAttribute("resposta", conta.getSaldo());				
+				}	
+		}
+		
+			
+		if (paramtipoCliente.equals("pessoaJuridica")) {
+			String CNPJ_CLIENTE = request.getParameter("cpfCnpj");
+			String NOME_CLIENTE_PESSOA_JURIDICA = request.getParameter("nomeCliente");	
+			PessoaJuridica pessoaJuridica = new PessoaJuridica(NOME_CLIENTE_PESSOA_JURIDICA,CNPJ_CLIENTE);	
+			ContaBancaria conta = new ContaBancaria(pessoaJuridica);
+			
+			
+			if (paramoperacao.equals("recuperarNomeCliente")) { 			
+				request.setAttribute("resposta", pessoaJuridica.getNome());
+			
+				}
+			if (paramoperacao.equals("rrecuperarCnpjCliente"))  { 
+				request.setAttribute("resposta", pessoaJuridica.getCnpj());
+
+				}
+			if (paramoperacao.equals("recuperarSaldo")) {
+				request.setAttribute("resposta", conta.getSaldo()); 
+				
+				}
+			if (paramoperacao.equals("depositarValor")) {
+				conta.depositar(Integer.parseInt(VALOR_DEPOSITO));
+				request.setAttribute("resposta", conta.getSaldo());
+			
+				}
+			if (paramoperacao.equals("sacarValor")) {
+				conta.sacar(Integer.parseInt(VALOR_SAQUE));
+				request.setAttribute("resposta", conta.getSaldo()); 
+				
+				}
+			if (paramoperacao.equals("depositarSacarValor")) {
+				conta.depositar(Integer.parseInt(VALOR_DEPOSITO));
+				conta.sacar(Integer.parseInt(VALOR_SAQUE));
+				request.setAttribute("resposta", conta.getSaldo()); 
+				
+				}	
+		}
+		request.getRequestDispatcher("resposta.jsp").forward(request, response);
+}
+}
