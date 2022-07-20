0c8ce58538f87e518b1c7a1fe6d459ea55782dd5
leonardokdfx <103157645+leonardokdfx@users.noreply.github.com>
2022-07-20 19:29:15 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/SubtracaoConjuntos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SubtracaoConjuntos {
+	public static void main (String [] args) {
+		Scanner in = new Scanner (System.in);
+		int A[] = new int [3];
+		A[0]= 1;
+		A[1]= 2;
+		A[2]= 3;
+		int B[] = new int [2];
+		B[0]= 2;
+		for(int i = 0; i<3; i++);
+		A[0]= in.nextInt();
+		A[1]= in.nextInt();
+		A[2]= in.nextInt();
+	    B[0]= in.nextInt();
+	    B[0]= in.nextInt();
+	    B[0]= in.nextInt();
+	
+		System.out.println( A[0]);
+		System.out.println (A[2]);
+	
+		 
+	}
+}
