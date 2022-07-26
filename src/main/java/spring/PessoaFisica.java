352c86bddadd857f61a3d9c4c068b7bc92b67f20
rodrigosabcunha <79269308+rodrigosabcunha@users.noreply.github.com>
2022-07-26 16:45:07 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,28 @@
+package spring;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+@Entity
+public class PessoaFisica extends Cliente  {
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+    public PessoaFisica() {
+    }
+
+
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
