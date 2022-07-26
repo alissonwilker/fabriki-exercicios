8b56b1d331d1a5b9f73f9e08a250d55a6c4d71fa
rodrigosabcunha <79269308+rodrigosabcunha@users.noreply.github.com>
2022-07-26 16:45:06 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisicaRepository.java
@@ -0,0 +1,13 @@
+package spring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Integer> {
+
+    // PessoaFisica insert(PessoaFisica pf);
+
+    // PessoaFisica update(PessoaFisica pf);
+    
+}
