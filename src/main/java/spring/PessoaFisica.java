70512cc7359b761fae0dac05aed8870acf0a969a
Samuel-Gouvea <84049041+Samuel-Gouvea@users.noreply.github.com>
2022-07-27 12:46:18 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,26 @@
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+@Entity
+public class PessoaFisica extends Cliente  {
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+    public PessoaFisica() {
+    }
+
+
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
