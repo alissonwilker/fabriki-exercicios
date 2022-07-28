10eb0198fd8abc0889f54048208f1bbeca4e4e97
EVA-00-PROTO <103697909+EVA-00-PROTO@users.noreply.github.com>
2022-07-28 17:33:12 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/MediaNotas.java
@@ -0,0 +1,39 @@
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class MediaNotas{
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner (System.in);
+
+		List<Integer> notas1 = new ArrayList<Integer>();
+
+		int qtdeNotas, notas, i, j =1;
+		qtdeNotas = entrada.nextInt();
+
+
+		for (i = 0; i < qtdeNotas; i++) {
+			notas = entrada.nextInt();
+			notas1.add(notas);	
+		}
+
+		double soma = 0;
+		for (Integer elementos : notas1)
+			soma += elementos;
+
+		double media = soma / qtdeNotas;
+		NumberFormat formatador = NumberFormat.getInstance(new Locale("pt", "BR"));
+		formatador.setMinimumFractionDigits(1);
+		formatador.setMaximumFractionDigits(1);
+
+		System.out.println("media = " + formatador.format(media));
+
+
+		for (Integer elementos : notas1)
+		System.out.println("nota " + (j++) +" = " + formatador.format(elementos));
+	}	
+}
