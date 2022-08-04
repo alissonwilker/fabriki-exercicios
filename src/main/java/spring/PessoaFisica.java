8d7c90daa6ae0fd7fe3afbf9ee1f3c68a7a930b7
Jorge Kayodê <68976406+RedCanister@users.noreply.github.com>
2022-08-04 19:36:19 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/spring/PessoaFisica.java
@@ -0,0 +1,31 @@
+package spring;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoafisica")
+public class PessoaFisica extends Cliente {
+	
+	@Column(updatable = true, nullable = false, unique = true)
+	private String cpf;
+	
+	public PessoaFisica() {
+		
+	}
+	
+	public PessoaFisica(String nome, String cpf) {
+		super(nome);
+		this.cpf = cpf;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String _cpf) {
+		this.cpf = _cpf;
+	}
+
+}
