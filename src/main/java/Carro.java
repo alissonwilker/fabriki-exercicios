dca1a2d295a3ffe661922a0e305442e536508b67
Marcos Silva <94481402+marcoseagh@users.noreply.github.com>
2022-08-04 20:29:50 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,18 @@
+
+public class Carro extends Automovel {
+
+	public  Carro () {
+		qtdePneus = 4;
+		
+	}
+	
+
+	public void moverParaTras() {
+		// TODO Auto-generated method stub
+		
+	}
+	protected void engatarMarchARe() {
+		
+		
+	}
+}
