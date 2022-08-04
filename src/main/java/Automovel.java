50ea612978bf387936a91370fe0b83d1bd4a224d
ronaldo-cruzeiro <103541296+ronaldo-cruzeiro@users.noreply.github.com>
2022-08-04 14:10:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,37 @@
+public class Automovel extends Veiculo {
+
+public void moverParaFrente() {
+		// TODO Auto-generated method stub
+	}
+
+	public void ligar() {
+		// TODO Auto-generated method stub	
+	}
+
+	public void desligar() {
+		// TODO Auto-generated method stub	
+	}
+
+	protected void engatarMarchaAFrente() {
+	}
+
+	public boolean estaLigado() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public void frear() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public boolean estaEmMarchaAFrente() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+
+}
