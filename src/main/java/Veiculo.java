c2734070b6024620339fd8b07187aa6cdbe83c91
ronaldo-cruzeiro <103541296+ronaldo-cruzeiro@users.noreply.github.com>
2022-08-04 14:10:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,12 @@
+public abstract class Veiculo {
+
+
+
+	public void aumentarVelocidade() {
+		// TODO Auto-generated method stub
+
+	}
+	public void frear() {
+		// TODO Auto-generated method stub
+
+	}
