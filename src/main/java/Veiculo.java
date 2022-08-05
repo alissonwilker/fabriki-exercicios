d7f0e9f69dc0c42766914a56a3fd6eba7bca6317
leonardokdfx <103157645+leonardokdfx@users.noreply.github.com>
2022-08-05 17:27:56 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,46 @@
+
+public class Veiculo {
+
+	protected int Velocidade; {
+		
+	}
+	protected int qtdePneus; {
+	}
+
+
+	public int getVelocidade; {
+			
+	}
+	public int getQtdePneus; {
+		
+	}
+	public void aumentarVelocidade() {
+			
+	}
+	public void frear() {
+		
+		
+	}
+
+	public void parar() {
+		
+		
+	}
+
+	public void moverParaFrente() {
+		
+		
+	}
+	protected void mover() {
+		
+	}
+	protected boolean estarEmMovimento() {
+		return true;
+	}
+
+
+
+
+
+	
+}
