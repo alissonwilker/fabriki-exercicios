b84004cd13b893f667c1645f3578477c65a5d08a
RuanMateus <92128521+RuanMateus@users.noreply.github.com>
2022-08-05 18:30:59 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,24 @@
+
+public class Carro extends Automovel {
+
+	public  Carro () {
+		qtdePneus = 4;
+
+	}
+
+
+	public void moverParaTras() {
+			ligar();
+			mover();
+		engatarMarchARe();
+		marchaAFrente = false;
+
+	}
+
+	protected void engatarMarchARe() {
+
+			parar();
+		velocidade++;
+		}
+
+	}
