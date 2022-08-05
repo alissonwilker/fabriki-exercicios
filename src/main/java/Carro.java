a53dd3f8122dc5e36d1ca0d55305dbf97d619c25
Tatiana Casado <88239560+Tdrcmelo@users.noreply.github.com>
2022-08-05 19:23:45 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,23 @@
+public class Carro extends Automovel {
+
+	public  Carro () {
+		qtdePneus = 4;
+
+	}
+
+
+	public void moverParaTras() {
+			ligar();
+			mover();
+		engatarMarchARe();
+		marchaAFrente = false;
+
+	}
+
+	protected void engatarMarchARe() {
+
+			parar();
+		velocidade++;
+		}
+
+	}
