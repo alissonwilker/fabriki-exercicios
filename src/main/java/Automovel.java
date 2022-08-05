53659f51a1284eb4875382cc94f8d6bcf8d1ff49
leonardokdfx <103157645+leonardokdfx@users.noreply.github.com>
2022-08-05 17:27:56 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Automovel.java
@@ -0,0 +1,44 @@
+
+public class Automovel extends Veiculo {
+	
+	protected boolean marchaAFrente() {
+		
+		return false;
+		
+	}
+
+   private void ligado() {
+		
+		
+	}
+   
+	public void moverParaFrente() {
+		
+		
+	}
+	public void ligar() {
+		
+		
+	}
+	public void desligar() {
+		
+		
+	}
+	public boolean estaLigado() {
+		
+		return false;
+	}
+	public boolean estaEmMarchaAFrente() {
+		
+		return false;
+	}
+
+
+	protected void engatarMarchaAFrente() {
+		
+		
+	}
+	
+	
+
+}
