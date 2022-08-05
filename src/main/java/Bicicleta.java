048c067a78d39240ace7da5f440c317604f9a267
marciofilhooo <96481317+marciofilhooo@users.noreply.github.com>
2022-08-05 11:41:22 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Bicicleta.java
@@ -0,0 +1,13 @@
+public class Bicicleta extends Veiculo{
+
+
+    public Bicicleta() {
+        this.setQtdePneus(2);
+    }
+
+    public Bicicleta bicicleta(){
+        Bicicleta bicicleta = new Bicicleta();
+        return bicicleta;
+    }
+
+}
