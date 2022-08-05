9197afcf6dfd6e81c870af9888584d86943fcace
Pritempone <103539007+Pritempone@users.noreply.github.com>
2022-08-05 15:49:51 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Carro.java
@@ -0,0 +1,18 @@
+public class Carro extends Automovel {
+
+	public  Carro () {
+		qtdePneus = 4;
+	}
+
+	public void moverParaTras() {
+			ligar();
+			mover();
+		engatarMarchARe();
+		marchaAFrente = false;
+	}
+
+	protected void engatarMarchARe() {
+			parar();
+		velocidade++;
+		}
+	}
