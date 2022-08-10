13cd2153fc410b56f028ad7735d9804b75ffb94a
fernando-felix <103541087+fernando-felix@users.noreply.github.com>
2022-08-10 19:35:10 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Veiculo.java
@@ -0,0 +1,61 @@
+
+
+public abstract class Veiculo extends java.lang.Object {	
+	
+	protected int velocidade;
+	
+	protected int qtdePneus;
+	
+	public Veiculo() {	
+		
+	}
+	
+	public int getQtdePneus() {
+		
+		return qtdePneus;
+	}
+	
+	public int getVelocidade() {
+		return velocidade;
+	}	
+	
+	public void aumentarVelocidade() {
+		if (velocidade != 0) {
+		velocidade++;
+		}
+	}
+	
+	public void frear() {
+		if (velocidade != 0) {
+			velocidade--;
+			}
+	}
+	
+	public void parar() {
+		while (velocidade != 0) {
+			velocidade--;
+		}
+	}
+	
+	public void moverParaFrente() {
+		if (velocidade == 0) {
+			velocidade++;
+			}
+	}
+	
+	public void mover() {
+		if (velocidade == 0) {
+			velocidade++;
+			}
+	}
+	
+	protected boolean estaEmMovimento() {
+		if (velocidade != 0) {
+			return true;
+		}else 
+			return false;
+		
+		
+	}
+	
+}
