e824e049ba941c25531084c172087fc9e6f61d83
Pedro Carvalho <91221176+OpKSpatY@users.noreply.github.com>
2022-11-09 11:25:56 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/PessoaJuridica.java
@@ -0,0 +1,18 @@
+public class PessoaJuridica extends Cliente{
+
+	private String cnpj;
+	
+	protected PessoaJuridica(String cliente, String cnpj) {
+		super(cliente);
+		this.setCnpj(cnpj);		
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+}
