f638a3bde15b5081b271dbfc136d7592027d8703
Rossano Alves Lima Júnior <80058375+Rossano94@users.noreply.github.com>
2022-11-10 23:55:56 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,23 @@
+public class ContaBancaria {
+	
+
+	public ContaBancaria(Cliente pessoaFisica) {
+	
+	}
+	
+	public Object getSaldo() {
+		return null;
+	}
+
+	public void depositar(int valorDeposito) {
+		
+	}
+	
+	public void sacar(int valorSaque) {
+		
+	}
+	
+	public Object getCliente() {
+		return null;
+	}
+}
