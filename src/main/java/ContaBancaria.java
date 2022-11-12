7adb7cdf83b714e64f162155fec47afab34e740a
Rossano Alves Lima Júnior <80058375+Rossano94@users.noreply.github.com>
2022-11-11 21:16:27 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/ContaBancaria.java
@@ -0,0 +1,35 @@
+
+public class ContaBancaria {
+	
+	private int saldo;
+	private Cliente cliente;
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public ContaBancaria(Cliente pessoaFisica) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getSaldo() {
+		// TODO Auto-generated method stub
+		return saldo;
+	}
+
+	public void depositar(int valorDeposito) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void sacar(int valorSaque) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
