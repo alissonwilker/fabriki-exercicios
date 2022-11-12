6e34214d268865c80843192439ba1a72077c0aab
MarthosMfc <89743763+MarthosMfc@users.noreply.github.com>
2022-11-11 23:22:46 -0300

    Resolucao de exercicio do Fabriki

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente {
+	private String nome;
+
+	protected Cliente(String cliente) {
+		this.setNome(cliente);
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
